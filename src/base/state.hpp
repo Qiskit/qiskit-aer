deb5b54b25ffb8f70e0a79637c8c485875b3b9f8
cjwood <cjwood@us.ibm.com>
2018-07-27 11:20:26 -0400

    added new base State class for simulator

---
+++ src/base/state.hpp
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2018, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+/**
+ * @file    state.hpp
+ * @brief   State interface base class for qiskit-aer simulator engines
+ * @author  Christopher J. Wood <cjwood@us.ibm.com>
+ */
+
+#ifndef _aer_base_state_hpp_
+#define _aer_base_state_hpp_
+
+#include <set>
+#include <string>
+
+#include "framework/json.hpp"
+#include "framework/operations.hpp"
+#include "framework/rng.hpp"
+#include "framework/types.hpp"
+
+namespace AER {
+namespace Base {
+
+//============================================================================
+// State interface base class for Qiskit-Aer
+//============================================================================
+
+template <class state_t>
+class State {
+
+public:
+  State() = default;
+  virtual ~State() = default;
+
+  //----------------------------------------------------------------
+  // Abstract methods that must be defined by derived classes
+  //----------------------------------------------------------------
+
+  // Set of allowed operations for the state class.
+  // Example:
+  //    {"U", "CX", "measure", "reset"};
+  const static std::set<std::string> allowed_ops;
+  
+  // Applies an operation to the state class.
+  // This should support all and only the operations defined in
+  // allowed_operations.
+  virtual void apply_operation(const Op &op) = 0;
+
+  // Initializes an n-qubit state to the all |0> state
+  virtual void initialize(uint_t num_qubits) = 0;
+
+  // Returns the required memory for storing an n-qubit state in megabytes.
+  // TODO: Is this enough? Some State representaitons might also depend on 
+  // the circuit (eg. tensor slicing, clifford+t simulator)
+  virtual uint_t required_memory_mb(uint_t num_qubits, uint_t num_ops) = 0;
+
+  //----------------------------------------------------------------
+  // Optional methods: Measurement 
+  //----------------------------------------------------------------
+  
+  // If the state supports measurement this should be set to true
+  // and the apply_measure method overriden
+  bool has_measure = false;
+
+  // Measure qubits and return a list of outcomes [q0, q1, ...]
+  inline virtual reg_t apply_measure(const reg_t& qubits) {return reg_t(qubits.size(), 0);};
+
+  //----------------------------------------------------------------
+  // Optional methods: Config
+  //----------------------------------------------------------------
+  
+  // Load any settings for the State class from a config JSON
+  inline virtual void load_config(const json_t &config) {(void)config;};
+
+  //----------------------------------------------------------------
+  // Members that should not be modified by derrived classes
+  //----------------------------------------------------------------
+
+  // Returns a reference to the states data structure
+  inline state_t &data() { return data_; };
+
+  // Set the state data to a fixed value 
+  inline void set_state(const state_t &state) {data_ = state;};
+  inline void set_state(state_t &&state) {data_ = std::move(state);};
+
+  // Sets the number of threads available to the State implementation
+  // If negative there is no restriction on the backend
+  inline void set_available_threads(int n) {threads_ = n;};
+  inline int get_available_threads() {return threads_;};
+  
+  // Access the RngEngine for random number generation
+  inline RngEngine &access_rng() { return rng_; };
+
+  // Set the RngEngine seed to a fixed value
+  // Otherwise it is initialized with a random value
+  inline void set_rng_seed(uint_t seed) { rng_ = RngEngine(seed); };
+
+  
+protected:
+  // The quantum state data structure
+  state_t data_;
+
+  // Maximum threads which may be used by the backend for OpenMP multithreading
+  // Default value is single-threaded unless overridden
+  int threads_ = 1;
+
+  // The RngEngine for the state
+  RngEngine rng_;
+};
+
+//------------------------------------------------------------------------------
+} // end namespace Base
+} // end namespace AER
+//------------------------------------------------------------------------------
+#endif
---
+++ src/framework/rng.hpp
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2018, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+/**
+ * @file rng.hpp
+ * @brief RngEngine use by the BaseBackend simulator class
+ * @author Christopher J. Wood <cjwood@us.ibm.com>
+ */
+
+#ifndef _aer_framework_rng_hpp_
+#define _aer_framework_rng_hpp_
+
+#include <cstdint>
+#include <random>
+
+#include "framework/types.hpp"
+
+namespace AER {
+
+/***************************************************************************/ /**
+  *
+  * RngEngine Class
+  *
+  * Objects of this class are used to generate random numbers for backends.
+  *These
+  * are used to decide outcomes of measurements and resets, and for implementing
+  * noise.
+  *
+  ******************************************************************************/
+
+class RngEngine {
+public:
+  /**
+   * Generate a uniformly distributed pseudo random real in the half-open
+   * interval [a,b)
+   * @param a closed lower bound on interval
+   * @param b open upper bound on interval
+   * @return the generated double
+   */
+  double rand(double a, double b);
+
+  /**
+   * Generate a uniformly distributed pseudo random real in the half-open
+   * interval [0,b)
+   * @param b open upper bound on interval
+   * @return the generated double
+   */
+  inline double rand(double b) { return rand(0, b); };
+
+  /**
+   * Generate a uniformly distributed pseudo random real in the half-open
+   * interval [0,1)
+   * @return the generated double
+   */
+  inline double rand() { return rand(0, 1); };
+
+  /**
+   * Generate a uniformly distributed pseudo random integer in the closed
+   * interval [a,b]
+   * @param a lower bound on interval
+   * @param b upper bound on interval
+   * @return the generated integer
+   */
+  int_t rand_int(int_t a, int_t b);
+
+  /**
+   * Generate a pseudo random integer from an input discrete distribution
+   * @param probs the discrete distribution to sample from
+   * @return the generated integer
+   */
+  template<class IntType> 
+  IntType rand_int(std::discrete_distribution<IntType> probs);
+
+  /**
+   * Generate a pseudo random integer from a a discrete distribution
+   * constructed from an input vector of probabilities for [0,..,n-1]
+   * where n is the lenght of the vector. If this vector is not normalized
+   * it will be scaled when it is converted to a discrete_distribution
+   * @param probs the vector of probabilities
+   * @return the generated integer
+   */
+  int_t rand_int(const std::vector<double> &probs);
+
+  /**
+   * Default constructor initialize RNG engine with a random seed
+   */
+  RngEngine() {
+    std::random_device rd;
+    rng.seed(rd());
+  };
+
+  /**
+   * Seeded constructor initialize RNG engine with a fixed seed
+   * @param seed integer to use as seed for mt19937 engine
+   */
+  explicit RngEngine(uint_t seed) { rng.seed(seed); };
+
+private:
+  std::mt19937 rng; // Mersenne twister rng engine
+};
+
+/*******************************************************************************
+ *
+ * RngEngine Methods
+ *
+ ******************************************************************************/
+
+double RngEngine::rand(double a, double b) {
+  double p = std::uniform_real_distribution<double>(a, b)(rng);
+  return p;
+}
+
+// randomly distributed integers in [a,b]
+int_t RngEngine::rand_int(int_t a, int_t b) {
+  int_t n = std::uniform_int_distribution<int_t>(a, b)(rng);
+  return n;
+}
+
+// randomly distributed integers from discrete distribution
+template <class IntType>
+IntType RngEngine::rand_int(std::discrete_distribution<IntType> probs) {
+  IntType n = probs(rng);
+  return n;
+}
+
+// randomly distributed integers from vector
+int_t RngEngine::rand_int(const std::vector<double> &probs) {
+  int_t n = std::discrete_distribution<int_t>(probs.begin(), probs.end())(rng);
+  return n;
+}
+
+//------------------------------------------------------------------------------
+} // End namespace QISKIT
+#endif
