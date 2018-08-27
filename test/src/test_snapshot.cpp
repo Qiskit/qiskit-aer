28ca71c197d693bbd054a79d22d4848c1b538069
Juan Gomez <atilag@gmail.com>
2018-08-27 15:30:39 +0200

    * Added CMake support for testing using Catch2

--- test/src/test_snapshot.cpp
+++ test/src/test_snapshot.cpp
@@ -2,10 +2,10 @@
 #include <map>
 #include <catch.hpp>
 
-#include "base/controller.hpp"
-#include "base/engine.hpp"
-#include "simulators/qubitvector/qubitvector.hpp"
-#include "simulators/qubitvector/qv_state.hpp"
+#include <base/controller.hpp>
+#include <base/engine.hpp>
+#include <simulators/qubitvector/qubitvector.hpp>
+#include <simulators/qubitvector/qv_state.hpp>
 
 #include "utils.hpp"
 
@@ -16,26 +16,25 @@ SCENARIO("We can get snapshots from different simulator types") {
         
         std::map<std::string, json_t> qobj_snapshots;
         qobj_snapshots["state"] = 
-            AER::Test::Utilities::load_qobj("../../../test/data/qobj_snapshot_state.json");
+            AER::Test::Utilities::load_qobj("../../test/data/qobj_snapshot_state.json");
         qobj_snapshots["probs"] = 
-            AER::Test::Utilities::load_qobj("../../../test/data/qobj_snapshot_probs.json");
+            AER::Test::Utilities::load_qobj("../../test/data/qobj_snapshot_probs.json");
         qobj_snapshots["pauli"] = 
-            AER::Test::Utilities::load_qobj("../../../test/data/qobj_snapshot_pauli.json");
+            AER::Test::Utilities::load_qobj("../../test/data/qobj_snapshot_pauli.json");
         qobj_snapshots["matrix"] =
-            AER::Test::Utilities::load_qobj("../../../test/data/qobj_snapshot_matrix.json");
+            AER::Test::Utilities::load_qobj("../../test/data/qobj_snapshot_matrix.json");
 
         using State = AER::QubitVector::State;
-        using Engine = AER::Base::Engine<QV::QubitVector>;
         AER::Base::Controller sim{};
 
         WHEN("we get the expected results"){
-             auto expected_result = R"({
-                 "final":[[[0.7071067811865476,0.0],[0.0,0.0],[0.0,0.0],[0.7071067811865475,0.0]]],
-                 "initial":[[[1.0,0.0],[0.0,0.0],[0.0,0.0],[0.0,0.0]]],
-                 "middle":[[[0.7071067811865476,0.0],[0.7071067811865475,0.0],[0.0,0.0],[0.0,0.0]]]
+            auto expected_result = R"({
+                "final":[[[0.7071067811865476,0.0],[0.0,0.0],[0.0,0.0],[0.7071067811865475,0.0]]],
+                "initial":[[[1.0,0.0],[0.0,0.0],[0.0,0.0],[0.0,0.0]]],
+                "middle":[[[0.7071067811865476,0.0],[0.7071067811865475,0.0],[0.0,0.0],[0.0,0.0]]]
             })"_json;
             THEN("the state simulator should pass"){
-                auto result = sim.execute<Engine, State>(qobj_snapshots["state"]);
+                auto result = sim.execute<QV::QubitVector, State>(qobj_snapshots["state"]);
                 result = result["result"][0]["data"]["snapshots"]["state"];
                 REQUIRE(result == expected_result);
             }
--- test/src/utils.hpp
+++ test/src/utils.hpp
@@ -3,7 +3,6 @@
 #include "framework/json.hpp"
 
 namespace AER::Test::Utilities {
-    // QObj Utilities
     json_t load_qobj(const std::string& filename){
         return JSON::load(filename);
     }
