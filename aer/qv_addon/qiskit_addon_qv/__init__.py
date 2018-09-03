02a62381f279b8941367c9040ca9bfe80ad9abdf
Juan Gomez <juan.gomez1@ibm.com>
2018-09-03 23:06:52 +0200

    * Added support for using the QV Simulator as Qiskit Addon

---
+++ aer/qv_addon/qiskit_addon_qv/__init__.py
@@ -0,0 +1,4 @@
+from .aer_qv_provider import AerQvProvider
+from aer_qv_wrapper import AerQvSimulatorWrapper
+
+__version__ = '0.0.1'
---
+++ aer/qv_addon/setup.py
@@ -0,0 +1,26 @@
+from skbuild import setup
+from setuptools import find_packages
+
+setup(
+    name='qiskit_addon_qv',
+    packages=['qiskit_addon_qv'],
+    cmake_source_dir='../..',
+    description="AER - QV Quantum Simulator addon for Qiskit",
+    author="AER Development Team",
+    author_email="qiskit@us.ibm.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit simulator quantum addon backend"
+)
