f53b9ff89e1e1135c89dc01204d4d4a2da306cbd
cjwood <cjwood@us.ibm.com>
2018-09-13 13:05:01 -0400

    added qiskit backend for qv_wrapper and updated input/output json to schema spec

--- aer/qv_addon/qiskit_addon_qv/__init__.py
+++ aer/qv_addon/qiskit_addon_qv/__init__.py
@@ -1,4 +1,4 @@
 from .aer_qv_provider import AerQvProvider
-from aer_qv_wrapper import AerQvSimulatorWrapper
+from .aer_qv_simulator import AerQvSimulator
 
 __version__ = '0.0.1'
--- src/framework/qobj.hpp
+++ src/framework/qobj.hpp
@@ -71,8 +71,8 @@ void Qobj::load_qobj_from_string(const std::string &input) {
 void Qobj::load_qobj_from_json(const json_t &js) {
 
   // Get qobj id
-  if (JSON::get_value(id, "id", js) == false) {
-    throw std::invalid_argument("Invalid qobj: no \"id\" field");
+  if (JSON::get_value(id, "qobj_id", js) == false) {
+    throw std::invalid_argument("Invalid qobj: no \"qobj_id\" field");
   };
   // Get header and config;
   JSON::get_value(config, "config", js);
@@ -88,7 +88,7 @@ void Qobj::load_qobj_from_json(const json_t &js) {
   }
   const json_t &circs = js["experiments"];
   for (auto it = circs.cbegin(); it != circs.cend(); ++it) {
-    circuits.emplace_back(*it);
+    circuits.emplace_back(*it, config);
   }
 }
 
