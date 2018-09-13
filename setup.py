ab90f67a082e3ee1e526cb8adcb302c05ef9e630
Juan Gomez <juan.gomez1@ibm.com>
2018-09-13 12:16:36 +0200

    * Fixed old setup.py to include third-party headers in the build * Fixed template parameter for State on Cyhton wrapper

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ from simulator_extension import simulator_extension
 # Simulator extension
 package_name = 'aer.backends.aer_qv_wrapper'
 source_files = [os.path.abspath('src/simulators/qubitvector/qv_wrapper.pyx')]
-include_dirs = [os.path.abspath('./src')]
+include_dirs = [os.path.abspath('./src'),
+                os.path.abspath('./src/third-party/headers')]
 
 simulator = simulator_extension(package_name, source_files, include_dirs=include_dirs)
 
