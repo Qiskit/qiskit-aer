98197d55f4d29e4af32fff54ce0a24d56fab1b62
yaelbh <yaelbh@il.ibm.com>
2018-09-20 16:47:37 +0300

    Basis for the first Python test

---
+++ test/terra/test_without_measurement.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+from test.terra.common import QiskitQvTestCase
+
+import unittest
+
+from qiskit import (execute, QuantumRegister,
+                    ClassicalRegister, QuantumCircuit, wrapper)
+from qiskit.extensions.simulator.snapshot import snapshot
+from qiskit_addon_qv import AerQvSimulator
+
+
+class QvNoMeasurementTest(QiskitQvTestCase):
+    """Test the final statevector in circuits whose simulation is deterministic, i.e., costain no measurement or noise"""
+
+    def setUp(self):
+        # Comment will be removed when Issue #35 is resolved
+        #wrapper.register(provider_class=AerQvSimulator)
+        self._number_of_qubits = 6
+
+    def test_no_measurement(self):     
+        """Test the final statevector in circuits whose simulation is deterministic, i.e., constain no measurement or noise"""
+
+        # A very simple circuit,
+        # to be replaced by random circuits covering all types of gates
+        q = QuantumRegister(self._number_of_qubits)
+        circuit = QuantumCircuit(q)
+        circuit.h(q[0])
+        circuit.cx(q[0], q[1])
+
+        # Waiting for Issue #10 to be resolved
+        circuit.state_snapshot('0')
+
+        # The following line will replace the one next to it
+        # when Issue #35 is resolved
+        #result = execute(circuit, provider='local_qv_simulator').result()
+        result = execute(circuit, backend=AerQvSimulator(), skip_transpiler=True).result()
+        self.assertEqual(result.get_status(), 'COMPLETED')
+
+        # Compare with the result of the Python simulator
+        result_py = execute(circuit, backend='local_statevector_simulator_py').result()
+        self.assertEqual(result_py.get_status(), 'COMPLETED')
+
+        # Assuming that the Python simulator supports statevector,
+        # and that it supports the same gates as the aer simulator:
+        # Add lines to verify the same statevector
+        # for the Python and aer simulators
+
+
+if __name__ == '__main__':
+    unittest.main()
