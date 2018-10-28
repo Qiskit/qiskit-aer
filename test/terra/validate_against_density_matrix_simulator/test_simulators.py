58ccea6ec34410abc961a3ee86cfe6c69fffdfbc
yaelbh <yaelbh@il.ibm.com>
2018-10-28 12:15:14 +0200

    Rewrote test_simulators as a unittest, and moved it to new simulator API

--- test/terra/validate_against_density_matrix_simulator/test_simulators.py
+++ test/terra/validate_against_density_matrix_simulator/test_simulators.py
@@ -1,19 +1,27 @@
+import test.terra.common as common
+import unittest
+
 from density_matrix_simulator import DensityMatrixSimulator
 from qstructs import DensityMatrix
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import compile
-from qiskit_addon_qv import AerQvSimulator
+from qiskit_aer.backends import QasmSimulator
 
-qv_backend = AerQvSimulator()
-den_sim = DensityMatrixSimulator()
+class TestSimulators(common.QiskitAerTestCase):
 
-qreg = QuantumRegister(2)
-creg = ClassicalRegister(4)
-qc = QuantumCircuit(qreg, creg)
-qc.h(qreg[0])
-qc.cx(qreg[0], qreg[1])
+    def setUp(self):
+        self.qasm_sim = QasmSimulator()
+        self.den_sim = DensityMatrixSimulator()
 
-qobj = compile(qc, qv_backend)
-result = den_sim.run(qobj)
-print(result)
+    def test_simulators(self):
+        
+        qreg = QuantumRegister(2)
+        creg = ClassicalRegister(4)
+        qc = QuantumCircuit(qreg, creg)
+        qc.h(qreg[0])
+        qc.cx(qreg[0], qreg[1])
+
+        qobj = compile(qc, self.qasm_sim)
+        result = self.den_sim.run(qobj)
+        print(result)
 
