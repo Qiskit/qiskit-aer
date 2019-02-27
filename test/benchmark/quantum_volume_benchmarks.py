e5a6243d7becb741f36ae35eeade7f77e2a00691
Juan Gomez <juan.gomez1@ibm.com>
2019-02-27 19:03:05 +0100

    * Added new benchmarks for simple circuits with just u3 and cnots   but different number of qubits * Removing old code

--- test/benchmark/quantum_volume_benchmarks.py
+++ test/benchmark/quantum_volume_benchmarks.py
@@ -4,13 +4,11 @@
 import qiskit as Terra
 from qiskit import QiskitError
 from qiskit.providers.aer import QasmSimulator
-from .quantumvolume import quantum_volume_circuit
-from .tools import tools_mixed_unitary_noise_model, \
-                   tools_reset_noise_model, \
-                   tools_kraus_noise_model
+from .tools import quantum_volume_circuit, mixed_unitary_noise_model, \
+                    reset_noise_model, kraus_noise_model
 
 
-class TimeSuite:
+class QuantumVolumeTimeSuite:
     """
     Benchmarking times for Quantum Volume with various noise configurations
     - ideal (no noise)
@@ -23,16 +21,16 @@ class TimeSuite:
     """
 
     def __init__(self):
-        # We want always the same SEED, as we want always the same circuits
-        # for the same value pairs of qubits,depth
         self.timeout = 60 * 20
         self.qv_circuits = []
         self.backend = QasmSimulator()
         for num_qubits in 16 ,:
             for depth in 10 ,:
+                # We want always the same seed, as we want always the same circuits
+                # for the same value pairs of qubits,depth
                 circ = quantum_volume_circuit(num_qubits, depth, seed=1)
                 self.qv_circuits.append(
-                    Terra.compile(circ, self.backend, shots=1024)
+                    Terra.compile(circ, self.backend, shots=1)
                 )
         self.param_names = ["Quantum Volume (16qubits 10depth)"]
         self.params = (self.qv_circuits)
@@ -50,7 +48,7 @@ class TimeSuite:
     def time_mixed_unitary_quantum_volume(self, qobj):
         result = self.backend.run(
             qobj,
-            noise_model=tools_mixed_unitary_noise_model()
+            noise_model=mixed_unitary_noise_model()
         ).result()
         if result.status != 'COMPLETED':
             raise QiskitError("Simulation failed. Status: " + result.status)
@@ -59,7 +57,7 @@ class TimeSuite:
     def time_reset_quantum_volume(self, qobj):
         result = self.backend.run(
             qobj,
-            noise_model=tools_reset_noise_model()
+            noise_model=reset_noise_model()
         ).result()
         if result.status != 'COMPLETED':
             raise QiskitError("Simulation failed. Status: " + result.status)
@@ -68,7 +66,7 @@ class TimeSuite:
     def time_kraus_quantum_volume(self, qobj):
         result = self.backend.run(
             qobj,
-            noise_model=tools_kraus_noise_model()
+            noise_model=kraus_noise_model()
         ).result()
         if result.status != 'COMPLETED':
             raise QiskitError("Simulation failed. Status: " + result.status)
---
+++ test/benchmark/simple_benchmarks.py
@@ -0,0 +1,127 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+
+import qiskit as Terra
+from qiskit import QiskitError
+from qiskit.providers.aer import QasmSimulator
+from .tools import simple_u3_circuit, simple_cnot_circuit, \
+                    mixed_unitary_noise_model, reset_noise_model, \
+                    kraus_noise_model
+
+class SimpleU3TimeSuite:
+    """
+    Benchmarking times for simple circuits with just one gate but with various
+    noise configurations:
+    - ideal (no noise)
+    - mixed state
+    - reset
+    - kraus
+
+    For each noise model, we want to test various configurations of number of
+    qubits
+    """
+
+    def __init__(self):
+
+        self.timeout = 60 * 20
+        self.backend = QasmSimulator()
+        self.circuits = []
+        for i in 5,16,20,30:
+            circuit = simple_u3_circuit(i)
+            self.circuits.append(
+                Terra.compile(circuit, self.backend, shots=1)
+            )
+
+        self.param_names = ["Simple u3 circuits (5/16/20/30 qubits)"]
+        self.params = (self.circuits)
+
+    def time_ideal_simple_u3(self, qobj):
+        result = self.backend.run(qobj).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_mixed_unitary_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=mixed_unitary_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_reset_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=reset_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_kraus_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=kraus_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+class SimpleCxTimeSuite:
+    """
+    Benchmarking times for simple circuits with just one gate but with various
+    noise configurations:
+    - ideal (no noise)
+    - mixed state
+    - reset
+    - kraus
+
+    For each noise model, we want to test various configurations of number of
+    qubits
+    """
+
+    def __init__(self):
+
+        self.timeout = 60 * 20
+        self.backend = QasmSimulator()
+        self.circuits = []
+        self.param_names = ["Simple cnot circuits (5/16/20/30 qubits)"]
+        for i in 5,16,20,30:
+            circuit = simple_cnot_circuit(i)
+            self.circuits.append(
+                Terra.compile(circuit, self.backend, shots=1)
+            )
+        self.params = (self.circuits)
+
+    def time_ideal_simple_u3(self, qobj):
+        result = self.backend.run(qobj).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_mixed_unitary_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=mixed_unitary_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_reset_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=reset_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
+
+
+    def time_kraus_simple_u3(self, qobj):
+        result = self.backend.run(
+            qobj,
+            noise_model=kraus_noise_model()
+        ).result()
+        if result.status != 'COMPLETED':
+            raise QiskitError("Simulation failed. Status: " + result.status)
