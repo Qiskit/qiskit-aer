5964437d18a27c603d8a489b831f4ced8bda9aa7
Shelly Garion <shelly@il.ibm.com>
2019-04-10 17:39:21 +0300

    updated ref_initialize_temp

--- test/terra/utils/ref_initialize_temp_2.py
+++ test/terra/utils/ref_initialize_temp_2.py
@@ -2,7 +2,7 @@ from qiskit import *
 from qiskit import Aer, execute
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, QiskitError
 from qiskit.providers.aer import StatevectorSimulator
-from qiskit.compiler import assemble_circuits
+from qiskit.compiler import assemble_circuits,  RunConfig
 from qiskit.qobj import QasmQobjInstruction
 import numpy as np
 
@@ -83,14 +83,14 @@ def initialize_QuantumCircuit():
     statevector = result.get_statevector(circ4)
     print ("statevector of circ4:", statevector)
 
-    qobj = assemble_circuits(circ4)
+    qobj = assemble_circuits(circ4, run_config=RunConfig(shots=1000))
     print(qobj)
     qobj.experiments[0].instructions.append(QasmQobjInstruction(name='initialize', qubits=[0], params=[[0,0],[1,0]]))
     print(qobj)
 
-    #result = simulator.run(qobj).result()
-    #statevector = result.get_statevector(circ4)
-    #print ("statevector of updated circ4:", statevector)
+    result = simulator.run(qobj).result()
+    statevector = result.get_statevector(circ4)
+    print ("statevector of updated circ4 (after initialize):", statevector)
 
     circ5 = QuantumCircuit(qr)
     circ5.h(qr[0])
