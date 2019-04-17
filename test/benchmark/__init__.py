ae73c08942ff58f450474d3d19d1d7a995cc46f4
Juan Gomez <atilag@gmail.com>
2019-04-17 15:05:40 +0200

    * Monkey patching Qobj.__repr__ for beautifull ASV reports * Tweaking asv config files

--- test/benchmark/__init__.py
+++ test/benchmark/__init__.py
@@ -1,1 +1,12 @@
+from qiskit.qobj import Qobj
 
+def repr_hook(self):
+    """ This is needed for beauty printing reports from ASV """
+    return "Qobj<{0} qubits, {1} {2}, {3} shots>".format(
+        self.config.n_qubits,
+        len(self.experiments),
+        'circuits' if self.type == 'QASM' else 'schedules',
+        self.config.shots
+    )
+
+Qobj.__repr__ = repr_hook
