8c0b1a94fce263c879345377b4ddffd6c61db397
Matthew Treinish <mtreinish@kortar.org>
2019-04-24 10:51:15 -0400

    Tweak install command for openblas

--- tools/install_wheel_deps.sh
+++ tools/install_wheel_deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 curl -OL http://github.com/xianyi/OpenBLAS/archive/v0.2.20.tar.gz
-tar xzvf v0.2.20.tar.gz
-cd OpenBLAS-0.2.20
+tar xzf v0.2.20.tar.gz
+pushd OpenBLAS-0.2.20
 make
-make install
+make PREFIX=/usr/local install
+popd
