d281a006a2d320cd4451462e6356d59c5a5d855e
cjwood <cjwood@us.ibm.com>
2019-04-29 14:39:08 -0400

    update copyright headers

--- src/misc/gcc_omp_symbols.hpp
+++ src/misc/gcc_omp_symbols.hpp
@@ -1,3 +1,17 @@
+/**
+ * This code is part of Qiskit.
+ *
+ * (C) Copyright IBM Corp. 2017 and later.
+ *
+ * This code is licensed under the Apache License, Version 2.0. You may
+ * obtain a copy of this license in the LICENSE.txt file in the root directory
+ * of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Any modifications or derivative works of this code must retain this
+ * copyright notice, and modified files need to carry a notice indicating
+ * that they have been altered from the originals.
+ */
+
 #ifndef _aer_misc_hacks_gcc_symbols_
 #define _aer_misc_hacks_gcc_symbols_
 
