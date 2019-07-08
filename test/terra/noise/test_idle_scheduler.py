6da67913c006952e071eac15630b48940968ebcb
Gadi <gadia@il.ibm.com>
2019-07-08 10:50:18 +0300

    Fixes to the idle shceduler tests

--- test/terra/noise/test_idle_scheduler.py
+++ test/terra/noise/test_idle_scheduler.py
@@ -19,9 +19,6 @@ from qiskit.converters import circuit_to_dag
 import unittest
 
 class TestIdleScheduler(unittest.TestCase):
-    def assertCircuitsEqual(self, lhs, rhs):
-        self.assertEqual(circuit_to_dag(lhs), circuit_to_dag(rhs))
-
     def test_circuits_equal(self):
         qr = QuantumRegister(3, 'qr')
         circuit = QuantumCircuit(qr)
@@ -33,10 +30,9 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.iden(qr[1])
         target_circuit.y(qr[2])
 
-        circuit1 = schedule_idle_gates(circuit, op_times={'id': 2, 'x': 2})
-        circuit2 = schedule_idle_gates(circuit, op_times={'id': 2, 'x': 2})
+        result_circuit = schedule_idle_gates(circuit, op_times={'id': 2, 'x': 2})
 
-        self.assertCircuitsEqual(circuit1, circuit2)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit(self):
         qr = QuantumRegister(3, 'qr')
@@ -50,7 +46,7 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.y(qr[2])
 
         result_circuit = schedule_idle_gates(circuit)
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_double_x_time(self):
         qr = QuantumRegister(3, 'qr')
@@ -67,7 +63,7 @@ class TestIdleScheduler(unittest.TestCase):
 
         result_circuit = schedule_idle_gates(circuit, op_times={'x': 2})
 
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_double_id_time(self):
         qr = QuantumRegister(3, 'qr')
@@ -80,7 +76,7 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.y(qr[2])
 
         result_circuit = schedule_idle_gates(circuit, op_times={'id': 2})
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_double_id_and_x_time(self):
         qr = QuantumRegister(3, 'qr')
@@ -94,7 +90,7 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.y(qr[2])
 
         result_circuit = schedule_idle_gates(circuit, op_times={'id': 2, 'x':2})
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_double_id_and_x_time_double_length(self):
         qr = QuantumRegister(3, 'qr')
@@ -115,7 +111,7 @@ class TestIdleScheduler(unittest.TestCase):
 
         result_circuit = schedule_idle_gates(circuit, op_times={'id': 2, 'x': 2})
 
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_barrier_circuit(self):
         qr = QuantumRegister(3, 'qr')
@@ -135,7 +131,7 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.iden(qr[2])
 
         result_circuit = schedule_idle_gates(circuit)
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_nondefault_time(self):
         qr = QuantumRegister(3, 'qr')
@@ -149,7 +145,7 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.y(qr[2])
 
         result_circuit = schedule_idle_gates(circuit, default_op_time=0.3153)
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
 
     def test_small_circuit_nondefault_time_and_different_x_y(self):
         qr = QuantumRegister(3, 'qr')
@@ -165,4 +161,4 @@ class TestIdleScheduler(unittest.TestCase):
         target_circuit.y(qr[2])
 
         result_circuit = schedule_idle_gates(circuit, default_op_time=0.3153, op_times={'x': 0.974, 'y': 0.734})
-        self.assertCircuitsEqual(target_circuit, result_circuit)
+        self.assertEqual(target_circuit, result_circuit)
