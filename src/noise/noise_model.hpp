d7dbf6311d00b26f812f5090768a4f85fae67f07
cjwood <cjwood@us.ibm.com>
2019-07-26 16:38:29 -0400

    add qubit remapper to NoiseModel

--- src/noise/noise_model.hpp
+++ src/noise/noise_model.hpp
@@ -54,6 +54,39 @@ public:
   // can be done in a thread-safe manner
   Circuit sample_noise(const Circuit &circ, RngEngine &rng) const;
 
+  //-----------------------------------------------------------------------
+  // Checking if errors types are in noise model
+  //-----------------------------------------------------------------------
+
+  // Return True if noise model contains no readout errors
+  inline bool ideal_readout() const {
+    return readout_errors_.empty();
+  }
+
+  // Return True if noise model contains no quantum errors
+  inline bool ideal_quantum() const {
+    return ideal_local_quantum() && ideal_nonlocal_quantum();
+  }
+
+  // Return True if noise model contains no nonlocal quantum errors
+  inline bool ideal_nonlocal_quantum() const {
+    return !nonlocal_quantum_errors_;
+  }
+
+  // Return True if noise model contains no local quantum errors
+  inline bool ideal_local_quantum() const {
+    return !local_quantum_errors_;
+  }
+
+  // Return true if the noise model is ideal
+  inline bool ideal() const {
+    return ideal_quantum() && ideal_readout();
+  }
+
+  //-----------------------------------------------------------------------
+  // Add errors to noise model
+  //-----------------------------------------------------------------------
+
   // Load a noise model from JSON
   void load_from_json(const json_t &js);
 
@@ -67,15 +100,38 @@ public:
   void add_readout_error(const ReadoutError &error,
                          const std::vector<reg_t> &op_qubits = {});
   
-  // Return true if the noise model is ideal
-  inline bool ideal() const {
-    return !(local_quantum_errors_ || nonlocal_quantum_errors_) && readout_errors_.empty();
-  }
-
   // Set which single qubit gates should use the X90 waltz error model
   inline void set_x90_gates(const stringset_t &x90_gates) {
     x90_gates_ = x90_gates;
   }
+
+  //-----------------------------------------------------------------------
+  // Utils
+  //-----------------------------------------------------------------------
+
+  // Remap the qubits in the noise model.
+  // A remapping is entered as a map {old: new}
+  // Any qubits not in the mapping are assumed to be mapped to themselves.
+  // Hence the sets of all keys and all values of the map must be equal.
+  void remap_qubits(const std::unordered_map<uint_t, uint_t> &mapping);
+
+  // Return vector of noise qubits for non local error on specified label and qubits
+  // If no nonlocal error exists an empty set is returned.
+  std::set<uint_t> nonlocal_noise_qubits(const std::string label, const reg_t &qubits) const;
+
+  // Return vector of qubit registers for all readout errors in noise model.
+  // The empty reg is used to an all-qubit readout error.
+  std::vector<reg_t> readout_error_keys() const;
+
+  // Return vector of key pairs for all non-local quantum errors in the noise
+  // model. Vector elements are tuples (label, qubits, noise_qubits) where
+  // label is the string label for the error instruction, qubits is the reg of
+  // qubits the instruction is applied to, and noise_qubits is the reg of
+  // qubits the error is applied to.
+  // The empty noise_qubit reg is used for local quantum errors.
+  // The empty qubit reg is used for all-qubit quantum errors.
+  using keytuple_t = std::tuple<std::string, reg_t, reg_t>;
+  std::vector<keytuple_t> quantum_error_keys() const;
 
   // Set threshold for applying u1 rotation angles.
   // an Op for u1(theta) will only be added if |theta| > 0 and |theta - 2*pi| > 0
@@ -140,6 +196,9 @@ private:
   // List of readout errors
   std::vector<ReadoutError> readout_errors_;
 
+  // Set of qubits used in noise model
+  std::set<uint_t> noise_qubits_;
+
   using inner_table_t = stringmap_t<std::vector<size_t>>;
   using outer_table_t = stringmap_t<inner_table_t>;
 
@@ -156,6 +215,8 @@ private:
   // Helper function to convert reg to string for key of unordered maps/sets
   std::string reg2string(const reg_t &reg) const;
   reg_t string2reg(std::string s) const;
+  std::string remap_string(const std::string key,
+                           const std::unordered_map<uint_t, uint_t> &mapping) const;
 
   // Table of single-qubit gates to use a Waltz X90 based error model
   stringset_t x90_gates_;
@@ -263,8 +324,11 @@ void NoiseModel::add_readout_error(const ReadoutError &error,
   if (op_qubits.empty()) {
     readout_error_table_[""].push_back(error_pos);
   } else {
-    for (const auto &qubits : op_qubits)
+    for (const auto &qubits : op_qubits) {
       readout_error_table_[reg2string(qubits)].push_back(error_pos);
+      for (const auto &qubit : qubits)
+        noise_qubits_.insert(qubit);
+    }
   }
 }
 
@@ -303,8 +367,11 @@ void NoiseModel::add_local_quantum_error(const QuantumError &error,
   const auto error_pos = quantum_errors_.size() - 1;
   // Add error index to the error table
   for (const auto &gate: op_labels)
-    for (const auto &qubits : op_qubits)
+    for (const auto &qubits : op_qubits) {
       local_quantum_error_table_[gate][reg2string(qubits)].push_back(error_pos);
+      for (const auto &qubit : qubits)
+        noise_qubits_.insert(qubit);
+    }
 }
 
 
@@ -323,9 +390,15 @@ void NoiseModel::add_nonlocal_quantum_error(const QuantumError &error,
   const auto error_pos = quantum_errors_.size() - 1;
   // Add error index to the error table
   for (const auto &gate: op_labels)
-    for (const auto &qubits_gate : op_qubits)
-      for (const auto &qubits_noise : noise_qubits)
+    for (const auto &qubits_gate : op_qubits) {
+      for (const auto &qubit : qubits_gate)
+        noise_qubits_.insert(qubit);
+      for (const auto &qubits_noise : noise_qubits) {
         nonlocal_quantum_error_table_[gate][reg2string(qubits_gate)][reg2string(qubits_noise)].push_back(error_pos);
+        for (const auto &qubit : qubits_noise)
+          noise_qubits_.insert(qubit);
+      }
+    }
 }
 
 
@@ -606,6 +679,168 @@ reg_t NoiseModel::string2reg(std::string s) const {
     s.erase(0, pos + 1);
   }
   return result;
+}
+
+
+//=========================================================================
+// Qubit Remapping
+//=========================================================================
+
+std::set<uint_t> NoiseModel::nonlocal_noise_qubits(const std::string label,
+                                                   const reg_t& qubits) const {
+  std::set<uint_t> all_noise_qubits;
+  // Check if label has noise
+  const auto outer_it = nonlocal_quantum_error_table_.find(label);
+  if (outer_it != nonlocal_quantum_error_table_.end()) {
+    const auto outer_table = outer_it->second;
+    const auto it = outer_table.find(reg2string(qubits));
+    // Check if label on specified qubits has noise
+    if (it != outer_table.end()) {
+      // Add all noise qubit errors to the return value
+      const auto inner_table = it->second;
+      for (const auto &pair : inner_table) {
+        auto noise_qubits = string2reg(pair.first);
+        for (const auto& qubit : noise_qubits) {
+          all_noise_qubits.insert(qubit);
+        }
+      }
+    }
+  }
+  return all_noise_qubits;
+}
+
+
+std::string NoiseModel::remap_string(const std::string key,
+                                     const std::unordered_map<uint_t, uint_t> &mapping) const{
+  reg_t qubits = string2reg(key);
+  for (size_t j=0; j<qubits.size(); j++)
+    qubits[j] = mapping.at(qubits[j]);
+  return reg2string(qubits);
+}
+
+
+void NoiseModel::remap_qubits(const std::unordered_map<uint_t, uint_t> &mapping) {
+
+  // If noise model is ideal we have no need to remap
+  if (ideal())
+    return;
+
+  // We only need the mapping for qubits in the noise model.
+  // We add qubits not specified in the mapping as trivial mapping to themselves
+  // We also validate the mapping while building the full mapping
+  std::unordered_map<uint_t, uint_t> full_mapping;
+  std::set<uint_t> qubits_in;
+  std::set<uint_t> qubits_out;
+  for (const auto &qubit : noise_qubits_) {
+    qubits_in.insert(qubit);
+    if (mapping.find(qubit) == mapping.end()) {
+      full_mapping[qubit] = qubit;
+      qubits_out.insert(qubit);
+    } else {
+      auto mapped_qubit = mapping.at(qubit);
+      full_mapping[qubit] = mapped_qubit;
+      qubits_out.insert(mapped_qubit);
+    }
+  }
+  // Check mapping is valid
+  if (qubits_in != qubits_out) {
+    throw std::invalid_argument("NoiseModel: qubit remapping is invalid.");
+  }
+
+  // Remap readout error
+  if (!ideal_readout()) {
+    inner_table_t new_readout_error_table;
+    for (const auto& pair : readout_error_table_) {
+      new_readout_error_table[remap_string(pair.first, full_mapping)] = pair.second;
+    }
+    readout_error_table_ = new_readout_error_table;
+    new_readout_error_table.clear();
+  }
+
+  // Remap local quantum error
+  if (!ideal_local_quantum()) {
+    for (auto& outer_pair : local_quantum_error_table_) {
+      // Get reference to the inner table we need to change the keys for
+      auto& inner_table = outer_pair.second;
+      // Make a temporary table to store remapped table
+      inner_table_t new_table;
+      for (const auto& inner_pair : inner_table) {
+        new_table[remap_string(inner_pair.first, full_mapping)] = inner_pair.second;
+      }
+      // Replace inner table with the remapped table
+      inner_table = new_table;
+    }
+  }
+
+  // Remap nonlocal quantum error
+    if (!ideal_nonlocal_quantum()) {
+    for (auto& pair : nonlocal_quantum_error_table_) {
+      // Get reference to the middle table we need to change the keys for
+      auto& outer_table = pair.second;
+      // Make a temporary table to store remapped outer table
+      outer_table_t new_outer_table;
+      for (auto& outer_pair : outer_table) {
+        // Remap inner table
+        auto& inner_table = outer_pair.second;
+        inner_table_t new_inner_table;
+        for (const auto& inner_pair : inner_table) {
+          new_inner_table[remap_string(inner_pair.first, full_mapping)] = inner_pair.second;
+        }
+        // Update outer table with remapped inner table
+        new_outer_table[remap_string(outer_pair.first, full_mapping)] = new_inner_table;
+      }
+      outer_table = new_outer_table;
+    }
+  }
+}
+
+
+std::vector<reg_t> NoiseModel::readout_error_keys() const {
+  // If ideal readout return an empty vector
+  if (ideal_readout())
+    return std::vector<reg_t>();
+
+  // Otherwise construct the vector
+  std::vector<reg_t> ret;
+  ret.reserve(readout_error_table_.size());
+  for (const auto& pair: readout_error_table_) {
+    ret.push_back(string2reg(pair.first));
+  }
+  return ret;
+}
+
+
+std::vector<NoiseModel::keytuple_t>
+NoiseModel::quantum_error_keys() const {
+  // If ideal readout return an empty vector
+  if (ideal_quantum())
+    return std::vector<keytuple_t>();
+
+  // Otherwise construct the vector
+  std::vector<keytuple_t> ret;
+  ret.reserve(local_quantum_error_table_.size() + nonlocal_quantum_error_table_.size());
+
+  // Add local quantum errors
+  for (const auto& outer_pair: local_quantum_error_table_) {
+    auto label = outer_pair.first;
+    for (const auto& inner_pair : outer_pair.second) {
+      auto qubits = string2reg(inner_pair.first);
+      ret.push_back(keytuple_t({label, qubits, reg_t()}));
+    }
+  }
+
+  // Add nonlocal quantum errors
+  for (const auto& outer_pair: nonlocal_quantum_error_table_) {
+    auto label = outer_pair.first;
+    for (const auto& middle_pair : outer_pair.second) {
+      const reg_t qubits = string2reg(middle_pair.first);
+      for (const auto& inner_pair : middle_pair.second) {
+        const reg_t noise_qubits = string2reg(inner_pair.first);
+        ret.push_back(keytuple_t({label, qubits, noise_qubits}));
+      } 
+    }
+  }
+  return ret;
 }
 
 //=========================================================================
