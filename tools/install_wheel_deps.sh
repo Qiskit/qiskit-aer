7efb1d8df087499fde50c027aab8a0a7b3fd5c2c
Matthew Treinish <mtreinish@kortar.org>
2019-08-29 07:52:19 -0400

    Use latest openblas

--- tools/install_wheel_deps.sh
+++ tools/install_wheel_deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -OL http://github.com/xianyi/OpenBLAS/archive/v0.2.20.tar.gz
-tar xzf v0.2.20.tar.gz
-pushd OpenBLAS-0.2.20
+curl -OL http://github.com/xianyi/OpenBLAS/archive/v0.3.7.tar.gz
+tar xzf v0.3.7.tar.gz
+pushd OpenBLAS-0.3.7
 make
 make PREFIX=/usr/local install
 popd
