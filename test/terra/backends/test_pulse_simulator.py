1e69f7ede98cb9c8d80df1953e56bac843ec0431
Zachary Schoenfeld <zach.schoenfeld33@gmail.com>
2019-10-11 17:22:02 -0400

    Incorporate feedback; test frame change

--- test/terra/backends/test_pulse_simulator.py
+++ test/terra/backends/test_pulse_simulator.py
@@ -25,95 +25,149 @@ import qiskit.pulse as pulse
 from qiskit.compiler import assemble
 
 from qiskit.test.mock import FakeOpenPulse2Q
-from qiskit.pulse.commands import SamplePulse
+from qiskit.pulse.commands import SamplePulse, FrameChange
 
 class TestPulseSimulator(common.QiskitAerTestCase):
     """ PulseSimulator tests.
 
-    Uses Hamiltonian H = -0.5*omega0*sigmaz + omega1*os(omega*t+phi)*sigmax,
-    as it is analytically solvable. Action of the gates is computed by performing
-    the rotating frame transformation R = e^(-i*omega*t*sigmaz/2). The Hamiltonian in the
-    rotating frame, applying the Rotating Wave Approximation (RWA), is
-    Hrot = 0.5*(omega-omega0)*sigmaz + 0.5*omega1*cos(phi)*sigmax - 0.5*omega1*sin(phi)*sigmay
-    (note time independence). The unitary evolution is then given by Urot = e^(-i*Hrot*t).
+    # pylint: disable=anomalous backslash in string
+    Uses Hamiltonian `H = -0.5*\omega_0*\sigma_z + \omega_1*\cos(\omega*t+\phi)*\sigma_x`,
+    as it has a closed form solution under the rotating frame transformation. Described in readme.
     """
 
     def setUp(self):
         """ Set configuration settings for pulse simulator """
         qiskit.IBMQ.load_account()
 
-        #Get a pulse configuration from the fake backend
-        self.backend_fake = FakeOpenPulse2Q()
-        self.back_config = self.backend_fake.configuration()
-        self.back_config_dict = self.back_config.to_dict()
-        self.system = pulse.PulseChannelSpec.from_backend(self.backend_fake)
-        self.defaults = self.backend_fake.defaults()
+        #Get a pulse configuration from the mock real device
+        self.backend_real = FakeOpenPulse2Q()
+        self.back_config = self.backend_real.configuration().to_dict()
+        self.system = pulse.PulseChannelSpec.from_backend(self.backend_real)
+        self.defaults = self.backend_real.defaults()
 
-        self.qubit = 0 # use 0 qubit for tests
-        self.freq_qubit = self.defaults.qubit_freq_est[self.qubit]
+        # define the qubits
+        self.qubit_0 = 0
+        self.freq_qubit_0 = self.defaults.qubit_freq_est[self.qubit_0]
+
+        self.qubit_1 = 1
+        self.freq_qubit_1 = self.defaults.qubit_freq_est[self.qubit_1]
+
+        # define the measurement map (so can measure the qubits seperately)
+        self.meas_map = [[self.qubit_0], [self.qubit_1]]
+
+        # define the pulse time (# of samples)
+        self.drive_samples = 100
+
+         # Define acquisition
+        acq_cmd = pulse.Acquire(duration=self.drive_samples)
+        self.acq_0 = acq_cmd(self.system.acquires[self.qubit_0],
+                             self.system.memoryslots[self.qubit_0])
 
         #Get pulse simulator backend
         self.backend_sim = qiskit.Aer.get_backend('pulse_simulator')
 
-        self.drive_samples = 100
-
-    def create_qobj(self, shots, omega0, omega1, omega, phi, meas_level):
-        """ Creates qobj for the specified pulse experiment
-
+    def single_pulse_schedule(self, phi):
+        """ Creates schedule for single pulse experiment
         Args:
-            See Hamiltonian at top of file
-            shots (int): number of times to perform experiment
-            omega0 (float): qubit frequency
-            omega1 (float): drive power/amplitude
-            omega (float): drive frequency
-            phi (float): drive phase
-            meas_level (int): how to return the data
-
+            phi (float): drive phase (phi in Hamiltonian)
         Returns:
-            Qobj: qobj representing this pulse experiment
+            schedule (pulse schedule): schedule for this experiment
         """
-
-        # Acquire pulse
-        acq_cmd = pulse.Acquire(duration=self.drive_samples)
-        acquire = acq_cmd(self.system.acquires, self.system.memoryslots)
-
-        # Create schedule
-
-        # drive pulse (just phase; omega1 included in Hamiltonian)
+         # drive pulse (just phase; omega1 included in Hamiltonian)
         const_pulse = np.ones(self.drive_samples)
         phase = np.exp(1j*phi)
         drive_pulse = SamplePulse(phase*const_pulse, name='drive_pulse')
 
         # add commands to schedule
         schedule = pulse.Schedule(name='drive_pulse')
-        schedule += drive_pulse(self.system.qubits[self.qubit].drive)
-        schedule += acquire << schedule.duration
+        schedule |= drive_pulse(self.system.qubits[self.qubit_0].drive)
+        schedule |= self.acq_0 << schedule.duration
 
+        return schedule
+
+    def frame_change_schedule(self, phi, fc_phi, fc_dur1, fc_dur2):
+        """ Creates schedule for frame change experiment. Does a pulse w/ phase phi of duration dur1,
+        then frame change of phase fc_phi, then another pulse of phase phi of duration dur2.
+        The different durations for the pulses allow manipulation of rotation angles on Bloch sphere
+        Args:
+            phi (float): drive phase (phi in Hamiltonian)
+            fc_phi (float): phase for frame change
+            fc_dur1 (int): duration of first pulse
+            fc_dur2 (int): duration of second pulse
+
+        Returns:
+            schedule (pulse schedule): schedule for frame change experiment
+        """
+         # drive pulse (just phase; omega1 included in Hamiltonian)
+        phase = np.exp(1j*phi)
+        drive_pulse_1 = SamplePulse(phase*np.ones(fc_dur1), name='drive_pulse_1')
+        drive_pulse_2 = SamplePulse(phase*np.ones(fc_dur2), name='drive_pulse_2')
+
+        # frame change
+        fc_pulse = FrameChange(phase=fc_phi, name='fc')
+
+        # add commands to schedule
+        schedule = pulse.Schedule(name='drive_pulse')
+        schedule |= drive_pulse_1(self.system.qubits[self.qubit_0].drive)
+        schedule += fc_pulse(self.system.qubits[self.qubit_0].drive)
+        schedule += drive_pulse_2(self.system.qubits[self.qubit_0].drive)
+        schedule |= self.acq_0 << schedule.duration
+
+        return schedule
+
+    def create_qobj(self, shots, omega0, omega1, omega, phi, meas_level, schedule_type=None,
+                    fc_phi=0, fc_dur1=0, fc_dur2=0):
+        """ Creates qobj for the specified pulse experiment. Uses Hamiltonian above.
+        Args:
+            shots (int): number of times to perform experiment
+            omega0 (float): qubit frequency
+            omega1 (float): drive power/amplitude
+            omega (float): drive frequency
+            phi (float): drive phase
+            meas_level (int): how to return the data
+            schedule_type (str): type of schedule. Default is single pulse, can also set to
+            "frame_change" or (fill in)
+            fc_phi (float): frame change phase
+            fc_dur1 (int): duration of first pulse in fc experiment
+            fc_dur2 (int): duration of second pulse in fc experiment
+        Returns:
+            Qobj: qobj representing this pulse experiment
+        """
+        # Create schedule (default is single pulse schedule)
+        schedule = self.single_pulse_schedule(phi=phi)
+
+        if schedule_type == "frame_change":
+            schedule = self.frame_change_schedule(phi=phi, fc_phi=fc_phi,
+                                                  fc_dur1=fc_dur1, fc_dur2=fc_dur2)
         # Create the hamiltonian
         hamiltonian = {}
         hamiltonian['h_str'] = []
 
-        #Q0 terms
+        # Q0 terms
         hamiltonian['h_str'].append('-0.5*omega0*Z0')
         hamiltonian['h_str'].append('0.5*omega1*X0||D0')
 
+        # Q1 terms
+        # none for now
+
+        # Set variables in ham
         hamiltonian['vars'] = {'omega0': omega0, 'omega1': omega1}
 
         # set the qubit dimension to 2
         hamiltonian['qub'] = {'0': 2}
 
         # update the back_end
-        self.back_config_dict['hamiltonian'] = hamiltonian
-        self.back_config_dict['noise'] = {}
-        self.back_config_dict['dt'] = 1.0
+        self.back_config['hamiltonian'] = hamiltonian
+        self.back_config['noise'] = {}
+        self.back_config['dt'] = 1.0
 
-        self.back_config_dict['ode_options'] = {} # optionally set ode settings
-        self.back_config_dict['qubit_list'] = [0] # restrict qubit set
+        self.back_config['ode_options'] = {} # optionally set ode settings
+        self.back_config['qubit_list'] = [self.qubit_0] # restrict qubit set to 0
 
-        qobj = assemble([schedule], self.backend_fake,
+        qobj = assemble([schedule], self.backend_real,
                         meas_level=meas_level, meas_return='single',
-                        memory_slots=1, qubit_lo_freq=[omega/(2*np.pi)],
-                        shots=shots, sim_config=self.back_config_dict)
+                        meas_map=self.meas_map, qubit_lo_freq=[omega/(2*np.pi)],
+                        memory_slots=1, shots=shots, sim_config=self.back_config)
 
         return qobj
 
@@ -127,7 +181,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         # set variables
 
         # set omega0, omega equal (use qubit frequency) -> drive on resonance
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0
 
         # Require omega1*time = pi to implement pi pulse (x gate)
@@ -141,7 +195,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         result = self.backend_sim.run(x_qobj).result()
         counts = result.get_counts()
 
-        exp_result = {'10':256} # Bitstring format of the 1 state
+        exp_result = {'1':256}
 
         self.assertDictAlmostEqual(counts, exp_result)
 
@@ -153,7 +207,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         # set variables
 
         # set omega0, omega equal (use qubit frequency) -> drive on resonance
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0
 
         # Require omega1*time = pi/2 to implement pi/2 rotation pulse
@@ -168,24 +222,22 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         result = self.backend_sim.run(had_qobj).result()
         counts = result.get_counts()
 
-        # compare proportions
-        prop = {}
+        # compare probs
+        prob = {}
         for key in counts.keys():
-            prop[key] = counts[key]/shots
+            prob[key] = counts[key]/shots
 
-        exp_prop = {'0':0.5, '10':0.5}
+        exp_prob = {'0':0.5, '1':0.5}
 
-        self.assertDictAlmostEqual(prop, exp_prop, delta=0.01)
+        self.assertDictAlmostEqual(prob, exp_prob, delta=0.01)
 
     def test_arbitrary_gate(self):
-        """ Test a few examples w/ arbitary drive, phase and amplitude. Compare to result obtained
-        via the rotating Hamiltonian at top of file (Hrot).
-        """
+        """ Test a few examples w/ arbitary drive, phase and amplitude. """
 
         # Gate 1
 
         # set variables
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0+0.01
 
         omega1 = 2*np.pi/3/self.drive_samples
@@ -193,24 +245,24 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         phi = 5*np.pi/7
         shots = 10000000 # large number of shots so get good probabilities
 
-        # Run qobj and compare proportions to expected result
+        # Run qobj and compare probs to expected result
         qobj1 = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega,
                                  phi=phi, meas_level=2)
         result1 = self.backend_sim.run(qobj1).result()
         counts1 = result1.get_counts()
 
-        prop1 = {}
+        prob1 = {}
         for key in counts1.keys():
-            prop1[key] = counts1[key]/shots
+            prob1[key] = counts1[key]/shots
 
-        exp_prop1 = {'0':0.315253, '10':1-0.315253}
+        exp_prob1 = {'0':0.315253, '1':1-0.315253}
 
-        self.assertDictAlmostEqual(prop1, exp_prop1, delta=0.001)
+        self.assertDictAlmostEqual(prob1, exp_prob1, delta=0.001)
 
         # Gate 2
 
         # set variables
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0+0.02
 
         omega1 = 7*np.pi/5/self.drive_samples
@@ -218,24 +270,24 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         phi = 19*np.pi/14
         shots = 10000000 # large number of shots so get good probabilities
 
-        # Run qobj and compare proportions to expected result
+        # Run qobj and compare probs to expected result
         qobj2 = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega,
                                  phi=phi, meas_level=2)
         result2 = self.backend_sim.run(qobj2).result()
         counts2 = result2.get_counts()
 
-        prop2 = {}
+        prob2 = {}
         for key in counts2.keys():
-            prop2[key] = counts2[key]/shots
+            prob2[key] = counts2[key]/shots
 
-        exp_prop2 = {'0':0.634952, '10':1-0.634952}
+        exp_prob2 = {'0':0.634952, '1':1-0.634952}
 
-        self.assertDictAlmostEqual(prop2, exp_prop2, delta=0.001)
+        self.assertDictAlmostEqual(prob2, exp_prob2, delta=0.001)
 
         # Gate 3
 
         # set variables
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0+0.005
 
         omega1 = 0.1
@@ -243,19 +295,19 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         phi = np.pi/4
         shots = 10000000 # large number of shots so get good probabilities
 
-        # Run qobj and compare proportions to expected result
+        # Run qobj and compare probs to expected result
         qobj3 = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega,
                                  phi=phi, meas_level=2)
         result3 = self.backend_sim.run(qobj3).result()
         counts3 = result3.get_counts()
 
-        prop3 = {}
+        prob3 = {}
         for key in counts3.keys():
-            prop3[key] = counts3[key]/shots
+            prob3[key] = counts3[key]/shots
 
-        exp_prop3 = {'0':0.0861794, '10':1-0.0861794}
+        exp_prob3 = {'0':0.0861794, '1':1-0.0861794}
 
-        self.assertDictAlmostEqual(prop3, exp_prop3, delta=0.001)
+        self.assertDictAlmostEqual(prob3, exp_prob3, delta=0.001)
 
     # ---------------------------------------------------------------------
     # Test meas level 1
@@ -267,7 +319,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         # perform hadamard setup (so get some 0's and some 1's), but use meas_level = 1
 
          # set omega0, omega equal (use qubit frequency) -> drive on resonance
-        omega0 = 2*np.pi*self.freq_qubit
+        omega0 = 2*np.pi*self.freq_qubit_0
         omega = omega0
 
         # Require omega1*time = pi/2 to implement pi/2 rotation pulse
@@ -276,26 +328,80 @@ class TestPulseSimulator(common.QiskitAerTestCase):
 
         phi = -np.pi/2
 
-        shots = 100000 # run large number of shots for good proportions
+        shots = 100000 # run large number of shots for good probs
 
         qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
                                 meas_level=1)
         result = self.backend_sim.run(qobj).result()
 
         # Verify that (about) half the IQ vals have abs val 1 and half have abs val 0
-        # (use proportions for easier comparison)
-        mem = np.abs(result.get_memory()[:, self.qubit])
+        # (use probs for easier comparison)
+        mem = np.abs(result.get_memory()[:, self.qubit_0])
 
-        iq_prop = {'0': 0, '10': 0}
+        iq_prob = {'0': 0, '1': 0}
         for i in mem:
             if i == 0:
-                iq_prop['0'] += 1/shots
+                iq_prob['0'] += 1/shots
             else:
-                iq_prop['10'] += 1/shots
+                iq_prob['1'] += 1/shots
 
-        exp_prop = {'0': 0.5, '10': 0.5}
+        exp_prob = {'0': 0.5, '1': 0.5}
 
-        self.assertDictAlmostEqual(iq_prop, exp_prop, delta=0.01)
+        self.assertDictAlmostEqual(iq_prob, exp_prob, delta=0.01)
+
+    # ---------------------------------------------------------------------
+    # Test FrameChange and PersistentValue commands
+    # ---------------------------------------------------------------------
+
+    def test_frame_change(self):
+        """ Test frame change.
+        """
+        shots = 1000000
+        # set omega0, omega equal (use qubit frequency) -> drive on resonance
+        omega0 = 2*np.pi*self.freq_qubit_0
+        omega = omega0
+
+        # set phi = 0
+        phi = 0
+
+        # Test 1
+        # do pi/2 pulse, then pi phase change, then another pi/2 pulse. Verify left in |0> state
+        fc_dur1 = self.drive_samples
+        fc_dur2 = fc_dur1
+        omega1 = np.pi/2/fc_dur1
+
+        fc_phi = np.pi
+
+        fc_qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
+                                   meas_level=2, schedule_type="frame_change", fc_phi=fc_phi,
+                                   fc_dur1=fc_dur1, fc_dur2=fc_dur2)
+        result = self.backend_sim.run(fc_qobj).result()
+        counts = result.get_counts()
+        exp_result = {'0':shots}
+
+        self.assertDictAlmostEqual(counts, exp_result)
+
+        # Test 2
+        # do pi/4 pulse, then pi phase change, then do pi/8 pulse. Should get |0> w/ prob = 0.96194.
+        fc_dur1 = self.drive_samples
+        fc_dur2 = fc_dur1//2 # half time to do the pi/8 pulse (halves angle)
+        omega1 = np.pi/4/fc_dur1
+
+        fc_phi = np.pi
+
+        fc_qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
+                                   meas_level=2, schedule_type="frame_change", fc_phi=fc_phi,
+                                   fc_dur1=fc_dur1, fc_dur2=fc_dur2)
+        result = self.backend_sim.run(fc_qobj).result()
+        counts = result.get_counts()
+
+        # verify probs
+        prob = {}
+        for key in counts.keys():
+            prob[key] = counts[key]/shots
+
+        exp_prob = {'0':0.96194, '1':(1-0.96194)}
+        self.assertDictAlmostEqual(prob, exp_prob, delta=0.001)
 
 
 if __name__ == '__main__':
