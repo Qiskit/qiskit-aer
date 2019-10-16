b41d12013961c5405b7a9cca72b94fb1a328509f
Zachary Schoenfeld <zach.schoenfeld33@gmail.com>
2019-10-16 17:30:42 -0400

    Add PersistantValue tests

--- test/terra/backends/test_pulse_simulator.py
+++ test/terra/backends/test_pulse_simulator.py
@@ -25,7 +25,7 @@ import qiskit.pulse as pulse
 from qiskit.compiler import assemble
 
 from qiskit.test.mock import FakeOpenPulse2Q
-from qiskit.pulse.commands import SamplePulse, FrameChange
+from qiskit.pulse.commands import SamplePulse, FrameChange, PersistentValue
 
 class TestPulseSimulator(common.QiskitAerTestCase):
     """ PulseSimulator tests.
@@ -67,11 +67,11 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         self.backend_sim = qiskit.Aer.get_backend('pulse_simulator')
 
     def single_pulse_schedule(self, phi):
-        """ Creates schedule for single pulse experiment
+        """ Creates schedule for single pulse test
         Args:
             phi (float): drive phase (phi in Hamiltonian)
         Returns:
-            schedule (pulse schedule): schedule for this experiment
+            schedule (pulse schedule): schedule for this test
         """
          # drive pulse (just phase; omega1 included in Hamiltonian)
         const_pulse = np.ones(self.drive_samples)
@@ -86,7 +86,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         return schedule
 
     def frame_change_schedule(self, phi, fc_phi, fc_dur1, fc_dur2):
-        """ Creates schedule for frame change experiment. Does a pulse w/ phase phi of duration dur1,
+        """ Creates schedule for frame change test. Does a pulse w/ phase phi of duration dur1,
         then frame change of phase fc_phi, then another pulse of phase phi of duration dur2.
         The different durations for the pulses allow manipulation of rotation angles on Bloch sphere
         Args:
@@ -96,7 +96,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
             fc_dur2 (int): duration of second pulse
 
         Returns:
-            schedule (pulse schedule): schedule for frame change experiment
+            schedule (pulse schedule): schedule for frame change test
         """
          # drive pulse (just phase; omega1 included in Hamiltonian)
         phase = np.exp(1j*phi)
@@ -107,7 +107,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         fc_pulse = FrameChange(phase=fc_phi, name='fc')
 
         # add commands to schedule
-        schedule = pulse.Schedule(name='drive_pulse')
+        schedule = pulse.Schedule(name='fc_schedule')
         schedule |= drive_pulse_1(self.system.qubits[self.qubit_0].drive)
         schedule += fc_pulse(self.system.qubits[self.qubit_0].drive)
         schedule += drive_pulse_2(self.system.qubits[self.qubit_0].drive)
@@ -115,8 +115,35 @@ class TestPulseSimulator(common.QiskitAerTestCase):
 
         return schedule
 
+    def persistent_value_schedule(self, omega1_pv):
+        """ Creates schedule for persistent value experiment. Creates pv pulse w/ drive amplitude
+        omega1_pv. It does this by setting the omega1 term in the Hamiltonian = 1. Sets length of
+        the pv pulse = self.drive_samples. The product omega1_pv*self.drive_samples, then, controls
+        the resulting state.
+        Args:
+            omega1_pv (float): drive amplitude from the pv pulse
+
+        Returns:
+            schedule (pulse schedule): schedule for pv experiment
+        """
+
+        # pv pulse
+        pv_pulse = PersistentValue(value=omega1_pv, name='pv')
+
+        # 0 amp drive pulse
+        #drive_pulse = SamplePulse(np.ones(self.drive_samples), name='drive_pulse_0amp')
+
+        # add commands to schedule
+        schedule = pulse.Schedule(name='pv_schedule')
+        schedule |= pv_pulse(self.system.qubits[self.qubit_0].drive)
+        # make pv_pulse last exactly self.drive_samples
+        #schedule += drive_pulse(self.system.qubits[self.qubit_0].drive) << self.drive_samples
+        schedule |= self.acq_0 << self.drive_samples
+
+        return schedule
+
     def create_qobj(self, shots, omega0, omega1, omega, phi, meas_level, schedule_type=None,
-                    fc_phi=0, fc_dur1=0, fc_dur2=0):
+                    fc_phi=0, fc_dur1=0, fc_dur2=0, omega1_pv=0, qub_dim=2):
         """ Creates qobj for the specified pulse experiment. Uses Hamiltonian above.
         Args:
             shots (int): number of times to perform experiment
@@ -126,19 +153,26 @@ class TestPulseSimulator(common.QiskitAerTestCase):
             phi (float): drive phase
             meas_level (int): how to return the data
             schedule_type (str): type of schedule. Default is single pulse, can also set to
-            "frame_change" or (fill in)
-            fc_phi (float): frame change phase
-            fc_dur1 (int): duration of first pulse in fc experiment
-            fc_dur2 (int): duration of second pulse in fc experiment
+            'fc' or 'pv'
+            fc_phi (float): frame change phase (set to 0 unless fc experiment)
+            fc_dur1 (int): duration of first pulse in fc experiment (set to 0 unless fc experiment)
+            fc_dur2 (int): duration of second pulse in fc experiment (set to 0 unless fc experiment)
+            omega1_pv (float): drive amplitude in pv_experiment (set to 0 unless pv
+            experiment)
+            qub_dim (int): dimension of the qubit subspace (defaults to 2)
         Returns:
             Qobj: qobj representing this pulse experiment
         """
         # Create schedule (default is single pulse schedule)
         schedule = self.single_pulse_schedule(phi=phi)
 
-        if schedule_type == "frame_change":
+        if schedule_type == 'fc':
             schedule = self.frame_change_schedule(phi=phi, fc_phi=fc_phi,
                                                   fc_dur1=fc_dur1, fc_dur2=fc_dur2)
+        elif schedule_type == 'pv':
+            omega1 = 1 # make sure omega1 is equal to 1 (so that drive amp is omega1_pv)
+            schedule = self.persistent_value_schedule(omega1_pv=omega1_pv)
+
         # Create the hamiltonian
         hamiltonian = {}
         hamiltonian['h_str'] = []
@@ -153,13 +187,13 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         # Set variables in ham
         hamiltonian['vars'] = {'omega0': omega0, 'omega1': omega1}
 
-        # set the qubit dimension to 2
-        hamiltonian['qub'] = {'0': 2}
+        # set the qubit dimension to qub_dim
+        hamiltonian['qub'] = {'0': qub_dim}
 
         # update the back_end
         self.back_config['hamiltonian'] = hamiltonian
         self.back_config['noise'] = {}
-        self.back_config['dt'] = 1.0
+        self.back_config['dt'] = 1.0 # makes time = self.drive_samples
 
         self.back_config['ode_options'] = {} # optionally set ode settings
         self.back_config['qubit_list'] = [self.qubit_0] # restrict qubit set to 0
@@ -354,8 +388,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
     # ---------------------------------------------------------------------
 
     def test_frame_change(self):
-        """ Test frame change.
-        """
+        """ Test frame change command. """
         shots = 1000000
         # set omega0, omega equal (use qubit frequency) -> drive on resonance
         omega0 = 2*np.pi*self.freq_qubit_0
@@ -373,7 +406,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         fc_phi = np.pi
 
         fc_qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
-                                   meas_level=2, schedule_type="frame_change", fc_phi=fc_phi,
+                                   meas_level=2, schedule_type='fc', fc_phi=fc_phi,
                                    fc_dur1=fc_dur1, fc_dur2=fc_dur2)
         result = self.backend_sim.run(fc_qobj).result()
         counts = result.get_counts()
@@ -390,7 +423,7 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         fc_phi = np.pi
 
         fc_qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
-                                   meas_level=2, schedule_type="frame_change", fc_phi=fc_phi,
+                                   meas_level=2, schedule_type='fc', fc_phi=fc_phi,
                                    fc_dur1=fc_dur1, fc_dur2=fc_dur2)
         result = self.backend_sim.run(fc_qobj).result()
         counts = result.get_counts()
@@ -403,6 +436,29 @@ class TestPulseSimulator(common.QiskitAerTestCase):
         exp_prob = {'0':0.96194, '1':(1-0.96194)}
         self.assertDictAlmostEqual(prob, exp_prob, delta=0.001)
 
+    def test_persistent_value(self):
+        """ Test persistent value command. """
+
+        shots = 256
+        # set omega0, omega equal (use qubit frequency) -> drive on resonance
+        omega0 = 2*np.pi*self.freq_qubit_0
+        omega = omega0
+
+        # set phi = 0 (so don't need to account for this in pv amplitude)
+        phi = 0
+
+        # Set omega1 = 1 and do pi pulse w/ omega1_pv. Verify result is the |1> state
+        omega1 = 1
+        omega1_pv = np.pi/self.drive_samples # pi pulse
+
+        pv_qobj = self.create_qobj(shots=shots, omega0=omega0, omega1=omega1, omega=omega, phi=phi,
+                                   meas_level=2, schedule_type='pv',
+                                   omega1_pv=omega1_pv)
+        result = self.backend_sim.run(pv_qobj).result()
+        counts = result.get_counts()
+        exp_result = {'1':shots}
+
+        self.assertDictAlmostEqual(counts, exp_result)
 
 if __name__ == '__main__':
     unittest.main()
