b57f924f540098c423f67083daf730cf371ca9af
Christopher Wood <cjwood@us.ibm.com>
2019-10-18 11:21:03 -0400

    Add non-json types for additional data

--- src/framework/results/experiment_data.hpp
+++ src/framework/results/experiment_data.hpp
@@ -80,18 +80,30 @@ class ExperimentData {
 
   // Add new data at the specified key.
   // If they key already exists this will override the stored data
+  // This will use the json conversion method `to_json` for data type T
+  // if it isn't a natively supported data type.
+  // Current native types are: cvector_t, cmatrix_t.
   template <typename T>
-  void add_additional_data(const std::string &key, const T &data);
+  void add_additional_data(const std::string &key, T &&data);
+
+  // Erase additional data stored at the provided key.
+  // This will erase from all additional data containers
+  void erase_additional_data(const std::string &key);
 
   //----------------------------------------------------------------
   // Metadata
   //----------------------------------------------------------------
 
+  // Access metadata map
   stringmap_t<json_t> &metadata() { return metadata_; }
   const stringmap_t<json_t> &metadata() const { return metadata_; }
 
+  // Add new data to metadata at the specified key.
+  // This will use the json conversion method `to_json` for data type T.
+  // If they key already exists this will update the current data
+  // with the new data.
   template <typename T>
-  void add_metadata(const std::string &key, const T &data);
+  void add_metadata(const std::string &key, T &&data);
 
   //----------------------------------------------------------------
   // Config
@@ -161,6 +173,15 @@ class ExperimentData {
   // Miscelaneous data
   stringmap_t<json_t> additional_json_data_;
 
+  // Complex vector data
+  stringmap_t<cvector_t> additional_cvector_data_;
+
+  // Complex matrix data
+  stringmap_t<cmatrix_t> additional_cmatrix_data_;
+
+  // Check if key name is reserved and if so throw an exception
+  void check_reserved_key(const std::string &key);
+
   //----------------------------------------------------------------
   // Metadata
   //----------------------------------------------------------------
@@ -190,6 +211,10 @@ void ExperimentData::set_config(const json_t &config) {
   JSON::get_value(return_register_, "register", config);
   JSON::get_value(return_snapshots_, "snapshots", config);
 }
+
+//------------------------------------------------------------------
+// Classical data
+//------------------------------------------------------------------
 
 void ExperimentData::add_memory_count(const std::string &memory) {
   // Memory bits value
@@ -245,48 +270,101 @@ void ExperimentData::add_average_snapshot(const std::string &type,
 // Additional Data
 //------------------------------------------------------------------
 
-const stringset_t ExperimentData::reserved_keys_ = {
-    "counts", "memory", "register", "snapshots"};
+const stringset_t ExperimentData::reserved_keys_ = {"counts", "memory",
+                                                    "register", "snapshots"};
 
-template <typename T>
-void ExperimentData::add_additional_data(const std::string &key,
-                                         const T &data) {
-  // Check key isn't one of the reserved keys
+void ExperimentData::check_reserved_key(const std::string &key) {
   if (reserved_keys_.find(key) != reserved_keys_.end()) {
     throw std::invalid_argument(
         "Cannot add additional data with reserved key name \"" + key + "\".");
   }
-  if (return_additional_data_) {
-    // Use implicit to_json conversion function for T
-    json_t jdata = data;
-    additional_json_data_[key] = std::move(jdata);
-  }
+}
+
+void ExperimentData::erase_additional_data(const std::string &key) {
+  additional_json_data_.erase(key);
+  additional_cvector_data_.erase(key);
+  additional_cmatrix_data_.erase(key);
 }
 
 template <typename T>
-void ExperimentData::add_metadata(const std::string &key, const T &data) {
-  // Use implicit to_json conversion function for T
-  json_t jdata = data;
-  auto elt = metadata_.find("key");
-  if (elt == metadata_.end()) {
-    // If key doesn't already exist add new data
-    metadata_[key] = std::move(jdata);
-  } else {
-    // If key already exists append with additional data
-    elt->second.update(jdata.begin(), jdata.end());
+void ExperimentData::add_additional_data(const std::string &key, T &&data) {
+  check_reserved_key(key);
+  if (return_additional_data_) {
+    json_t jdata = data;
+    add_additional_data(key, std::move(jdata));
   }
 }
 
+template <>
+void ExperimentData::add_additional_data(const std::string &key, json_t &&data) {
+  check_reserved_key(key);
+  if (return_additional_data_) {
+    erase_additional_data(key);
+    additional_json_data_[key] = data;
+  }
+}
+
+template <>
+void ExperimentData::add_additional_data(const std::string &key,
+                                         cvector_t &&data) {
+  check_reserved_key(key);
+  if (return_additional_data_) {
+    erase_additional_data(key);
+    additional_cvector_data_[key] = data;
+  }
+}
+
+template <>
+void ExperimentData::add_additional_data(const std::string &key,
+                                         cmatrix_t &&data) {
+  check_reserved_key(key);
+  if (return_additional_data_) {
+    erase_additional_data(key);
+    additional_cmatrix_data_[key] = data;
+  }
+}
+
+//------------------------------------------------------------------
+// Metadata
+//------------------------------------------------------------------
+
+template <typename T>
+void ExperimentData::add_metadata(const std::string &key, T &&data) {
+  // Use implicit to_json conversion function for T
+  json_t jdata = data;
+  add_metadata(key, std::move(jdata));
+}
+
+template<>
+void ExperimentData::add_metadata(const std::string &key, json_t &&data) {
+  auto elt = metadata_.find("key");
+  if (elt == metadata_.end()) {
+    // If key doesn't already exist add new data
+    metadata_[key] = data;
+  } else {
+    // If key already exists append with additional data
+    elt->second.update(data.begin(), data.end());
+  }
+}
+
+//------------------------------------------------------------------
+// Clear and combine
+//------------------------------------------------------------------
+
 void ExperimentData::clear() {
-  // Clear measure and counts
+  // Clear measurement data
   counts_.clear();
   memory_.clear();
   register_.clear();
-  // Clear snapshots
+  // Clear pershot snapshots
   pershot_json_snapshots_.clear();
+  // Clear average snapshots
   average_json_snapshots_.clear();
   // Clear additional data
   additional_json_data_.clear();
+  additional_cvector_data_.clear();
+  additional_cmatrix_data_.clear();
+  // Clear metadata
   metadata_.clear();
 }
 
@@ -315,27 +393,53 @@ ExperimentData &ExperimentData::combine(ExperimentData &data) {
   // Combine additional data
   for (auto &pair : data.additional_json_data_) {
     const auto &key = pair.first;
-    // erase_duplicate_data_keys(key, "json");
-    additional_json_data_[key] = pair.second;
+    erase_additional_data(key);
+    additional_json_data_[key] = std::move(pair.second);
   }
-
+  for (auto &pair : data.additional_cvector_data_) {
+    const auto &key = pair.first;
+    erase_additional_data(key);
+    additional_cvector_data_[key] = std::move(pair.second);
+  }
+  for (auto &pair : data.additional_cmatrix_data_) {
+    const auto &key = pair.first;
+    erase_additional_data(key);
+    additional_cmatrix_data_[key] = std::move(pair.second);
+  }
   // Clear any remaining data from other container
   data.clear();
 
   return *this;
 }
 
+//------------------------------------------------------------------------------
+// JSON serialization
+//------------------------------------------------------------------------------
+
 json_t ExperimentData::json() const {
   // Initialize output as additional data JSON
-  json_t tmp = additional_json_data_;
+  json_t tmp;
 
   // Measure data
   if (return_counts_ && counts_.empty() == false) tmp["counts"] = counts_;
   if (return_memory_ && memory_.empty() == false) tmp["memory"] = memory_;
   if (return_register_ && register_.empty() == false)
     tmp["register"] = register_;
-  // Average snapshot data
+
+  // Add additional data
+  for (const auto &pair : additional_json_data_) {
+    tmp[pair.first] = pair.second;
+  }
+  for (const auto &pair : additional_cvector_data_) {
+    tmp[pair.first] = pair.second;
+  }
+  for (const auto &pair : additional_cmatrix_data_) {
+    tmp[pair.first] = pair.second;
+  }
+
+  // Snapshot data
   if (return_snapshots_) {
+    // Average snapshots
     for (auto &pair : average_json_snapshots_) {
       tmp["snapshots"][pair.first] = pair.second;
     }
@@ -351,9 +455,6 @@ json_t ExperimentData::json() const {
   return tmp;
 }
 
-//------------------------------------------------------------------------------
-// Implicit JSON conversion function
-//------------------------------------------------------------------------------
 inline void to_json(json_t &js, const ExperimentData &data) {
   js = data.json();
 }
