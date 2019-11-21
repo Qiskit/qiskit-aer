280b606dafc3d3ef084126f51f5296b3c9d90a61
Eric Eastman <ericeastman@ibm.com>
2019-11-21 15:25:48 -0500

    Fixing pylint errors

--- qiskit/providers/aer/backends/aerbackend.py
+++ qiskit/providers/aer/backends/aerbackend.py
@@ -28,7 +28,6 @@ from qiskit.qobj import validate_qobj_against_schema
 from qiskit.result import Result
 from qiskit.util import local_hardware_info
 
-from ..utils.helpers import deprecation
 from ..aerjob import AerJob
 from ..aererror import AerError
 
