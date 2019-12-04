41d487ea941d89ece60808512f8708ad0e805344
Christian Clauss <cclauss@me.com>
2019-12-04 05:49:43 +0100

    setup.py: Add Python 3.7 and 3.8 to classifiers

--- setup.py
+++ setup.py
@@ -49,6 +49,8 @@ setup(
         "Programming Language :: C++",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering",
     ],
     install_requires=requirements,
