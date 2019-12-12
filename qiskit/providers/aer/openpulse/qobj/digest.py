6be326101c0ec0543808a849c26a9cd644814387
Christopher Wood <cjwood@us.ibm.com>
2019-12-12 17:30:19 -0500

    fix another estates transpose bug

--- qiskit/providers/aer/openpulse/qobj/digest.py
+++ qiskit/providers/aer/openpulse/qobj/digest.py
@@ -109,9 +109,9 @@ def digest_pulse_obj(qobj_input, backend_options, noise_model):
     out.estates = ham_model._estates
     dim_qub = ham_model._dim_qub
     dim_osc = ham_model._dim_osc
-    out.initial_state = op.Qobj(ham_model._estates.T[0])
     # convert estates into a Qutip qobj
-    estates = [op.state(state) for state in ham_model._estates[:]]
+    estates = [op.state(state) for state in ham_model._estates.T]
+    out.initial_state = estates[0]
     out.global_data['vars'] = list(out.vars.values())
     out.global_data['freqs'] = list(out.freqs.values())
 
