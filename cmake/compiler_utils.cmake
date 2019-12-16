4a79579b669f28da285feff6cd4f2570c0d9fc49
hhorii <hhorii@users.noreply.github.com>
2019-12-16 19:25:08 +0200

    Fix build error in gpu (#491)
    * Fix build bugs in gpu branch with standalone simulator
    * Support Clang 11 on Mac for fake gpu
    * Refactor CMake to make Thrust support simpler

--- cmake/compiler_utils.cmake
+++ cmake/compiler_utils.cmake
@@ -1,3 +1,15 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 include(CheckCXXCompilerFlag)
 function(enable_cxx_compiler_flag_if_supported flag)
     string(FIND "${CMAKE_CXX_FLAGS}" "${flag}" flag_already_set)
@@ -56,4 +68,14 @@ function(get_muparserx_source_code)
                 Please download MuparserX library from https://github.com/beltoforion/muparserx.git \
                 and checkout latest stable release")
     endif()
+endfunction()
+
+function(get_clang_version version_str)
+    string(REPLACE "." ";" VERSION_LIST ${version_str})
+    list(GET VERSION_LIST 0 TMP_MAJOR_VERSION)
+    list(GET VERSION_LIST 1 TMP_MINOR_VERSION)
+    list(GET VERSION_LIST 2 TMP_PATCH_VERSION)
+    set(CLANG_MAJOR_VERSION ${TMP_MAJOR_VERSION} PARENT_SCOPE)
+    set(CLANG_MINOR_VERSION ${TMP_MINOR_VERSION} PARENT_SCOPE)
+    set(CLANG_PATCH_VERSION ${TMP_PATCH_VERSION} PARENT_SCOPE)
 endfunction()
