af01f704abcda7cf35a9062a3bdded03630435c9
Eric Eastman <ereastman96@gmail.com>
2019-12-17 15:39:46 -0500

    Separate json headers from pybind headers to remove standalone dependency on pybind

--- qiskit/providers/aer/backends/wrappers/qasm_bindings.cc
+++ qiskit/providers/aer/backends/wrappers/qasm_bindings.cc
@@ -1,9 +1,10 @@
+#include <iostream>
+
 #include <pybind11/pybind11.h>
 
 #include "simulators/qasm/qasm_controller.hpp"
 #include "simulators/controller_execute.hpp"
-#include <iostream>
-#include "framework/json.hpp"
+#include "framework/pybind_json.hpp"
 
 PYBIND11_MODULE(qasm_controller_wrapper, m) {
     m.def("qasm_controller_execute_json", &AER::controller_execute_json<AER::Simulator::QasmController>, "instance of controller_execute for QasmController");
--- qiskit/providers/aer/backends/wrappers/unitary_bindings.cc
+++ qiskit/providers/aer/backends/wrappers/unitary_bindings.cc
@@ -2,8 +2,7 @@
 
 #include "simulators/unitary/unitary_controller.hpp"
 #include "simulators/controller_execute.hpp"
-
-#include "framework/json.hpp"
+#include "framework/pybind_json.hpp"
 
 PYBIND11_MODULE(unitary_controller_wrapper, m) {
     m.def("unitary_controller_execute_json", &AER::controller_execute_json<AER::Simulator::UnitaryController>, "instance of controller_execute for UnitaryController");
