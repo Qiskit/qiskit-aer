47ddb6c99cf674acc1f754991b56c81be876b551
Eric Eastman <ereastman96@gmail.com>
2019-12-23 21:59:15 -0500

    Added macro to check MSVC version and potentially undef snprintf

--- src/framework/pybind_json.hpp
+++ src/framework/pybind_json.hpp
@@ -15,6 +15,12 @@
 #ifndef _aer_framework_pybind_json_hpp_
 #define _aer_framework_pybind_json_hpp_
 
+#if defined(_MSC_VER) && _MSC_VER < 1500 // VC++ 8.0 and below
+#define snprintf _snprintf
+#else
+#undef snprintf
+#endif
+
 #include <complex>
 #include <cstdint>
 #include <fstream>
