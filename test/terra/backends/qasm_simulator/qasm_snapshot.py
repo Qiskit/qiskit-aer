15fb3096415cd52721420283dd997a1c3fdcd92c
Hiroshi Horii <horii@jp.ibm.com>
2020-01-13 22:51:28 +0900

    Merge recent master changes

--- test/terra/backends/qasm_simulator/qasm_snapshot.py
+++ test/terra/backends/qasm_simulator/qasm_snapshot.py
@@ -511,8 +511,8 @@ class QasmSnapshotProbabilitiesTests:
 
     SIMULATOR = QasmSimulator()
     SUPPORTED_QASM_METHODS = [
-        'automatic', 'statevector', 'statevector_gpu', 'statevector_fake_gpu',
-        'density_matrix', 'matrix_product_state'
+        'automatic', 'statevector', 'stabilizer', 'density_matrix', 'matrix_product_state',
+        'statevector_gpu', 'statevector_fake_gpu'
     ]
     BACKEND_OPTS = {}
 
