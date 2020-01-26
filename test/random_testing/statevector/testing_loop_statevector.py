b33430654907e92368b74f987b1f766d8a7b8716
Merav Aharoni <merav@il.ibm.com>
2020-01-26 11:07:25 +0200

    removed random_test directory that was commited by mistake

--- test/random_testing/statevector/testing_loop_statevector.py
+++
@@ -1,9 +0,0 @@
-from random_statevector_test import random_statevector_test
-
-max_num_qubits = 22
-max_num_gates = 200
-for num_qubits in range(2, max_num_qubits+1, 2):
-    for num_gates in range(10, max_num_gates+1, 20):
-        random_statevector_test(num_qubits, num_gates)
-
-
