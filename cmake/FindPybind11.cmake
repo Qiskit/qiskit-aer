6f0ef570dd5f9d16f2aa5e7e61012bc26098ef69
Juan Gomez <atilag@gmail.com>
2020-01-28 16:52:09 +0100

    Issue 524 - Fix OpenMP on Linux (and Windows) (#550)
    * Fix OpenMP on Linux (and Windows)
    * Add correct Linux version of libmuparserx.a
    * Fix Cython building on Mac
    * Fix building python extension on Linux
    * Better static linking
    * Windows build don't need DL library

--- cmake/FindPybind11.cmake
+++ cmake/FindPybind11.cmake
@@ -44,14 +44,7 @@ function(basic_pybind11_add_module target_name)
 
     # This sets various properties (python include dirs) and links to python libs
     target_include_directories(${target_name} PRIVATE ${PYTHON_INCLUDE_DIRS})
-    set_target_properties(${target_name} PROPERTIES PREFIX "${PYTHON_MODULE_PREFIX}")
-    set_target_properties(${target_name} PROPERTIES SUFFIX "${PYTHON_EXTENSION_MODULE_SUFFIX}")
-
     target_include_directories(${target_name} PRIVATE ${PYBIND_INCLUDE_DIRS})
-    set_target_properties(${target_name} PROPERTIES LINKER_LANGUAGE CXX)
-    set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden")
-    set_target_properties(${target_name} PROPERTIES CUDA_VISIBILITY_PRESET "hidden")
-    set_target_properties(${target_name} PROPERTIES CXX_STANDARD 14)
 
     if(WIN32 OR CYGWIN)
         # Link against the Python shared library on Windows
@@ -64,7 +57,7 @@ function(basic_pybind11_add_module target_name)
         # into Blender or Maya later on, this will cause segfaults when multiple
         # conflicting Python instances are active at the same time (even when they
         # are of the same version).
- 
+
         # Windows is not affected by this issue since it handles DLL imports
         # differently. The solution for Linux and Mac OS is simple: we just don't
         # link against the Python library. The resulting shared library will have
@@ -80,9 +73,24 @@ function(basic_pybind11_add_module target_name)
             # See: Two-Leve namespace symbol resolution
             set(AER_LINKER_FLAGS "${AER_LINKER_FLAGS} -undefined dynamic_lookup -flat_namespace")
         endif()
-        set_target_properties(${target_name} PROPERTIES
-            LINK_FLAGS ${AER_LINKER_FLAGS}
-            COMPILE_FLAGS ${AER_COMPILE_FLAGS}
-            MACOSX_RPATH ON)
+        if(ARG_SHARED)
+            set_target_properties(${target_name} PROPERTIES MACOSX_RPATH ON)
+        endif()
     endif()
+    # -fvisibility=hidden is required to allow multiple modules compiled against
+    # different pybind versions to work properly, and for some features (e.g.
+    # py::module_local).  We force it on everything inside the `pybind11`
+    # namespace; also turning it on for a pybind module compilation here avoids
+    # potential warnings or issues from having mixed hidden/non-hidden types.
+    set_target_properties(${target_name} PROPERTIES PREFIX "${PYTHON_MODULE_PREFIX}")
+    set_target_properties(${target_name} PROPERTIES SUFFIX "${PYTHON_EXTENSION_MODULE_SUFFIX}")
+    set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden")
+    set_target_properties(${target_name} PROPERTIES CUDA_VISIBILITY_PRESET "hidden")
+    set_target_properties(${target_name} PROPERTIES
+        CXX_STANDARD 14
+        LINKER_LANGUAGE CXX)
+    # Warning: Do not merge PROPERTIES when one of the variables can be empty, it breaks
+    # the rest of the properties so they are not properly added.
+    set_target_properties(${target_name} PROPERTIES LINK_FLAGS ${AER_LINKER_FLAGS})
+    set_target_properties(${target_name} PROPERTIES COMPILE_FLAGS ${AER_COMPILER_FLAGS})
 endfunction()
