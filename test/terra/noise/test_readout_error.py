d20f5fe8e569e4a35cb6eb3bb9fe2bf57167d117
Christopher J. Wood <cjwood@us.ibm.com>
2020-01-30 18:29:27 -0500

    Update QuantumError and ReadoutError to mimic new BaseOperator API (#563)
    * Add dot method to QuantumError and ReadoutError
    * Add operator overloads for *, @, ^ for dot, compose, tensor to
      QuantumError and ReadoutError
    * Raise NotImplementedError for overloads +, -, /. scalar *, as these aren't
      supported for QuantumError, ReadoutError.
    * Deprecate front=True kwarg of compose for QuantumError, ReadoutError
    * Remove deprecated kron method from QuantumError

--- test/terra/noise/test_readout_error.py
+++ test/terra/noise/test_readout_error.py
@@ -14,17 +14,16 @@ ReadoutError class tests
 """
 
 import unittest
+from test.terra import common
 
 import numpy as np
 
-from test.terra import common
 from qiskit.providers.aer.noise.noiseerror import NoiseError
 from qiskit.providers.aer.noise.errors.readout_error import ReadoutError
 
 
 class TestReadoutError(common.QiskitAerTestCase):
     """Testing ReadoutError class"""
-
     def test_probabilities_normalized_exception(self):
         """Test exception is raised for probabilities greater than 1."""
         probs = [[0.9, 0.2], [0, 1]]
@@ -146,14 +145,19 @@ class TestReadoutError(common.QiskitAerTestCase):
         }
         error0 = ReadoutError(probs0)
         error1 = ReadoutError(probs1)
+        # compose method
         error = error0.compose(error1)
-
+        self.assertEqual(error.number_of_qubits, 1)
+        self.assertEqual(error.probabilities.tolist(), probs)
+        self.assertEqual(error.to_dict(), error_dict)
+        # @ method
+        error = error0 @ error1
         self.assertEqual(error.number_of_qubits, 1)
         self.assertEqual(error.probabilities.tolist(), probs)
         self.assertEqual(error.to_dict(), error_dict)
 
-    def test_compose_front(self):
-        """Test front compose of two readout errors."""
+    def test_dot_front(self):
+        """Test dot of two readout errors."""
         probs0 = [[0.9, 0.1], [0.4, 0.6]]
         probs1 = [[0.5, 0.5], [0.2, 0.8]]
         probs = np.dot(probs1, probs0).tolist()
@@ -164,8 +168,13 @@ class TestReadoutError(common.QiskitAerTestCase):
         }
         error0 = ReadoutError(probs0)
         error1 = ReadoutError(probs1)
-        error = error1.compose(error0, front=True)
-
+        # dot method
+        error = error1.dot(error0)
+        self.assertEqual(error.number_of_qubits, 1)
+        self.assertEqual(error.probabilities.tolist(), probs)
+        self.assertEqual(error.to_dict(), error_dict)
+        # * method
+        error = error1 * error0
         self.assertEqual(error.number_of_qubits, 1)
         self.assertEqual(error.probabilities.tolist(), probs)
         self.assertEqual(error.to_dict(), error_dict)
