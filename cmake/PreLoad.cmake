36782e12c02049f04290304e8d2726c50c5b4291
Matthew Treinish <mtreinish@kortar.org>
2020-01-30 11:24:21 -0500

    Remove unused cmake file

--- cmake/PreLoad.cmake
+++
@@ -1,8 +0,0 @@
-# This is an undocumented feature of CMake. With this PreLoad.cmake file we
-# can set the Generator without using the command line argument -G.
-# This is needed in order to make automatic wheel generator works in our CIs
-# Please, don't rely on this functionality.
-
-if (DEFINED ENV{FORCE_GENERATOR})
-    set(CMAKE_GENERATOR ENV{FORCE_GENERATOR} CACHE INTERNAL "" FORCE)
-endif()
