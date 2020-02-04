d313b89886173ee52b96ca21f33809244aa42cae
Hiroshi <horii@jp.ibm.com>
2020-02-04 15:23:04 +0900

    enable fusion by default for 20 or more qubits

--- src/transpile/fusion.hpp
+++ src/transpile/fusion.hpp
@@ -30,16 +30,21 @@ using reg_t = std::vector<uint_t>;
 class Fusion : public CircuitOptimization {
 public:
   // constructor
-  Fusion(uint_t max_qubit = 5, uint_t threshold = 16, double cost_factor = 1.8);
+  Fusion(uint_t max_qubit = 5, uint_t threshold = 20, double cost_factor = 1.8);
 
   /*
    * Fusion optimization uses following configuration options
-   *   - fusion_verbose (bool): if true, output generated gates in metadata (default: false)
-   *   - fusion_enable (bool): if true, activate fusion optimization (default: false)
-   *   - fusion_max_qubit (int): maximum number of qubits for a operation (default: 5)
-   *   - fusion_threshold (int): a threshold to activate fusion optimization when fusion_enable is true (default: 16)
-   *   - fusion_cost_factor (double): a cost function to estimate an aggregate gate (default: 1.8)
-  */
+   * - fusion_enable (bool): Enable fusion optimization in circuit optimization
+   *       passes [Default: True]
+   * - fusion_verbose (bool): Output gates generated in fusion optimization
+   *       into metadata [Default: False]
+   * - fusion_max_qubit (int): Maximum number of qubits for a operation generated
+   *       in a fusion optimization [Default: 5]
+   * - fusion_threshold (int): Threshold that number of qubits must be greater
+   *       than to enable fusion optimization [Default: 20]
+   * - fusion_cost_factor (double): a cost function to estimate an aggregate
+   *       gate [Default: 1.8]
+   */
   void set_config(const json_t &config) override;
 
   void optimize_circuit(Circuit& circ,
@@ -94,7 +99,7 @@ private:
   uint_t threshold_;
   double cost_factor_;
   bool verbose_ = false;
-  bool active_ = false;
+  bool active_ = true;
 };
 
 const std::vector<std::string> Fusion::supported_gates({
