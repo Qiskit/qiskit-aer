2cf72d936892e53dc09afb34d2bf66686f79d8df
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2020-03-02 16:26:55 -0500

    monte carlo solver successfully brought into new structure

---
+++ qiskit/providers/aer/pulse/de_solvers/qutip_solvers.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+# pylint: disable=no-name-in-module, import-error, invalid-name
+
+"""
+Solver from qutip.
+"""
+
+from math import log
+import logging
+import numpy as np
+from scipy.integrate import ode
+from scipy.linalg.blas import get_blas_funcs
+
+from .zvode import qiskit_zvode
+from ..pulse0.cy.measure import occ_probabilities, write_shots_memory
+from ..pulse0.qutip_lite.cy.spmatfuncs import cy_expect_psi_csr, spmv_csr
+
+dznrm2 = get_blas_funcs("znrm2", dtype=np.float64)
+
+
+def unitary_evolution(exp,
+                      sim_data,
+                      ode_options,
+                      system=None,
+                      channels=None):
+    """
+    Note: parameters with None default are required for C++ evaluation
+
+    depends on op_system attributes:
+        - x use_cpp_ode_func
+        - x channels
+        - x system
+        - global_data (a dict), uses following keys:
+            - 'string'
+            - 'initial_state'
+            - 'n_registers' (no idea what this is)
+            - 'rhs_func'
+            - 'h_diag_elems'
+        - global_data keys used in cpp numeric_integrator:
+            - 'freqs'
+            - 'pulse_array'
+            - 'pulse_indices'
+            - 'vars'
+            - 'var_names'
+            - 'num_h_terms'
+            - 'h_ops_data'
+            - 'h_ops_ind'
+            - 'h_ops_ptr'
+            - 'h_diag_elems'
+        - global_data keys used in cython
+            - maybe none, it's already baked into the cython before this point???
+
+    Calculates evolution when there is no noise,
+    or any measurements that are not at the end
+    of the experiment.
+
+    Args:
+        exp (dict): Dictionary of experimental pulse and fc
+        op_system (OPSystem): Global OpenPulse system settings
+
+    Returns:
+        array: Memory of shots.
+
+    Raises:
+        Exception: Error in ODE solver.
+    """
+
+    global_data = sim_data
+
+    tlist = exp['tlist']
+
+    # Init register
+    # Not sure how this is being used
+    register = np.ones(global_data['n_registers'], dtype=np.uint8)
+
+    num_channels = len(exp['channels'])
+
+    rhs_func = global_data['rhs_func']
+    ODE = ode(rhs_func)
+
+    # Don't know how to use OrderedDict type on Cython, so transforming it to dict
+    channels = dict(channels)
+    ODE.set_f_params(global_data, exp, system, channels, register)
+
+    ODE.set_integrator('zvode',
+                       method=ode_options.method,
+                       order=ode_options.order,
+                       atol=ode_options.atol,
+                       rtol=ode_options.rtol,
+                       nsteps=ode_options.nsteps,
+                       first_step=ode_options.first_step,
+                       min_step=ode_options.min_step,
+                       max_step=ode_options.max_step)
+
+    if not ODE._y:
+        ODE.t = 0.0
+        ODE._y = np.array([0.0], complex)
+    ODE._integrator.reset(len(ODE._y), ODE.jac is not None)
+
+    # Since all experiments are defined to start at zero time.
+    ODE.set_initial_value(global_data['initial_state'], 0)
+    for time in tlist[1:]:
+        ODE.integrate(time, step=0)
+        if ODE.successful():
+            psi = ODE.y / dznrm2(ODE.y)
+        else:
+            err_msg = 'ZVODE exited with status: %s' % ODE.get_return_code()
+            raise Exception(err_msg)
+
+
+    # apply final rotation to come out of rotating frame
+    psi_rot = np.exp(-1j * global_data['h_diag_elems'] * ODE.t)
+    psi *= psi_rot
+
+    return psi, ODE.t
+
+def monte_carlo_evolution(seed, exp, op_system):
+    """
+    Monte Carlo algorithm returning state-vector or expectation values
+    at times tlist for a single trajectory.
+    """
+
+    global_data = op_system.global_data
+    ode_options = op_system.ode_options
+
+    cy_rhs_func = global_data['rhs_func']
+    rng = np.random.RandomState(seed)
+    tlist = exp['tlist']
+    snapshots = []
+    # Init memory
+    memory = np.zeros((1, global_data['memory_slots']), dtype=np.uint8)
+    # Init register
+    register = np.zeros(global_data['n_registers'], dtype=np.uint8)
+
+    # Get number of acquire, snapshots, and conditionals
+    num_acq = len(exp['acquire'])
+    acq_idx = 0
+    num_snap = len(exp['snapshot'])
+    snap_idx = 0
+    num_cond = len(exp['cond'])
+    cond_idx = 0
+
+    collapse_times = []
+    collapse_operators = []
+
+    # first rand is collapse norm, second is which operator
+    rand_vals = rng.rand(2)
+
+    ODE = ode(cy_rhs_func)
+
+    # Don't know how to use OrderedDict type on Cython, so transforming it to dict
+    channels = dict(op_system.channels)
+    ODE.set_f_params(global_data, exp, op_system.system, channels, register)
+
+    # initialize ODE solver for RHS
+    ODE._integrator = qiskit_zvode(method=ode_options.method,
+                                   order=ode_options.order,
+                                   atol=ode_options.atol,
+                                   rtol=ode_options.rtol,
+                                   nsteps=ode_options.nsteps,
+                                   first_step=ode_options.first_step,
+                                   min_step=ode_options.min_step,
+                                   max_step=ode_options.max_step
+                                   )
+    # Forces complex ODE solving
+    if not any(ODE._y):
+        ODE.t = 0.0
+        ODE._y = np.array([0.0], complex)
+    ODE._integrator.reset(len(ODE._y), ODE.jac is not None)
+
+    ODE.set_initial_value(global_data['initial_state'], 0)
+
+    # make array for collapse operator inds
+    cinds = np.arange(global_data['c_num'])
+    n_dp = np.zeros(global_data['c_num'], dtype=float)
+
+    # RUN ODE UNTIL EACH TIME IN TLIST
+    for stop_time in tlist:
+        # ODE WHILE LOOP FOR INTEGRATE UP TO TIME TLIST[k]
+        while ODE.t < stop_time:
+            t_prev = ODE.t
+            y_prev = ODE.y
+            norm2_prev = dznrm2(ODE._y) ** 2
+            # integrate up to stop_time, one step at a time.
+            ODE.integrate(stop_time, step=1)
+            if not ODE.successful():
+                raise Exception("ZVODE step failed!")
+            norm2_psi = dznrm2(ODE._y) ** 2
+            if norm2_psi <= rand_vals[0]:
+                # collapse has occured:
+                # find collapse time to within specified tolerance
+                # ------------------------------------------------
+                ii = 0
+                t_final = ODE.t
+                while ii < ode_options.norm_steps:
+                    ii += 1
+                    t_guess = t_prev + \
+                        log(norm2_prev / rand_vals[0]) / \
+                        log(norm2_prev / norm2_psi) * (t_final - t_prev)
+                    ODE._y = y_prev
+                    ODE.t = t_prev
+                    ODE._integrator.call_args[3] = 1
+                    ODE.integrate(t_guess, step=0)
+                    if not ODE.successful():
+                        raise Exception(
+                            "ZVODE failed after adjusting step size!")
+                    norm2_guess = dznrm2(ODE._y)**2
+                    if (abs(rand_vals[0] - norm2_guess) <
+                            ode_options.norm_tol * rand_vals[0]):
+                        break
+
+                    if norm2_guess < rand_vals[0]:
+                        # t_guess is still > t_jump
+                        t_final = t_guess
+                        norm2_psi = norm2_guess
+                    else:
+                        # t_guess < t_jump
+                        t_prev = t_guess
+                        y_prev = ODE.y
+                        norm2_prev = norm2_guess
+                if ii > ode_options.norm_steps:
+                    raise Exception("Norm tolerance not reached. " +
+                                    "Increase accuracy of ODE solver or " +
+                                    "Options.norm_steps.")
+
+                collapse_times.append(ODE.t)
+                # all constant collapse operators.
+                for i in range(n_dp.shape[0]):
+                    n_dp[i] = cy_expect_psi_csr(global_data['n_ops_data'][i],
+                                                global_data['n_ops_ind'][i],
+                                                global_data['n_ops_ptr'][i],
+                                                ODE._y, 1)
+
+                # determine which operator does collapse and store it
+                _p = np.cumsum(n_dp / np.sum(n_dp))
+                j = cinds[_p >= rand_vals[1]][0]
+                collapse_operators.append(j)
+
+                state = spmv_csr(global_data['c_ops_data'][j],
+                                 global_data['c_ops_ind'][j],
+                                 global_data['c_ops_ptr'][j],
+                                 ODE._y)
+
+                state /= dznrm2(state)
+                ODE._y = state
+                ODE._integrator.call_args[3] = 1
+                rand_vals = rng.rand(2)
+
+        # after while loop (Do measurement or conditional)
+        # ------------------------------------------------
+        out_psi = ODE._y / dznrm2(ODE._y)
+        for aind in range(acq_idx, num_acq):
+            if exp['acquire'][aind][0] == stop_time:
+                current_acq = exp['acquire'][aind]
+                qubits = current_acq[1]
+                memory_slots = current_acq[2]
+                probs = occ_probabilities(qubits, out_psi, global_data['measurement_ops'])
+                rand_vals = rng.rand(memory_slots.shape[0])
+                write_shots_memory(memory, memory_slots, probs, rand_vals)
+                acq_idx += 1
+
+    return memory
