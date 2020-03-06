7c59edcbb3d3199ddf32770c1f02e7aa3bc075dd
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2020-03-06 08:00:40 -0500

    moving dimensions over

--- qiskit/providers/aer/pulse/system_models/string_model_parser/__init__.py
+++ qiskit/providers/aer/pulse/system_models/string_model_parser/__init__.py
@@ -14,11 +14,5 @@
 
 """Temporary folder for string parsers.
 
-Currently:
-- Nothing in this folder directly depends on QuTip, but it does depend on qutip
-  functionality (i.e. it manipulates qutip qobjs, though never directly creates them
-  itself)
-- The connection to qutip is through direct_qutip_dependence/qobj_generators
-    - The calls to this can be replaced with calls to something else that creates
-      the instances of the operator class that we end up using
+Note: I think apply_str_func_to_qobj (and the call to it in string_model_parser) can be removed
 """
