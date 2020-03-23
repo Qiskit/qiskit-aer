2a73f7b0a55a5082357d474fc22fc55bcaf11fd9
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2020-03-23 11:50:59 -0400

    fixed issue - adding things back to Qobj isn't possible, so just removed functions from apply_str_func that were attempting to call them (making the issue moot)

--- qiskit/providers/aer/pulse/system_models/string_model_parser/apply_str_func_to_qobj.py
+++ qiskit/providers/aer/pulse/system_models/string_model_parser/apply_str_func_to_qobj.py
@@ -18,65 +18,15 @@ Functions for applying scalar functions in __fundict to the operators
 represented in qutip Qobj.
 """
 
-#from ...qutip_extra_lite.qobj import Qobj
-
-
-def sin(val):
-    """ Qiskit wrapper of sine function
-    """
-    if isinstance(val, Qobj):
-        return val.sinm()
-    else:
-        return np.sin(val)
-
-
-def cos(val):
-    """ Qiskit wrapper of cosine function
-    """
-    if isinstance(val, Qobj):
-        return val.cosm()
-    else:
-        return np.cos(val)
-
-
-def exp(val):
-    """ Qiskit wrapper of exponential function
-    """
-    if isinstance(val, Qobj):
-        return val.expm()
-    else:
-        return np.exp(val)
-
-
-def sqrt(val):
-    """ Qiskit wrapper of square root
-    """
-    if isinstance(val, Qobj):
-        return val.sqrtm()
-    else:
-        return np.sqrt(val)
-
 
 def dag(qobj):
     """ Qiskit wrapper of adjoint
     """
     return qobj.dag()
 
-
-def conj(val):
-    """ Qiskit wrapper of conjugate
-    """
-    if isinstance(val, Qobj):
-        return val.conj()
-    else:
-        return np.conj(val)
-
-
 def apply_func(name, qobj):
     """ Apply function of given name, or do nothing if func not found
     """
     return __funcdict.get(name, lambda x: x)(qobj)
 
-
-__funcdict = {'cos': cos, 'sin': sin, 'exp': exp,
-              'sqrt': sqrt, 'conj': conj, 'dag': dag}
+__fundict = {'dag': dag}
