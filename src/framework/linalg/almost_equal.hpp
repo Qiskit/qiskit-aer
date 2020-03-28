e14b2d951934eaf0444952887cd2b2d9b56d8225
Juan Gomez <atilag@gmail.com>
2020-03-28 18:42:40 +0100

    PR Add hermitian eigensystem function

--- src/framework/linalg/almost_equal.hpp
+++ src/framework/linalg/almost_equal.hpp
@@ -19,24 +19,104 @@
 #include <limits>
 #include <type_traits>
 
+#include "framework/matrix.hpp"
 #include "framework/linalg/enable_if_numeric.hpp"
+
+#define MAXDIFF 5.0*AER::Linalg::epsilon<T>::value
+#define MAXRELATIVEDIFF 5.0*AER::Linalg::epsilon<T>::value
 
 namespace AER {
 namespace Linalg {
 
-// No silver bullet for floating point comparison techniques.
-// With this function the user can at least specify the precision
-// If we have numbers closer to 0, then max_diff can be set to a value
-// way smaller than epsilon. For numbers larger than 1.0, epsilon will
-// scale (the bigger the number, the bigger the epsilon).
-template <typename T, typename = enable_if_numeric_t<T>>
+template<typename T>
+struct epsilon {
+    static constexpr auto value = std::numeric_limits<T>::epsilon();
+};
+
+/**
+ * The epsilon of a complex is the epsilon from the value type of the complex.
+ * eg: std::complex<double> = double
+ *     std::complex<float> = float
+ */
+template<typename T>
+struct epsilon<std::complex<T>> {
+    static constexpr auto value = epsilon<T>::value;
+};
+
+/*
+ * We use function overloads to dispatch to the correct library functions 
+ *   wherever necessary - mainly for container types (vector, matrix) and/or
+ *   structured data types (std::complex).
+ * read is_tt as is-double-template (I think)
+ * See here:
+ *   https://stackoverflow.com/questions/41438493/how-to-identifying-whether-a-template-argument-is-stdcomplex
+ *
+ */
+template <template <class...> class TT, class... Args>
+std::true_type is_tt_impl(TT<Args...>);
+template <template <class...> class TT>
+std::false_type is_tt_impl(...);
+
+template <template <class...> class TT, class T>
+using is_tt = decltype(is_tt_impl<TT>(std::declval<typename std::decay<T>::type>()));
+
+template <typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type >
 bool almost_equal(T f1, T f2,
-                  T max_diff = std::numeric_limits<T>::epsilon(),
-                  T max_relative_diff = std::numeric_limits<T>::epsilon()) {
+                  T max_diff = MAXDIFF,
+                  T max_relative_diff = MAXRELATIVEDIFF);
+
+template <typename T>
+bool almost_equal(const std::complex<T>& f1, const std::complex<T>& f2,
+                  T max_diff = MAXDIFF,
+                  T max_relative_diff = MAXRELATIVEDIFF);
+ 
+template <typename T>
+bool almost_equal(const matrix<T>& mat1, const matrix<T>& mat2);
+
+template <typename T>
+bool almost_equal(const std::vector<T>& vec1, const std::vector<T>& vec2);
+ 
+template <typename T, typename >
+bool almost_equal(T f1, T f2,
+                  T max_diff,
+                  T max_relative_diff) {
   T diff = std::abs(f1 - f2);
   if (diff <= max_diff) return true;
   return diff <=
          max_relative_diff * std::max(std::abs(f1), std::abs(f2));
+}
+
+template <typename T>
+bool almost_equal(const std::complex<T>& f1, const std::complex<T>& f2,
+                  T max_diff,
+                  T max_relative_diff) {
+    return almost_equal<T>(f1.real(), f2.real(), max_diff, max_relative_diff)
+         && almost_equal<T>(f1.imag(), f2.imag(), max_diff, max_relative_diff);
+}
+
+template <typename T>
+bool almost_equal(const matrix<T>& mat1, const matrix<T>& mat2) {
+  if(mat1.size() != mat2.size()) {
+       return false;
+  }
+  for(auto i = 0; i < mat1.size(); ++i){
+    if(!almost_equal(mat1[i], mat2[i])) return false;
+  }
+  return true;
+}
+
+template <typename T>
+bool almost_equal(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+  if (vec1.size() != vec2.size()) {
+    return false;
+  }
+
+  for(auto i = 0; i < vec1.size(); ++i){
+    if( ! almost_equal(vec1[i], vec2[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 //------------------------------------------------------------------------------
