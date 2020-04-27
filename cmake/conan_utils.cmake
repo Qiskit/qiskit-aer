97d70b7dc37de8249a875a5d7fa7a7aff4182b01
vvilpas <vvilpas@gmail.com>
2020-04-27 12:21:54 +0200

    Handle thrust TBB & OMP dependency

--- cmake/conan_utils.cmake
+++ cmake/conan_utils.cmake
@@ -8,6 +8,9 @@ macro(setup_conan)
 
     include(${CMAKE_BINARY_DIR}/conan.cmake)
 
+    # Right now every dependency shall be static
+    set(CONAN_OPTIONS ${CONAN_OPTIONS} "*:shared=False")
+
     set(REQUIREMENTS nlohmann_json/3.7.3 spdlog/1.5.0)
     if(APPLE AND CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         set(REQUIREMENTS ${REQUIREMENTS} llvm-openmp/9.0.1)
@@ -17,7 +20,7 @@ macro(setup_conan)
         set(REQUIREMENTS ${REQUIREMENTS} muparserx/4.0.8)
     endif()
 
-    if(NOT AER_THRUST_BACKEND STREQUAL "CUDA")
+    if(AER_THRUST_BACKEND AND NOT AER_THRUST_BACKEND STREQUAL "CUDA")
         set(REQUIREMENTS ${REQUIREMENTS} thrust/1.9.5)
         string(TOLOWER ${AER_THRUST_BACKEND} THRUST_BACKEND)
         set(CONAN_OPTIONS ${CONAN_OPTIONS} "thrust:device_system=${THRUST_BACKEND}")
