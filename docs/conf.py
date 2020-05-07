6b6fcf54923b7787e707c1eee08da60a65e50d20
Matthew Treinish <mtreinish@kortar.org>
2020-05-07 17:35:28 -0400

    Unblock ci for new jupyter-sphinx release (#738)
    The latest jupyter-sphinx release deprecated the plugin path we were
    using which will now emit a deprecation warning. However because
    warnings are marked as fatal with -W this is breaking the docs builds.
    This commit updates are jupyter-sphinx usage to use the new path and fix
    the warning/failure.

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.extlinks',
     'sphinx_tabs.tabs',
-    'jupyter_sphinx.execute',
+    'jupyter_sphinx',
     'reno.sphinxext',
 ]
 html_static_path = ['_static']
