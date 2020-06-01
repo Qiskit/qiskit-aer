Qiskit/qiskit-aer

commit eea9a48ddf193bb91d0196c180034ded9d89fa32

Fixed cz gate of DensityMatrixThrust #754 (#760)
* fixed cz and y gates of DensityMatrixThrust #754

Co-authored-by: Christopher Wood <cjwood@us.ibm.com>

src/simulators/density_matrix/densitymatrix_thrust.hpp
@@ -302,19 +302,14 @@ void DensityMatrixThrust<data_t>::apply_cnot(const uint_t qctrl, const uint_t qt
 
 template <typename data_t>
 void DensityMatrixThrust<data_t>::apply_cz(const uint_t q0, const uint_t q1) {
-  cvector_t<double> vec;
-  vec.resize(16, 0.);
-
-  vec[3] = -1.;
-  vec[7] = -1.;
-  vec[11] = -1.;
-  vec[12] = -1.;
-  vec[13] = -1.;
-  vec[14] = -1.;
-
+  cvector_t<double> diag({1, 1, 1, -1,
+                          1, 1, 1, -1,
+                          1, 1, 1, -1,
+                          -1, -1, -1, 1});
   const auto nq =  num_qubits();
   const reg_t qubits = {{q0, q1, q0 + nq, q1 + nq}};
-  BaseVector::apply_matrix(qubits, vec);
+  BaseVector::apply_diagonal_matrix(qubits, diag);
+
 #ifdef AER_DEBUG
 	BaseVector::DebugMsg(" density::apply_cz",qubits);
 #endif
@@ -402,16 +397,66 @@ void DensityMatrixThrust<data_t>::apply_x(const uint_t qubit) {
 }
 
 template <typename data_t>
+class DensityY : public GateFuncBase
+{
+protected:
+  uint_t mask0;
+  uint_t mask1;
+
+public:
+  DensityY(int q0,int q1)
+  {
+  	if(q0 < q1){
+      mask0 = (1ull << q0) - 1;
+      mask1 = (1ull << q1) - 1;
+  	}
+  	else{
+      mask0 = (1ull << q1) - 1;
+      mask1 = (1ull << q0) - 1;
+  	}
+
+  }
+
+	__host__ __device__ double operator()(const thrust::tuple<uint_t,struct GateParams<data_t>> &iter) const
+  {
+    uint_t i,i0,i1,i2;
+	thrust::complex<data_t>* pV;
+	uint_t* offsets;
+    thrust::complex<data_t> q0,q1,q2,q3;
+		struct GateParams<data_t> params;
+
+  	i = ExtractIndexFromTuple(iter);
+		params = ExtractParamsFromTuple(iter);
+		pV = params.buf_;
+		offsets = params.offsets_;
+
+    i0 = i & mask0;
+    i2 = (i - i0) << 1;
+    i1 = i2 & mask1;
+    i2 = (i2 - i1) << 1;
+
+    i0 = i0 + i1 + i2;
+
+    q0 = pV[offsets[0]+i0];
+    q1 = pV[offsets[1]+i0];
+    q2 = pV[offsets[2]+i0];
+    q3 = pV[offsets[3]+i0];
+
+    pV[offsets[0]+i0] = q0;
+    pV[offsets[1]+i0] = -q2;
+    pV[offsets[2]+i0] = -q1;
+    pV[offsets[3]+i0] = q3;
+		return 0.0;
+  }
+
+};
+
+
+template <typename data_t>
 void DensityMatrixThrust<data_t>::apply_y(const uint_t qubit) {
-  cvector_t<double> vec;
-  vec.resize(16, 0.);
-  vec[0 * 4 + 3] = 1.;
-  vec[1 * 4 + 2] = -1.;
-  vec[2 * 4 + 1] = -1.;
-  vec[3 * 4 + 0] = 1.;
-  // Use the lambda function
   const reg_t qubits = {{qubit, qubit + num_qubits()}};
-  BaseVector::apply_matrix(qubits, vec);
+
+	BaseVector::apply_function(DensityY<data_t>(qubits[0], qubits[1]), qubits);
 
 #ifdef AER_DEBUG
 	BaseVector::DebugMsg(" density::apply_y",qubits);
@@ -420,16 +465,10 @@ void DensityMatrixThrust<data_t>::apply_y(const uint_t qubit) {
 
 template <typename data_t>
 void DensityMatrixThrust<data_t>::apply_z(const uint_t qubit) {
-  cvector_t<double> vec;
-  vec.resize(16, 0.);
-  vec[0 * 4 + 0] = 1.;
-  vec[1 * 4 + 1] = -1.;
-  vec[2 * 4 + 2] = -1.;
-  vec[3 * 4 + 3] = 1.;
-
+  cvector_t<double> diag({1, -1, -1, 1});
   // Use the lambda function
   const reg_t qubits = {{qubit, qubit + num_qubits()}};
-  BaseVector::apply_matrix(qubits, vec);
+  BaseVector::apply_diagonal_matrix(qubits, diag);
 
 #ifdef AER_DEBUG
 	BaseVector::DebugMsg(" density::apply_z",qubits);

releasenotes/notes/fix-density-matrix-thrust-8f5f27748974f310.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    Fix bug in CZ gate and Z gate for "density_matrix_gpu" and
+    "density_matrix_thrust" QasmSimulator methods.

src/simulators/unitary/unitarymatrix_thrust.hpp
@@ -138,7 +138,8 @@ json_t UnitaryMatrixThrust<data_t>::json() const
 {
   const int_t nrows = rows_;
   int iPlace;
-  uint_t i, irow, icol, ic, nc;
+  int_t i;
+  uint_t irow, icol, ic, nc;
   uint_t pos = 0;
   uint_t csize = 1ull << BaseVector::m_maxChunkBits;
   cvector_t<data_t> tmp(csize);
@@ -272,7 +273,8 @@ void UnitaryMatrixThrust<data_t>::initialize_from_matrix(const AER::cmatrix_t &m
   }
 
 	int iPlace;
-	uint_t i,irow,icol,ic,nc;
+  int_t i;
+	uint_t irow,icol,ic,nc;
 	uint_t pos = 0;
 	uint_t csize = 1ull << BaseVector::m_maxChunkBits;
 	cvector_t<data_t> tmp(csize);
@@ -347,7 +349,8 @@ std::pair<bool, double> UnitaryMatrixThrust<data_t>::check_identity() const {
   // Check conditions 2 and 3
   double delta = 0.;
 	int iPlace;
-	uint_t i,irow,icol,ic,nc;
+  int_t i;
+	uint_t irow,icol,ic,nc;
 	uint_t pos = 0;
 	uint_t csize = 1ull << BaseVector::m_maxChunkBits;
 	cvector_t<data_t> tmp(csize);

commit e2e14c1d75d5d7901af0bf371948052d6663da3d

Fix bug in DensityMatrixThrust apply_y
Fixes a bug introduced in #760

src/simulators/density_matrix/densitymatrix_thrust.hpp
@@ -442,10 +442,10 @@ public:
     q2 = pV[offsets[2]+i0];
     q3 = pV[offsets[3]+i0];
 
-    pV[offsets[0]+i0] = q0;
+    pV[offsets[0]+i0] = q3;
     pV[offsets[1]+i0] = -q2;
     pV[offsets[2]+i0] = -q1;
-    pV[offsets[3]+i0] = q3;
+    pV[offsets[3]+i0] = q0;
 		return 0.0;
   }
 