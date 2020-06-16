34a82fcdec6a97026f2fc36a55ad80a6bf945dce
Merav Aharoni <merav@il.ibm.com>
2020-06-16 13:58:29 +0300

    Fixed bug in Windows by adding a function that compares small fractions by scaling them up

--- src/simulators/matrix_product_state/svd.cpp
+++ src/simulators/matrix_product_state/svd.cpp
@@ -72,6 +72,13 @@ std::vector<cmatrix_t> reshape_V_after_SVD(const cmatrix_t V)
   return Res;
 }
 
+template <typename T, typename = enable_if_numeric_t<T>>
+bool almost_equal_high_precision(T f1, T f2,
+				 T max_diff = std::numeric_limits<T>::epsilon(),
+				 T max_relative_diff = std::numeric_limits<T>::epsilon()) {
+  return Linalg::almost_equal(f1 * tiny_factor, f2 * tiny_factor);
+}
+
 
 //-------------------------------------------------------------
 // function name: num_of_SV
@@ -114,33 +121,28 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
   complex_t q = 0;
   // Transpose when m < n
   bool transposed = false;
-  if (m < n)
-    {
-    	transposed = true;
-    	A = AER::Utils::dagger(A);
-	std::swap(m,n);
+  if (m < n) {
+    transposed = true;
+    A = AER::Utils::dagger(A);
+    std::swap(m,n);
+  }
+  cmatrix_t temp_A = A;
+  c[0] = 0;
+  while(true) {
+    k1 = k + 1;
+    z = 0.0;
+    for( i = k; i < m; i++){
+      z = z + norm(A(i,k));
     }
-	cmatrix_t temp_A = A;
-	c[0] = 0;
-	while(true)
-	{
-		k1 = k + 1;
-		z = 0.0;
-		for( i = k; i < m; i++){
-			z = z + norm(A(i,k));
-		}
-		b[k] = 0.0;
-		if ( tol < z )
-		{
+    b[k] = 0.0;
+    if ( tol < z ){
 			z = std::sqrt( z );
 			b[k] = z;
 			w = std::abs( A(k,k) );
-			if (Linalg::almost_equal(static_cast<long double>(w), 
-						 static_cast<long double>(0.0) )) {
-				q = complex_t( 1.0, 0.0 );
-			}
-			else {
-				q = A(k,k) / w;
+			if (almost_equal_high_precision(w, 0.0)) {
+			  q = complex_t( 1.0, 0.0 );
+			} else {
+			  q = A(k,k) / w;
 			}
 			A(k,k) = q * ( z + w );
 
@@ -184,8 +186,7 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 			c[k1] = z;
 			w = std::abs( A(k,k1) );
 
-			if (Linalg::almost_equal(static_cast<long double>(w), 
-						 static_cast<long double>(0.0) )){
+			if (almost_equal_high_precision(w, 0.0)) {
 				q = complex_t( 1.0, 0.0 );
 			}
 			else{
@@ -319,8 +320,7 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 				h = sn * g;
 				g = cs * g;
 				w = std::sqrt( h * h + f * f );
-				if (Linalg::almost_equal(static_cast<long double>(w), 
-							 static_cast<long double>(0.0) )) {
+				if (almost_equal_high_precision(w, 0.0)) {
 #ifdef DEBUG
 				  std::cout << "ERROR 1: w is exactly 0: h = " << h << " , f = " << f << std::endl;
 				  std::cout << " w = " << w << std::endl;
@@ -332,8 +332,7 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 				f = x * cs + g * sn; // might be 0
 
 				long double large_f = 0;
-				if (Linalg::almost_equal(static_cast<long double>(f), 
-							 static_cast<long double>(0.0) )) {
+				if (almost_equal_high_precision(f, 0.0)) {
 #ifdef DEBUG
 				  std::cout << "f == 0 because " << "x = " << x << ", cs = " << cs << ", g = " << g << ", sn = " << sn  <<std::endl;
 #endif
@@ -367,15 +366,14 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 				std::cout << " h = " << h << " f = " << f << " large_f = " << large_f << std::endl;
 #endif
 				if (std::abs(h) < 1e-13 && std::abs(f) < 1e-13 && 
-				    !Linalg::almost_equal(large_f, 
-							  static_cast<long double>(0.0))) {
+				    !almost_equal_high_precision(large_f, static_cast<long double>(0.0))) {
 				  tiny_w = true;
 				} else {
 				  w = std::sqrt( h * h + f * f );
 				}
 				w = std::sqrt( h * h + f * f );
-				if (Linalg::almost_equal(static_cast<long double>(w), 
-							 static_cast<long double>(0.0)) && !tiny_w) {
+				if (almost_equal_high_precision(w, 0.0)  && !tiny_w) {
+							 
 
 #ifdef DEBUG
 				  std::cout << "ERROR: w is exactly 0: h = " << h << " , f = " << f << std::endl;
@@ -457,9 +455,7 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 
     for( k = n-1 ; k >= 0; k--)
 	{
-	  if (!Linalg::almost_equal(static_cast<long double>(b[k]), 
-				    static_cast<long double>(0.0)) )
-		{
+	  if (!almost_equal_high_precision(b[k], 0.0)) {
 			q = -A(k,k) / std::abs( A(k,k) );
 			for( j = 0; j < m; j++){
 				U(k,j) = q * U(k,j);
@@ -481,9 +477,7 @@ status csvd(cmatrix_t &A, cmatrix_t &U,rvector_t &S,cmatrix_t &V)
 	for( k = n-1 -1; k >= 0; k--)
 	{
 		k1 = k + 1;
-		if ( !Linalg::almost_equal(static_cast<long double>(c[k1]), 
-					   static_cast<long double>(0.0) ))
-		{
+		if ( !almost_equal_high_precision(c[k1], 0.0)) {
 			q = -std::conj( A(k,k1) ) / std::abs( A(k,k1) );
 
 			for( j = 0; j < n; j++){
