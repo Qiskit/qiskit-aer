98b9c88250e3e16d481a795533039058281c03d6
Merav Aharoni <merav@il.ibm.com>
2020-06-16 16:48:13 +0300

    fixed list of running tests

--- test/terra/backends/test_qasm_simulator_matrix_product_state.py
+++ test/terra/backends/test_qasm_simulator_matrix_product_state.py
@@ -50,38 +50,47 @@ from test.terra.backends.qasm_simulator.qasm_snapshot import QasmSnapshotExpvalP
 from test.terra.backends.qasm_simulator.qasm_snapshot import QasmSnapshotExpValMatrixTests
 
 
-# Due to bug in Windows (issue #744 https://github.com/Qiskit/qiskit-aer/issues/773)
-# We skip the full set of MPS tests for windows CI and use a separate test class below 
-@unittest.skipIf(os.name == 'nt', 'Skip full MPS tests on Windows until #773 is fixed')
 class TestQasmMatrixProductStateSimulator(
         common.QiskitAerTestCase,
-        QasmMeasureTests,
-        QasmMultiQubitMeasureTests,
-        QasmResetTests,
-        QasmConditionalGateTests,
-        QasmConditionalUnitaryTests,
-        QasmCliffordTests,
-        QasmCliffordTestsWaltzBasis,
-        QasmCliffordTestsMinimalBasis,
-        QasmNonCliffordTestsTGate,
-        QasmNonCliffordTestsCCXGate,
-        QasmNonCliffordTestsWaltzBasis,
-        QasmNonCliffordTestsMinimalBasis,
-        QasmAlgorithmTests,
-        QasmAlgorithmTestsWaltzBasis,
-        QasmAlgorithmTestsMinimalBasis,
-        QasmUnitaryGateTests,
-        # QasmInitializeTests,  # THROWS: partial initialize not supported
-        QasmReadoutNoiseTests,
-        QasmPauliNoiseTests,
-        QasmResetNoiseTests,
-        QasmSnapshotStatevectorTests,
-        QasmSnapshotProbabilitiesTests,
-        QasmSnapshotStabilizerTests,
-        QasmSnapshotExpValPauliTests,
-        QasmSnapshotExpvalPauliNCTests,
-        QasmSnapshotExpValMatrixTests,
-):
+                        QasmMeasureTests,
+                        QasmMultiQubitMeasureTests,
+                        QasmResetTests,
+                        #QasmInitializeTests,
+                        QasmConditionalGateTests,
+                        QasmConditionalUnitaryTests,
+                        #QasmConditionalKrausTests,
+                        QasmCliffordTests,
+                        QasmCliffordTestsWaltzBasis,
+                        QasmCliffordTestsMinimalBasis,
+                        QasmNonCliffordTestsTGate,
+                        QasmNonCliffordTestsCCXGate,
+                        #QasmNonCliffordTestsCGates,
+                        QasmNonCliffordTestsWaltzBasis,
+                        QasmNonCliffordTestsMinimalBasis,
+                        #QasmMultiplexerTests,
+                        QasmAlgorithmTests,
+                        QasmAlgorithmTestsWaltzBasis,
+                        QasmAlgorithmTestsMinimalBasis,
+                        QasmUnitaryGateTests,
+                        #QasmDiagonalGateTests,
+                        QasmReadoutNoiseTests,
+                        QasmPauliNoiseTests,
+                        #QasmThreadManagementTests,
+                        #QasmFusionTests,
+                        #QasmDelayMeasureTests,
+                        #QasmQubitsTruncateTests,
+                        QasmResetNoiseTests,
+                        #QasmKrausNoiseTests,
+                        #QasmBasicsTests,
+                        QasmSnapshotStatevectorTests,
+                        #QasmSnapshotDensityMatrixTests,
+                        QasmSnapshotProbabilitiesTests,
+                        QasmSnapshotExpValPauliTests,
+                        QasmSnapshotExpvalPauliNCTests,
+                        QasmSnapshotExpValMatrixTests,
+                        QasmSnapshotStabilizerTests):
+    
+
     """QasmSimulator matrix product state method tests."""
 
     BACKEND_OPTS = {
@@ -90,47 +99,6 @@ class TestQasmMatrixProductStateSimulator(
         "max_parallel_threads": 1
     }
 
-
-# Reduced set of tests on windows until #773 is fixed
-# Note that this is not ideal since it skips ALL tests of basic Clifford gates
-# Even though only the CZ gate tests (and one CX gate test) were failing
-@unittest.skipIf(os.name != 'nt', 'Skip Windows-specific MPS tests until #773 is fixed')
-class TestQasmMatrixProductStateSimulatorWIN(
-        common.QiskitAerTestCase,
-        QasmMeasureTests,
-        QasmMultiQubitMeasureTests,
-        QasmResetTests,
-        QasmConditionalGateTests,
-        QasmConditionalUnitaryTests,
-        QasmCliffordTests,  # Failing for CZ
-        QasmCliffordTestsWaltzBasis,  # Failing for CX, CZ
-        QasmCliffordTestsMinimalBasis,  # Failing for CX, CZ
-        QasmNonCliffordTestsTGate,
-        QasmNonCliffordTestsCCXGate,
-        QasmNonCliffordTestsWaltzBasis,  # Failing for CCX, CSwap
-        QasmNonCliffordTestsMinimalBasis,  # Failing for CCX, CSwap
-        QasmAlgorithmTests,  # Failing for Grovers
-        QasmAlgorithmTestsWaltzBasis,  # Failing for Grovers
-        QasmAlgorithmTestsMinimalBasis,  # Failing for Grovers
-        QasmUnitaryGateTests,
-        # QasmInitializeTests,  # THROWS: partial initialize not supported
-        QasmReadoutNoiseTests,
-        QasmPauliNoiseTests,
-        QasmResetNoiseTests,
-        QasmSnapshotStatevectorTests,
-        QasmSnapshotProbabilitiesTests,
-        QasmSnapshotStabilizerTests,
-        QasmSnapshotExpValPauliTests,
-        QasmSnapshotExpvalPauliNCTests,
-        QasmSnapshotExpValMatrixTests,
-):
-    """QasmSimulator matrix product state method tests."""
-
-    BACKEND_OPTS = {
-        "seed_simulator": 314159,
-        "method": "matrix_product_state",
-        "max_parallel_threads": 1
-    }
 
 
 if __name__ == '__main__':
