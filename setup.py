a978ef53822c3648be32ddaf334435195787cd16
KOLANICH <kolan_n@mail.ru>
2020-06-19 14:53:44 +0300

    Moved the metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -27,24 +27,6 @@ except ImportError:
 
 import setuptools
 
-requirements = [
-    'qiskit-terra>=0.12.0',
-    'numpy>=1.16.3;python_version>"3.5"',
-    'numpy>=1.16.3,<1.19.0;python_version<"3.6"',
-    'scipy>=1.0;python_version>"3.5"',
-    'scipy>=1.0,<1.5.0;python_version<"3.6"',
-    'cython>=0.27.1',
-    'pybind11>=2.4'  # This isn't really an install requirement,
-                     # Pybind11 is required to be pre-installed for
-                     # CMake to successfully find header files.
-                     # This should be fixed in the CMake build files.
-]
-
-setup_requirements = requirements + [
-    'scikit-build',
-    'cmake!=3.17,!=3.17.0',
-    'conan>=1.22.2'
-]
 
 if not hasattr(setuptools,
                'find_namespace_packages') or not inspect.ismethod(
@@ -54,48 +36,8 @@ if not hasattr(setuptools,
           "repeat install.".format(setuptools.__version__))
     sys.exit(1)
 
-VERSION_PATH = os.path.join(os.path.dirname(__file__),
-                            "qiskit", "providers", "aer", "VERSION.txt")
-with open(VERSION_PATH, "r") as version_file:
-    VERSION = version_file.read().strip()
-
-README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                           'README.md')
-with open(README_PATH) as readme_file:
-    README = readme_file.read()
-
 setup(
-    name='qiskit-aer',
-    version=VERSION,
     packages=setuptools.find_namespace_packages(include=['qiskit.*']),
     cmake_source_dir='.',
-    description="Qiskit Aer - High performance simulators for Qiskit",
-    long_description=README,
-    long_description_content_type='text/markdown',
-    url="https://github.com/Qiskit/qiskit-aer",
-    author="AER Development Team",
-    author_email="qiskit@us.ibm.com",
-    license="Apache 2.0",
-    classifiers=[
-        "Environment :: Console",
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-    ],
-    install_requires=requirements,
-    setup_requires=setup_requirements,
-    include_package_data=True,
     cmake_args=["-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9"],
-    keywords="qiskit aer simulator quantum addon backend",
-    zip_safe=False
 )
