37d85adca11440e0fa3e5104204ef4305955becc
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2020-06-29 10:35:21 -0400

    fixing line too long in verify_wheels

--- tools/verify_wheels.py
+++ tools/verify_wheels.py
@@ -15,7 +15,8 @@ from qiskit import QuantumCircuit
 from qiskit import QuantumRegister
 
 from qiskit.providers.aer.pulse.system_models.duffing_model_generators import duffing_system_model
-from qiskit.pulse import Schedule, Play, Acquire, SamplePulse, DriveChannel, AcquireChannel, MemorySlot
+from qiskit.pulse import (Schedule, Play, Acquire, SamplePulse, DriveChannel, AcquireChannel,
+                          MemorySlot)
 
 from qiskit.providers.aer import QasmSimulator
 from qiskit.providers.aer import StatevectorSimulator
