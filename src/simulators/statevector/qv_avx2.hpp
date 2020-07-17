32c57135143795d6aeebde9604a27e709c87a9cf
hhorii <hhorii@users.noreply.github.com>
2020-07-18 01:49:01 +0900

    Add SIMD (AVX) extension for QubitVector (#684)
    * Add avx2 support for Intel environment
    * Calibrate cost estimation for fused operations
    * Add runtime check for AVX presenence using CPU features
    * Isolate SIMD (AVX) code so only CPUs with AVX support can hit the SIMD path
    * Add QubitVectorAvx2 class using CRTP, and change QubitVector class accordingly
    * Add /bigobj compiler flag
    
    Because there's runtime detection that doesn't impact on performance, we can safely build with AVX flags and run everywhere.
    
    Co-authored-by: Juan Gomez <atilag@gmail.com>

---
+++ src/simulators/statevector/qv_avx2.hpp
@@ -0,0 +1,36 @@
+/**
+ * This code is part of Qiskit.
+ *
+ * (C) Copyright IBM 2020.
+ *
+ * This code is licensed under the Apache License, Version 2.0. You may
+ * obtain a copy of this license in the LICENSE.txt file in the root directory
+ * of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Any modifications or derivative works of this code must retain this
+ * copyright notice, and modified files need to carry a notice indicating
+ * that they have been altered from the originals.
+ */
+
+#ifndef _qv_avx2_hpp_
+#define _qv_avx2_hpp_
+
+#include <cstdint>
+#include <cstring>
+
+namespace AER {
+namespace QV {
+
+enum class Avx { NotApplied, Applied };
+
+template <typename FloatType>
+Avx apply_matrix_avx(FloatType* data,
+                     const uint64_t data_size,
+                     const uint64_t* qregs,
+                     const size_t qregs_size,
+                     const FloatType* mat,
+                     const size_t omp_threads);
+
+} // end namespace QV
+} // end namespace AER
+#endif
