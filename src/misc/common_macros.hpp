cf6b2f1a74ccabd977555b867a702927f39bd7c0
Victor Villar <59838221+vvilpas@users.noreply.github.com>
2020-08-07 14:56:07 +0200

    Fix/issue 856 build ppc (#864)

---
+++ src/misc/common_macros.hpp
@@ -0,0 +1,9 @@
+
+#ifndef QASM_SIMULATOR_COMMON_MACROS_HPP
+#define QASM_SIMULATOR_COMMON_MACROS_HPP
+
+#if defined(__GNUC__) && defined(__x86_64__)
+ #define GNUC_AVX2
+#endif
+
+#endif //QASM_SIMULATOR_COMMON_MACROS_HPP
