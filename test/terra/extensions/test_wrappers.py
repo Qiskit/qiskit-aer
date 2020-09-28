97bb51f60241e5f43192d4cec73e66f0dce6fbe5
Christopher Wood <cjwood@us.ibm.com>
2020-09-28 17:17:53 -0400

    Try again to improving hanging wrapper tests

--- test/terra/extensions/test_wrappers.py
+++ test/terra/extensions/test_wrappers.py
@@ -16,7 +16,7 @@ import copy
 import pickle
 from multiprocessing import Pool
 
-from qiskit import assemble, transpile
+from qiskit import assemble, transpile, QuantumCircuit
 from qiskit.providers.aer.backends import QasmSimulator, StatevectorSimulator, UnitarySimulator
 from qiskit.providers.aer.backends.controller_wrappers import (qasm_controller_execute,
                                                                statevector_controller_execute,
@@ -28,7 +28,9 @@ from test.terra.common import QiskitAerTestCase
 class TestControllerExecuteWrappers(QiskitAerTestCase):
     """Basic functionality tests for pybind-generated wrappers"""
 
-    CFUNCS = [qasm_controller_execute(), statevector_controller_execute(), unitary_controller_execute()]
+    CFUNCS = [qasm_controller_execute(),
+              statevector_controller_execute(),
+              unitary_controller_execute()]
 
     def test_deepcopy(self):
         """Test that the functors are deepcopy-able."""
@@ -41,10 +43,13 @@ class TestControllerExecuteWrappers(QiskitAerTestCase):
             bites = pickle.dumps(cfunc)
             cahpy = pickle.loads(bites)
 
-    def _create_qobj(self, circs, backend, backend_options=None, noise_model=None):
-        circs = transpile(circs, backend)
-        qobj = assemble(circs, backend)
-        fqobj = backend._format_qobj(qobj, backend_options=backend_options, noise_model=noise_model)
+    def _create_qobj(self, backend, noise_model=None):
+        num_qubits = 2
+        circuit = QuantumCircuit(num_qubits)
+        circuit.x(list(range(num_qubits)))
+        qobj = assemble(transpile(circuit, backend), backend)
+        opts = {'max_parallel_threads': 1}
+        fqobj = backend._format_qobj(qobj, backend_options=opts, noise_model=noise_model)
         return fqobj 
 
     def _map_and_test(self, cfunc, qobj):
@@ -60,25 +65,21 @@ class TestControllerExecuteWrappers(QiskitAerTestCase):
         """Test that the qasm controller can be mapped."""
         cfunc = qasm_controller_execute()
         sim = QasmSimulator()
-        circs = ref_algorithms.teleport_circuit()
-        fqobj = self._create_qobj(circs, sim)
+        fqobj = self._create_qobj(sim)
         self._map_and_test(cfunc, fqobj)
 
     def test_mappable_statevector(self):
         """Test that the statevector controller can be mapped."""
         cfunc = statevector_controller_execute()
         sim = StatevectorSimulator()
-        circs = ref_measure.measure_circuits_deterministic()
-        fqobj = self._create_qobj(circs, sim)
+        fqobj = self._create_qobj(sim)
         self._map_and_test(cfunc, fqobj)
 
     def test_mappable_unitary(self):
         """Test that the unitary controller can be mapped."""
         cfunc = unitary_controller_execute()
         sim = UnitarySimulator()
-        circs = ref_1q_clifford.h_gate_circuits_deterministic(
-            final_measure=False)
-        fqobj = self._create_qobj(circs, sim)
+        fqobj = self._create_qobj(sim)
         self._map_and_test(cfunc, fqobj)
 
 
