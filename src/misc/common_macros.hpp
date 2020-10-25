687cf4b5d36fecdabe659042d4c75b0d47becc08
Padraic Calpin <padraic.calpin93@gmail.com>
2020-10-25 15:49:23 +0100

    Move imports to common macros
    Less duplicated imports, also unbreaks my linting

--- src/misc/common_macros.hpp
+++ src/misc/common_macros.hpp
@@ -15,8 +15,11 @@
 #ifndef QASM_SIMULATOR_COMMON_MACROS_HPP
 #define QASM_SIMULATOR_COMMON_MACROS_HPP
 
-#if defined(__GNUC__) && defined(__x86_64__)
+#if defined(_MSC_VER)
+ #include <intrin.h>
+#elif defined(__GNUC__) && defined(__x86_64__)
  #define GNUC_AVX2
+ #include <cpuid.h>
 #endif
 
 #endif //QASM_SIMULATOR_COMMON_MACROS_HPP
