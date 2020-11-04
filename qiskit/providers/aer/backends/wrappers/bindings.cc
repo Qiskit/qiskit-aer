bf8e10a33519b8645ec0c308408f84cd7b626d92
vvilpas <vvilpas@gmail.com>
2020-11-04 13:28:03 +0100

    Refactor where avx2 needed headers are included
    Move avx2 needed headers inclusion from qasm_simulator.cpp and bindings.cc to avx2_detect.hpp, where are actually used.

--- qiskit/providers/aer/backends/wrappers/bindings.cc
+++ qiskit/providers/aer/backends/wrappers/bindings.cc
@@ -1,10 +1,4 @@
 #include <iostream>
-#include "misc/common_macros.hpp"
-#if defined(_MSC_VER)
-#include <intrin.h>
-#elif defined(GNUC_AVX2)
-#include <cpuid.h>
-#endif
 
 #include "misc/warnings.hpp"
 DISABLE_WARNING_PUSH
