db1ac67ec61d8715410518d6b7043d8bb8d5d067
Yael Ben-Haim <yaelbh@il.ibm.com>
2020-11-18 11:21:58 +0200

    Adding rotation gates to the MPS simulator (#1042)

--- qiskit/providers/aer/backends/qasm_simulator.py
+++ qiskit/providers/aer/backends/qasm_simulator.py
@@ -450,7 +450,8 @@ class QasmSimulator(AerBackend):
             config.description = 'A C++ QasmQobj matrix product state simulator with noise'
             config.basis_gates = [
                 'u1', 'u2', 'u3', 'u', 'p', 'cp', 'cx', 'cz', 'id', 'x', 'y', 'z', 'h', 's',
-                'sdg', 'sx', 't', 'tdg', 'swap', 'ccx', 'unitary', 'roerror', 'delay'
+                'sdg', 'sx', 't', 'tdg', 'swap', 'ccx', 'unitary', 'roerror', 'delay',
+                'r', 'rx', 'ry', 'rz', 'rxx', 'ryy', 'rzz', 'rzx'
             ]
 
         # Stabilizer method
