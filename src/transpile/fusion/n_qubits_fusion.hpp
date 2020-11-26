0178bc39ca8dd2e82e384246ecfb262c0379c163
Hiroshi <horii@jp.ibm.com>
2020-11-27 00:17:51 +0900

    make n_qubit fusion more configurable

--- src/transpile/fusion/n_qubits_fusion.hpp
+++ src/transpile/fusion/n_qubits_fusion.hpp
@@ -31,7 +31,7 @@ template<size_t N>
 class NQubitFusion {
 public:
   NQubitFusion(std::shared_ptr<FusionMethod> method_ = std::make_shared<FusionMethod>())
-    : method(method_), threshold(method_->get_default_threshold_qubit()) { }
+    : method(method_), qubit_threshold(method_->get_default_threshold_qubit()), gate_threshold_ratio(10) { }
 
   virtual ~NQubitFusion() {}
 
@@ -39,7 +39,7 @@ public:
 
   std::string name() const { return "two_qubit_fusion"; };
 
-  uint_t get_threshold() const { return threshold; }
+  uint_t get_threshold() const { return qubit_threshold; }
 
   bool aggregate_operations(oplist_t& ops, const int fusion_start, const int fusion_end) const;
 
@@ -52,7 +52,8 @@ public:
 private:
   const size_t n = N;
   const std::shared_ptr<FusionMethod> method;
-  uint_t threshold;
+  uint_t qubit_threshold;
+  uint_t gate_threshold_ratio;
   bool active = true;
 };
 
@@ -63,6 +64,21 @@ void NQubitFusion<N>::set_config(const json_t &config) {
 
   if (JSON::check_key("fusion_enable.n_qubits", config))
     JSON::get_value(active, "fusion_enable.n_qubits", config);
+
+  std::stringstream opt_name;
+  opt_name << "fusion_enable." << N << "_qubits";
+
+  if (JSON::check_key(opt_name.str(), config))
+    JSON::get_value(active, opt_name.str(), config);
+
+  if (JSON::check_key("fusion_threshold.n_qubits.ratio", config))
+    JSON::get_value(gate_threshold_ratio, "fusion_threshold.n_qubits.ratio", config);
+
+  opt_name.str("");
+  opt_name << "fusion_threshold." << N << "_qubits.ratio";
+
+  if (JSON::check_key(opt_name.str(), config))
+    JSON::get_value(gate_threshold_ratio, opt_name.str(), config);
 }
 
 #ifdef DEBUG
@@ -103,6 +119,10 @@ bool NQubitFusion<N>::aggregate_operations(oplist_t& ops,
   for (uint_t op_idx = fusion_start; op_idx < fusion_end; ++op_idx)
     dump_op_in_circuit(ops, op_idx);
 #endif
+
+  std::vector<std::pair<uint_t, optype_t>> nops;
+  std::vector<std::pair<uint_t, std::vector<op_t>>> targets;
+  uint_t fused = 0;
 
   for (uint_t op_idx = fusion_start; op_idx < fusion_end; ++op_idx) {
 
@@ -153,17 +173,27 @@ bool NQubitFusion<N>::aggregate_operations(oplist_t& ops,
     std::vector<op_t> fusing_ops;
     for (auto fusing_op_idx : fusing_op_idxs) {
       fusing_ops.push_back(ops[fusing_op_idx]); //copy
+      nops.push_back(std::make_pair(fusing_op_idx, ops[fusing_op_idx].type));
       ops[fusing_op_idx].type = optype_t::nop;
+      ++fused;
     }
-    ops[op_idx] = method->generate_fusion_operation(fusing_ops, ops[op_idx].qubits);
+    targets.push_back(std::make_pair(op_idx, fusing_ops));
+    ++fused;
   }
 
+  if (fused > ((fusion_end - fusion_start) / 10)) {
+    for (const auto& target: targets)
+      ops[target.first] = method->generate_fusion_operation(target.second, ops[target.first].qubits);
 #ifdef DEBUG
-  for (uint_t op_idx = fusion_start; op_idx < fusion_end; ++op_idx)
-    dump_op_in_circuit(ops, op_idx);
+    for (uint_t op_idx = fusion_start; op_idx < fusion_end; ++op_idx)
+      dump_op_in_circuit(ops, op_idx);
 #endif
-
-  return true;
+    return true;
+  } else {
+    for (const auto& target: nops)
+      ops[target.first].type = target.second;
+    return false;
+  }
 }
 
 //-------------------------------------------------------------------------
