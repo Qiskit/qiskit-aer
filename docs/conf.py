350e42125cc57b0acc9e8d44f4bc6030a9c70e8d
Matthew Treinish <mtreinish@kortar.org>
2021-02-18 15:27:05 -0500

    Prepare 0.7.5 release (#1146)
    This commit prepares the 0.7.5 release, it bumps the version string and
    adds a release note documenting what has been fixed.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ author = 'Qiskit Development Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.7.4'
+release = '0.7.5'
 
 # -- General configuration ---------------------------------------------------
 
