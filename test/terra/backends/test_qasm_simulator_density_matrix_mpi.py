60595fcfbb984cebd0e6010475d2c1cc6711c663
Christopher J. Wood <cjwood@us.ibm.com>
2021-02-22 13:51:09 -0500

    Start updating tests to use configurable simulator backend (#1150)

--- test/terra/backends/test_qasm_simulator_density_matrix_mpi.py
+++ test/terra/backends/test_qasm_simulator_density_matrix_mpi.py
@@ -14,16 +14,15 @@ QasmSimulator Integration Tests
 """
 
 import unittest
+from qiskit.providers.aer import QasmSimulator
+from qiskit.providers.aer import AerError
 from test.terra import common
 from test.terra.decorators import requires_method
-
-
 
 from test.terra.backends.qasm_simulator.qasm_mpi import QasmMPITests
 
 
-class DensityMatrixMPITests(
-        QasmMPITests):
+class DensityMatrixMPITests(QasmMPITests):
     """Container class of statevector method tests."""
     pass
 
@@ -34,39 +33,51 @@ class TestQasmSimulatorDensityMatrixMPI(common.QiskitAerTestCase, DensityMatrixM
     BACKEND_OPTS = {
         "seed_simulator": 271828,
         "method": "density_matrix",
-        "blocking_enable" : True,
-        "blocking_qubits" : 6,
+        "blocking_enable": True,
+        "blocking_qubits": 6,
         "max_parallel_threads": 1
     }
+    try:
+        SIMULATOR = QasmSimulator(**BACKEND_OPTS)
+    except AerError:
+        SIMULATOR = None
 
 
 @requires_method("qasm_simulator", "density_matrix_gpu")
 class TestQasmSimulatorDensityMatrixMPIThrustGPU(common.QiskitAerTestCase,
-                                            DensityMatrixMPITests):
+                                                 DensityMatrixMPITests):
     """QasmSimulator density_matrix_gpu method MPI tests."""
 
     BACKEND_OPTS = {
         "seed_simulator": 271828,
         "method": "density_matrix_gpu",
-        "blocking_enable" : True,
-        "blocking_qubits" : 6,
+        "blocking_enable": True,
+        "blocking_qubits": 6,
         "blocking_ignore_diagonal" : True,
         "max_parallel_threads": 1
     }
+    try:
+        SIMULATOR = QasmSimulator(**BACKEND_OPTS)
+    except AerError:
+        SIMULATOR = None
 
 
 @requires_method("qasm_simulator", "density_matrix_thrust")
 class TestQasmSimulatorDensityMatrixMPIThrustCPU(common.QiskitAerTestCase,
-                                            DensityMatrixMPITests):
+                                                 DensityMatrixMPITests):
     """QasmSimulator density_matrix_thrust method MPI tests."""
 
     BACKEND_OPTS = {
         "seed_simulator": 271828,
         "method": "density_matrix_thrust",
-        "blocking_enable" : True,
-        "blocking_qubits" : 6,
+        "blocking_enable": True,
+        "blocking_qubits": 6,
         "max_parallel_threads": 1
     }
+    try:
+        SIMULATOR = QasmSimulator(**BACKEND_OPTS)
+    except AerError:
+        SIMULATOR = None
 
 
 if __name__ == '__main__':
