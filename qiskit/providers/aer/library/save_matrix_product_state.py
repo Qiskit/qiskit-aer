7e8740d49434e2d512775f48f78dd1ffcb527599
Merav Aharoni <merav@il.ibm.com>
2021-03-01 17:51:39 +0200

    Adding new files forgotten in previous commit

---
+++ qiskit/providers/aer/library/save_matrix_product_state.py
@@ -0,0 +1,72 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Simulator instruction to save matrix product state.
+"""
+
+from qiskit.circuit import QuantumCircuit
+from .save_data import SaveSingleData, default_qubits
+
+
+class SaveMatrixProductState(SaveSingleData):
+    """Save matrix product state instruction"""
+    def __init__(self, key, num_qubits, pershot=False, conditional=False):
+        """Create new instruction to save the matrix product state.
+
+        Args:
+            key (str): the key for retrieving saved data from results.
+            num_qubits (int): the number of qubits 
+            pershot (bool): if True save the mps for each
+                            shot of the simulation [Default: False].
+            conditional (bool): if True save data conditional on the current
+                                classical register values [Default: False].
+
+        .. note::
+
+            This save instruction must always be performed on the full width of
+            qubits in a circuit, otherwise an exception will be raised during
+            simulation.
+        """
+        super().__init__('save_matrix_product_state',
+                         key,
+                         num_qubits,
+                         pershot=pershot,
+                         conditional=conditional)
+
+
+def save_matrix_product_state(self, key, pershot=False, conditional=False):
+    """Save the current simulator quantum state as a matrix product state.
+
+    Args:
+        key (str): the key for retrieving saved data from results.
+        pershot (bool): if True save the mps for each
+                        shot of the simulation [Default: False].
+        conditional (bool): if True save pershot data conditional on the
+                            current classical register values
+                            [Default: False].
+
+    Returns:
+        QuantumCircuit: with attached instruction.
+
+    .. note:
+
+        This instruction is always defined across all qubits in a circuit.
+    """
+    qubits = default_qubits(self)
+    instr = SaveMatrixProductState(key,
+                           len(qubits),
+                           pershot=pershot,
+                           conditional=conditional)
+    return self.append(instr, qubits)
+
+
+QuantumCircuit.save_matrix_product_state = save_matrix_product_state
---
+++ test/terra/backends/qasm_simulator/qasm_save_matrix_product_state.py
@@ -0,0 +1,67 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+QasmSimulator Integration Tests for SaveMatrixProductState instruction
+"""
+
+import qiskit
+import numpy as np
+from qiskit import QuantumCircuit, assemble
+from qiskit.providers.aer import QasmSimulator
+from qiskit import execute
+
+class QasmSaveMatrixProductStateTests:
+    """QasmSimulator MatrixProductState instruction tests."""
+
+    SIMULATOR = QasmSimulator()
+    BACKEND_OPTS = {}
+
+    def test_save_matrix_product_state(self):
+        """Test save matrix_product_state for instruction"""
+
+        SUPPORTED_METHODS = ['matrix_product_state']
+
+        # Target mps structure
+        target = []
+        q_vec = [([[(1-0j), -0j]],
+           [[-0j, (1-0j)]]),
+          ([[(1-0j)], [-0j]],
+           [[-0j], [(1-0j)]]),
+          ([[(1+0j)]], [[0j]])],
+        lambda_vec = [[0.7071067811865475, 0.7071067811865475], [1.0]]
+        target.append(q_vec)
+        target.append(lambda_vec)
+
+        # Matrix product state test circuit
+        circ = QuantumCircuit(3)
+        circ.h(0)
+        circ.cx(0, 1)
+
+        # Add save to circuit
+        save_key = 'mps'
+        circ.save_matrix_product_state(save_key)
+
+        # Run
+        shots = 1
+        opts = self.BACKEND_OPTS.copy()
+        qobj = assemble(circ, self.SIMULATOR, shots=shots)
+        result = self.SIMULATOR.run(qobj, **opts).result()
+        method = opts.get('method', 'matrix_product_state')
+        if method not in SUPPORTED_METHODS:
+            self.assertFalse(result.success)
+        else:
+            self.assertTrue(result.success)
+            data = result.data(0)
+            self.assertIn(save_key, data)
+            value = result.data(0)[save_key]
+            print(value)
+            self.assertAlmostEqual(value, target)
