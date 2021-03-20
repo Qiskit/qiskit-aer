fdb25e06a50469d53292a683746ff6748f67c2e8
Hiroshi <horii@jp.ibm.com>
2021-03-20 18:42:07 +0900

    remove aqua from benchmark

--- test/benchmark/vqe_application.py
+++ test/benchmark/vqe_application.py
@@ -16,13 +16,6 @@ import sys
 import numpy as np
 import multiprocessing
 from time import time
-from qiskit import Aer
-from qiskit.aqua.algorithms import VQE
-from qiskit.aqua.components.optimizers import SLSQP
-from qiskit.chemistry.applications import MolecularGroundStateEnergy
-from qiskit.chemistry.components.initial_states import HartreeFock
-from qiskit.chemistry.components.variational_forms import UCCSD
-from qiskit.chemistry.drivers import PySCFDriver, UnitsType
 
 class UCCSDBenchmarkSuite:
 
@@ -41,33 +34,99 @@ class UCCSDBenchmarkSuite:
         self.param_names = ["mol"]
 
     def _run_uccsd_vqe(self, mol_string, method, threads):
-        driver = PySCFDriver(atom=mol_string, unit=UnitsType.ANGSTROM, charge=0, spin=0, basis='sto3g')
-        def cb_create_solver(num_particles, num_orbitals,
-                             qubit_mapping, two_qubit_reduction, z2_symmetries):
-            initial_state = HartreeFock(num_orbitals, num_particles, qubit_mapping,
-                                        two_qubit_reduction, z2_symmetries.sq_list)
-            var_form = UCCSD(num_orbitals=num_orbitals,
-                             num_particles=num_particles,
-                             initial_state=initial_state,
-                             qubit_mapping=qubit_mapping,
-                             two_qubit_reduction=two_qubit_reduction,
-                             z2_symmetries=z2_symmetries)
-            vqe = VQE(var_form=var_form, include_custom=True, optimizer=SLSQP(maxiter=5000), max_evals_grouped=256)
-            vqe.quantum_instance = Aer.get_backend('qasm_simulator')
-            vqe.quantum_instance.backend_options['backend_options'] = {'max_parallel_experiments':threads, 'method': method} 
+        from qiskit import Aer
+        from qiskit.algorithms import VQE
+        from qiskit.algorithms.optimizers import SLSQP
+        from qiskit_nature.circuit.library import HartreeFock
+        from qiskit_nature.components.variational_forms import UCCSD
+        from qiskit_nature.drivers import PySCFDriver, UnitsType
+        from qiskit_nature.mappers.second_quantization import ParityMapper
+        from qiskit_nature.operators.second_quantization.qubit_converter import QubitConverter
+        from qiskit_nature.transformations import (FermionicTransformation,
+                                                   FermionicTransformationType,
+                                                   FermionicQubitMappingType)
+        
+        driver = PySCFDriver(atom=mol_string,
+                                  unit=UnitsType.ANGSTROM,
+                                  charge=0,
+                                  spin=0,
+                                  basis='sto3g')
+        qubit_converter = QubitConverter(mappers=ParityMapper())
+        fermionic_transformation = \
+            FermionicTransformation(transformation=FermionicTransformationType.FULL,
+                                    qubit_mapping=FermionicQubitMappingType.PARITY,
+                                    two_qubit_reduction=True,
+                                    freeze_core=True,
+                                    orbital_reduction=[])
+ 
+        qubit_op, _ = fermionic_transformation.transform(driver)
+             
+        optimizer = SLSQP(maxiter=5000)
+         
+        num_spin_orbitals = fermionic_transformation.molecule_info['num_orbitals']
+        num_particles = fermionic_transformation.molecule_info['num_particles']
+        z2_symmetries = fermionic_transformation.molecule_info['z2_symmetries']
+         
+        init_state = HartreeFock(
+            num_spin_orbitals=num_spin_orbitals,
+            num_particles=num_particles,
+            qubit_converter=qubit_converter)
+        var_form = UCCSD(
+            num_orbitals=num_spin_orbitals,
+            num_particles=num_particles,
+            initial_state=initial_state,
+            qubit_mapping=fermionic_transformation._qubit_mapping,
+            two_qubit_reduction=fermionic_transformation._two_qubit_reduction,
+            max_evals_grouped=256,
+            z2_symmetries=z2_symmetries)
+         
+        quantum_instance = QuantumInstance(
+                         backend=Aer.get_backend('statevector_simulator'))
+        quantum_instance.backend_options['backend_options'] = {'max_parallel_experiments':threads, 'method': method}
+         
+        solver = VQE(var_form=var_form,
+                     optimizer=optimizer,
+                     quantum_instance=quantum_instance)
+         
+        gsc = GroundStateEigensolver(self.fermionic_transformation, solver)
+         
+        result = gsc.solve(self.driver)
+
             
-            return vqe
-        mgse = MolecularGroundStateEnergy(driver)
-        result = mgse.compute_energy(cb_create_solver)
-            
-    def time_statevector(self, mol_name):
+    def _time_statevector(self, mol_name):
         threads = multiprocessing.cpu_count()
         mol_string = self.mol_strings[mol_name][0]
         qubit = self.mol_strings[mol_name][1]
         self._run_uccsd_vqe(mol_string, 'statevector', threads)
 
-    def time_statevector_gpu(self, mol_name):
+    def _time_statevector_gpu(self, mol_name):
         threads = 1
         mol_string = self.mol_strings[mol_name][0]
         qubit = self.mol_strings[mol_name][1]
         self._run_uccsd_vqe(mol_string, 'statevector_gpu', threads)
+
+    def run_manual(self):
+        import timeout_decorator
+        @timeout_decorator.timeout(self.timeout)
+        def run_with_timeout (suite, method, molstring):
+            start = time()
+            return eval(f'suite._time_{method}("{molstring}")')
+        
+        #for runtime in self.runtime_names:
+        for method in [ 'statevector' ]:
+            for mol_string in self.mol_strings:
+                print (f'{self.__name__},uccsd,{method},{mol_string},', end="")
+                try:
+                    elapsed = run_with_timeout(self, method, mol_string)
+                    print ('{0}'.format(elapsed))
+                except ValueError as e:
+                    print ('{0}'.format(e))
+                except:
+                    import traceback
+                    traceback.print_exc(file=sys.stderr)
+                    print ('unknown error')
+
+if __name__ == "__main__":
+    benrhmarks = [ UCCSDBenchmarkSuite() ]
+    for benrhmark in benrhmarks:
+        benrhmark.run_manual()
