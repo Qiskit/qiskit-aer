4d395062df38c1517bc47ba00fc63a6902d26797
vvilpas <vvilpas@gmail.com>
2021-03-30 17:19:53 +0200

    Formating and removing unused code

--- src/framework/pybind_casts.hpp
+++ src/framework/pybind_casts.hpp
@@ -20,38 +20,34 @@
 namespace py = pybind11;
 
 namespace pybind11 {
-    namespace detail {
-        template <typename T> struct type_caster<matrix<T>>{
-        using base = type_caster_base<matrix<T>>;
-        public:
-        PYBIND11_TYPE_CASTER(matrix<T>, _("matrix_complex_t"));
-        // Conversion part 1 (Python->C++):
-        bool load(py::handle src, bool convert){
-            // TODO: Check if make sense have to flavors of matrix: F-style and C-style
-//                auto py_matrix = py::cast<py::array_t<T, py::array::f_style | py::array::forcecast>>(src);
-            auto py_matrix = py::cast<py::array_t<T>>(src);
-            auto flags = py_matrix.attr("flags").attr("carray").template cast<bool>();
-            if(py_matrix.ndim() != 2){
-                throw std::invalid_argument(std::string("Python: invalid matrix (empty array)."));
-            }
-            size_t ncols = py_matrix.shape(0);
-            size_t nrows = py_matrix.shape(1);
-            // Matrix looks ok, now we parse it
-            auto raw_mat = py_matrix.template unchecked<2>();
-//                auto raw_mat = static_cast<T *>(py_matrix.request().ptr);
-//                value = matrix<T>::copy_from_buffer(nrows, ncols, raw_mat);
-            value = matrix<T>(nrows, ncols, false);
-            for (size_t r = 0; r < nrows; r++) {
-                for (size_t c = 0; c < ncols; c++) {
-                    value(r, c) = raw_mat(r, c);
-                }
-            }
-            return true;
-        }
-        // Conversion part 2 (C++ -> Python):
-        static py::handle cast(matrix<T>, py::return_value_policy policy, py::handle parent){
-        throw std::runtime_error("Casting from matrix to python not supported.");
-    }
+namespace detail {
+template <typename T> struct type_caster<matrix<T>>{
+  using base = type_caster_base<matrix<T>>;
+public:
+  PYBIND11_TYPE_CASTER(matrix<T>, _("matrix_complex_t"));
+  // Conversion part 1 (Python->C++):
+  bool load(py::handle src, bool convert){
+      // TODO: Check if make sense have to flavors of matrix: F-style and C-style
+      auto py_matrix = py::cast<py::array_t<T>>(src);
+      if(py_matrix.ndim() != 2){
+          throw std::invalid_argument(std::string("Python: invalid matrix (empty array)."));
+      }
+      size_t ncols = py_matrix.shape(0);
+      size_t nrows = py_matrix.shape(1);
+      // Matrix looks ok, now we parse it
+      auto raw_mat = py_matrix.template unchecked<2>();
+      value = matrix<T>(nrows, ncols, false);
+      for (size_t r = 0; r < nrows; r++) {
+          for (size_t c = 0; c < ncols; c++) {
+              value(r, c) = raw_mat(r, c);
+          }
+      }
+      return true;
+  }
+  // Conversion part 2 (C++ -> Python):
+  static py::handle cast(matrix<T>, py::return_value_policy policy, py::handle parent){
+      throw std::runtime_error("Casting from matrix to python not supported.");
+  }
 };
 
 template <> struct type_caster<Clifford::Clifford>{
