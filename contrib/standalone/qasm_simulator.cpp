Qiskit/qiskit-aer

commit 1a51d273dec6e00f62495d2dca97d7f09e72755f

Fix various compiler warnings (#1201)
Co-authored-by: Victor Villar <vvilpas@gmail.com>
Co-authored-by: Christopher J. Wood <cjwood@us.ibm.com>

contrib/standalone/qasm_simulator.cpp
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
   int indent = 4;
   json_t qobj;
   json_t config;
-  int myrank=0,nprocs=1;
+  int myrank=0;
 
 #ifdef AER_MPI
   int prov;

src/simulators/state_chunk.hpp
@@ -447,7 +447,6 @@ void StateChunk<state_t>::allocate(uint_t num_qubits,uint_t block_bits)
 {
   int_t i;
   uint_t nchunks;
-  int max_bits = num_qubits;
 
   num_qubits_ = num_qubits;
   block_bits_ = block_bits;
@@ -876,7 +875,8 @@ uint_t StateChunk<state_t>::mapped_index(const uint_t idx)
 template <class state_t>
 void StateChunk<state_t>::apply_chunk_swap(const reg_t &qubits)
 {
-  uint_t q0,q1,nLarge;
+  uint_t nLarge = 1;
+  uint_t q0,q1;
   int_t iChunk;
 
   q0 = qubits[qubits.size() - 2];
@@ -1052,7 +1052,6 @@ void StateChunk<state_t>::apply_chunk_swap(const reg_t &qubits)
 template <class state_t>
 void StateChunk<state_t>::apply_chunk_x(const uint_t qubit)
 {
-  uint_t q0,q1,t,nLarge;
   int_t iChunk;
 
 

src/framework/matrix.hpp
@@ -477,7 +477,8 @@ template <class T> inline std::vector<T> matrix<T>::row_index(size_t row) const 
   ret.reserve(cols_);
   for(size_t i = 0; i < cols_; i++)
     ret.emplace_back(data_[i * rows_ + row]);
-  return std::move(ret);
+  // Allow for Named Return Value Optimization (NRVO) by not using std::move
+  return ret;
 }
 template <class T> inline std::vector<T> matrix<T>::col_index(size_t col) const {
 #ifdef DEBUG
@@ -492,7 +493,8 @@ template <class T> inline std::vector<T> matrix<T>::col_index(size_t col) const 
   // we want the elements for all rows i..rows_ and column col
   for(size_t i = 0; i < rows_; i++)
     ret.emplace_back(data_[col * rows_ + i]);
-  return std::move(ret);
+  // Allow for Named Return Value Optimization (NRVO) by not using std::move
+  return ret;
 }
 
 template <class T> inline size_t matrix<T>::GetRows() const {

src/simulators/density_matrix/densitymatrix_state_chunk.hpp
@@ -343,7 +343,7 @@ void State<densmat_t>::initialize_qreg(uint_t num_qubits,
       uint_t icol_chunk = ((iChunk + BaseState::global_chunk_index_) & ((1ull << ((BaseState::num_qubits_ - BaseState::chunk_bits_)))-1)) << (BaseState::chunk_bits_);
 
       //copy part of state for this chunk
-      uint_t i,row,col;
+      uint_t i;
       cvector_t tmp(1ull << BaseState::chunk_bits_);
       for(i=0;i<(1ull << BaseState::chunk_bits_);i++){
         uint_t icol = i & ((1ull << (BaseState::chunk_bits_))-1);
@@ -381,7 +381,7 @@ void State<densmat_t>::initialize_qreg(uint_t num_qubits,
       uint_t icol_chunk = ((iChunk + BaseState::global_chunk_index_) & ((1ull << ((BaseState::num_qubits_ - BaseState::chunk_bits_)))-1)) << (BaseState::chunk_bits_);
 
       //copy part of state for this chunk
-      uint_t i,row,col;
+      uint_t i;
       cvector_t tmp(1ull << BaseState::chunk_bits_);
       for(i=0;i<(1ull << BaseState::chunk_bits_);i++){
         uint_t icol = i & ((1ull << (BaseState::chunk_bits_))-1);
@@ -420,7 +420,7 @@ void State<densmat_t>::initialize_qreg(uint_t num_qubits,
       uint_t icol_chunk = ((iChunk + BaseState::global_chunk_index_) & ((1ull << ((BaseState::num_qubits_ - BaseState::chunk_bits_)))-1)) << (BaseState::chunk_bits_);
 
       //copy part of state for this chunk
-      uint_t i,row,col;
+      uint_t i;
       cvector_t tmp(1ull << BaseState::chunk_bits_);
       for(i=0;i<(1ull << BaseState::chunk_bits_);i++){
         uint_t icol = i & ((1ull << (BaseState::chunk_bits_))-1);
@@ -858,7 +858,6 @@ void State<densmat_t>::apply_snapshot(const Operations::Op &op,
                                       ExperimentResult &result,
                                       bool last_op) 
 {
-  int_t i;
   // Look for snapshot type in snapshotset
   auto it = DensityMatrix::State<densmat_t>::snapshotset_.find(op.name);
   if (it == DensityMatrix::State<densmat_t>::snapshotset_.end())
@@ -1369,11 +1368,11 @@ std::vector<reg_t> State<densmat_t>::sample_measure(const reg_t &qubits,
 
     if(nIn > 0){
       auto chunkSamples = BaseState::qregs_[i].sample_measure(vRnd);
-      uint_t irow;
-      irow = (BaseState::global_chunk_index_ + i) >> ((BaseState::num_qubits_ - BaseState::chunk_bits_));
+      uint_t ir;
+      ir = (BaseState::global_chunk_index_ + i) >> ((BaseState::num_qubits_ - BaseState::chunk_bits_));
 
       for(j=0;j<nIn;j++){
-        local_samples[vIdx[j]] = (irow << BaseState::chunk_bits_) + chunkSamples[j];
+        local_samples[vIdx[j]] = (ir << BaseState::chunk_bits_) + chunkSamples[j];
       }
     }
   }

src/simulators/extended_stabilizer/extended_stabilizer_state.hpp
@@ -836,7 +836,6 @@ double State::expval_pauli(const reg_t &qubits,
     auto phi_norm = state_cpy.norm_estimation(norm_estimation_samples_, norm_estimation_repetitions_, rng);
     std::vector<chpauli_t>paulis(1, chpauli_t());
     for (uint_t pos = 0; pos < qubits.size(); ++pos) {
-      uint_t qubit = qubits[pos];
       switch (pauli[pauli.size() - 1 - pos]) {
         case 'I':
           break;

src/simulators/statevector/qubitvector.hpp
@@ -791,7 +791,7 @@ void QubitVector<data_t>::allocate_mem(size_t data_size){
   // Allocate memory for new vector
   if (data_ == nullptr) {
 #if !defined(_WIN64) && !defined(_WIN32)
-    void* data;
+    void* data = nullptr;
     posix_memalign(&data, 64, sizeof(std::complex<data_t>) * data_size);
     data_ = reinterpret_cast<std::complex<data_t>*>(data);
 #else
@@ -804,7 +804,7 @@ template <typename data_t>
 void QubitVector<data_t>::allocate_checkpoint(size_t data_size){
   free_checkpoint();
 #if !defined(_WIN64) && !defined(_WIN32)
-  void* data;
+  void* data = nullptr;
   posix_memalign(&data, 64, sizeof(std::complex<data_t>) * data_size);
   checkpoint_ = reinterpret_cast<std::complex<data_t>*>(data);
 #else

src/simulators/statevector/qv_avx2.cpp
@@ -182,10 +182,10 @@ struct RealVectorView {
   RealVectorView() = delete;
   // Unfortunately, shared_ptr implies allocations and we cannot afford
   // them in this piece of code, so this is the reason to use raw pointers.
-  RealVectorView(FloatType* data) : data(data) {}
-  inline FloatType* operator[](size_t i) { return &data[i * 2]; }
-  inline const FloatType* operator[](size_t i) const { return &data[i * 2]; }
-  FloatType* data = nullptr;
+  RealVectorView(FloatType* data) : data_(data) {}
+  inline FloatType* operator[](size_t i) { return &data_[i * 2]; }
+  inline const FloatType* operator[](size_t i) const { return &data_[i * 2]; }
+  FloatType* data_ = nullptr;
 };
 
 template <typename FloatType>
@@ -194,24 +194,24 @@ struct ImaginaryVectorView : std::false_type {};
 template <>
 struct ImaginaryVectorView<double> {
   ImaginaryVectorView() = delete;
-  ImaginaryVectorView(double* data) : data(data) {}
+  ImaginaryVectorView(double* data) : data_(data) {}
   // SIMD vectorization takes n bytes depending on the underlaying type, so
   // for doubles, SIMD loads 4 consecutive values (4 * sizeof(double) = 32
   // bytes)
-  inline double* operator[](size_t i) { return &data[i * 2 + 4]; }
-  inline const double* operator[](size_t i) const { return &data[i * 2 + 4]; }
-  double* data = nullptr;
+  inline double* operator[](size_t i) { return &data_[i * 2 + 4]; }
+  inline const double* operator[](size_t i) const { return &data_[i * 2 + 4]; }
+  double* data_ = nullptr;
 };
 
 template <>
 struct ImaginaryVectorView<float> {
   ImaginaryVectorView() = delete;
-  ImaginaryVectorView(float* data) : data(data) {}
+  ImaginaryVectorView(float* data) : data_(data) {}
   // SIMD vectorization takes n bytes depending on the underlaying type, so
   // for floats, SIMD loads 8 consecutive (8 * sizeof(float) = 32 bytes)
-  inline float* operator[](size_t i) { return &data[i * 2 + 8]; }
-  inline const float* operator[](size_t i) const { return &data[i * 2 + 8]; }
-  float* data = nullptr;
+  inline float* operator[](size_t i) { return &data_[i * 2 + 8]; }
+  inline const float* operator[](size_t i) const { return &data_[i * 2 + 8]; }
+  float* data_ = nullptr;
 };
 
 static auto _mm256_mul(const m256_t<double>& left,
@@ -951,29 +951,29 @@ inline Avx _apply_avx_kernel(const uint64_t* qregs,
   ImaginaryVectorView<float> img = {data};
 
   if (num_qubits > 2 && qregs[2] == 2) {
-    auto lambda = [&](const uint64_t index0, const float* mat) -> void {
-      _apply_matrix_float_avx_q0q1q2<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const float* m) -> void {
+      _apply_matrix_float_avx_q0q1q2<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 1, lambda, qregs, omp_threads, mat);
 
   } else if (num_qubits > 1 && qregs[1] < 3) {
-    auto lambda = [&](const uint64_t index0, const float* mat) -> void {
-      _apply_matrix_float_avx_qLqL<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const float* m) -> void {
+      _apply_matrix_float_avx_qLqL<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 2, lambda, qregs, omp_threads, mat);
 
   } else if (qregs[0] < 3) {
-    auto lambda = [&](const uint64_t index0, const float* mat) -> void {
-      _apply_matrix_float_avx_qL<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const float* m) -> void {
+      _apply_matrix_float_avx_qL<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 4, lambda, qregs, omp_threads, mat);
 
   } else {
-    auto lambda = [&](const uint64_t index0, const float* mat) -> void {
-      _apply_matrix_float_avx<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const float* m) -> void {
+      _apply_matrix_float_avx<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 8, lambda, qregs, omp_threads, mat);
@@ -991,22 +991,22 @@ inline Avx _apply_avx_kernel(const uint64_t* qregs,
   ImaginaryVectorView<double> img = {data};
 
   if (num_qubits > 1 && qregs[1] == 1) {
-    auto lambda = [&](const uint64_t index0, const double* mat) -> void {
-      _apply_matrix_double_avx_q0q1<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const double* m) -> void {
+      _apply_matrix_double_avx_q0q1<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 1, lambda, qregs, omp_threads, mat);
 
   } else if (qregs[0] < 2) {
-    auto lambda = [&](const uint64_t index0, const double* mat) -> void {
-      _apply_matrix_double_avx_qL<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const double* m) -> void {
+      _apply_matrix_double_avx_qL<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 2, lambda, qregs, omp_threads, mat);
 
   } else {
-    auto lambda = [&](const uint64_t index0, const double* mat) -> void {
-      _apply_matrix_double_avx<num_qubits>(real, img, mat, qregs, index0);
+    auto lambda = [&](const uint64_t index0, const double* m) -> void {
+      _apply_matrix_double_avx<num_qubits>(real, img, m, qregs, index0);
     };
 
     avx_apply_lambda<num_qubits>(data_size, 4, lambda, qregs, omp_threads, mat);
@@ -1180,7 +1180,7 @@ Avx apply_diagonal_matrix_avx<double>(double* qv_data_,
 #pragma omp parallel if (omp_threads > 1) num_threads(omp_threads)
   {
 #if !defined(_WIN64) && !defined(_WIN32)
-    void* data;
+    void* data = nullptr;
     posix_memalign(&data, 64, sizeof(std::complex<double>) * 2);
     tmps[_omp_get_thread_num()] = reinterpret_cast<std::complex<double>*>(data);
 #else
@@ -1198,13 +1198,13 @@ Avx apply_diagonal_matrix_avx<double>(double* qv_data_,
   const size_t q0_mask = q0_mask_;
   const auto batch = (data_size <= (1UL << 5) ? 0 : 4);
 
-  auto lambda = [&](const uint64_t i_, const std::complex<double>* input_vec) -> void {
+  auto lambda = [&](const uint64_t i_, const std::complex<double>* in_vec) -> void {
     const auto base = i_ << (batch + 1);
     const auto until = base + (1UL << (batch + 1));
     std::complex<double>* tmp = tmps[_omp_get_thread_num()];
     for (auto i = base; i < until; i+=2) {
       auto tgt_qv_data = _mm256_load(reinterpret_cast<double*>(&(qv_data[i])));
-      auto input_data = _load_diagonal_input(input_vec, tmp, i, qregs, qregs_size, q0_mask);
+      auto input_data = _load_diagonal_input(in_vec, tmp, i, qregs, qregs_size, q0_mask);
       _mm_complex_multiply<double>(tgt_qv_data, input_data);
       _mm256_store(reinterpret_cast<double*>(&(qv_data[i])), tgt_qv_data);
     }
@@ -1241,7 +1241,7 @@ Avx apply_diagonal_matrix_avx<float>(float* qv_data_,
 #pragma omp parallel if (omp_threads > 1) num_threads(omp_threads)
   {
   #if !defined(_WIN64) && !defined(_WIN32)
-    void* data;
+    void* data = nullptr;
     posix_memalign(&data, 64, sizeof(std::complex<float>) * 4);
     tmps[_omp_get_thread_num()] = reinterpret_cast<std::complex<float>*>(data);
   #else
@@ -1263,13 +1263,13 @@ Avx apply_diagonal_matrix_avx<float>(float* qv_data_,
   const size_t q1_mask = q1_mask_;
   const auto batch = (data_size <= (1UL << 6) ? 0 : 4);
 
-  auto lambda = [&](const uint64_t i_, const std::complex<float>* input_vec) -> void {
+  auto lambda = [&](const uint64_t i_, const std::complex<float>* in_vec) -> void {
     const auto base = i_ << (batch + 2);
     const auto until = base + (1UL << (batch + 2));
     std::complex<float>* tmp = tmps[_omp_get_thread_num()];
     for (auto i = base; i < until; i+=4) {
       m256_t<float> tgt_qv_data = _mm256_load(reinterpret_cast<float*>(&(qv_data[i])));
-      auto input_data = _load_diagonal_input(input_vec, tmp, i, qregs, qregs_size, q0_mask, q1_mask);
+      auto input_data = _load_diagonal_input(in_vec, tmp, i, qregs, qregs_size, q0_mask, q1_mask);
       _mm_complex_multiply<float>(tgt_qv_data, input_data);
       _mm256_store(reinterpret_cast<float*>(&(qv_data[i])), tgt_qv_data);
     }

src/simulators/statevector/statevector_state_chunk.hpp
@@ -387,9 +387,8 @@ void State<statevec_t>::initialize_qreg(uint_t num_qubits,
 
   initialize_omp();
 
-  int_t iChunk;
   if(BaseState::chunk_bits_ == BaseState::num_qubits_){
-    for(iChunk=0;iChunk<BaseState::num_local_chunks_;iChunk++){
+    for(int_t iChunk=0;iChunk<BaseState::num_local_chunks_;iChunk++){
       BaseState::qregs_[iChunk].set_num_qubits(BaseState::chunk_bits_);
       BaseState::qregs_[iChunk].initialize_from_vector(state);
     }
@@ -862,7 +861,6 @@ void State<statevec_t>::apply_snapshot(const Operations::Op &op,
                                        ExperimentResult &result,
                                        bool last_op) 
 {
-  int_t i;
   // Look for snapshot type in snapshotset
   auto it = Statevector::State<statevec_t>::snapshotset_.find(op.name);
   if (it == Statevector::State<statevec_t>::snapshotset_.end())
@@ -1675,7 +1673,7 @@ void State<statevec_t>::apply_kraus(const reg_t &qubits,
   double accum = 0.;
   bool complete = false;
 
-  int_t i,j;
+  int_t i;
   cvector_t vmat;
   double local_accum;
 

commit ca126486c262e60cfd7434134314a30fd1d48ede

Fix compiler errors when using MPI
This commit fixes compiler warnings that were caused by Commit
1a51d273dec6e00f62495d2dca97d7f09e72755f ("Fix various compiler
warnings (#1201)").

Fixes: https://github.com/Qiskit/qiskit-aer/issues/1234

contrib/standalone/qasm_simulator.cpp
@@ -99,6 +99,7 @@ int main(int argc, char **argv) {
 
 #ifdef AER_MPI
   int prov;
+  int nprocs=1;
   MPI_Init_thread(&argc,&argv,MPI_THREAD_MULTIPLE,&prov);
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);

src/simulators/state_chunk.hpp
@@ -1205,6 +1205,7 @@ template <class state_t>
 void StateChunk<state_t>::apply_chunk_x(const uint_t qubit)
 {
   int_t iChunk;
+  uint_t nLarge = 1;
 
 
   if(qubit < chunk_bits_*qubit_scale()){