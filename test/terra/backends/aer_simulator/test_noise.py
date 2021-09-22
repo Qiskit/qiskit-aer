30ad5dde6a38c0452cf4a1e8b987ade2d09b4600
Christopher Wood <cjwood@us.ibm.com>
2021-09-22 12:20:49 -0400

    Add backend tests for circuits containing quantum error

--- test/terra/backends/aer_simulator/test_noise.py
+++ test/terra/backends/aer_simulator/test_noise.py
@@ -17,7 +17,8 @@ from ddt import ddt
 
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import QFT
-from qiskit.providers.aer.noise import NoiseModel, depolarizing_error
+import qiskit.quantum_info as qi
+from qiskit.providers.aer import noise
 
 from test.terra.backends.simulator_test_case import (
     SimulatorTestCase, supported_methods)
@@ -40,8 +41,8 @@ class TestNoise(SimulatorTestCase):
     def test_empty_circuit_noise(self, method, device):
         """Test simulation with empty circuit and noise model."""
         backend = self.backend(method=method, device=device)
-        noise_model = NoiseModel()
-        noise_model.add_all_qubit_quantum_error(depolarizing_error(0.1, 1), ['x'])
+        noise_model = noise.NoiseModel()
+        noise_model.add_all_qubit_quantum_error(noise.depolarizing_error(0.1, 1), ['x'])
         result = backend.run(
             QuantumCircuit(), shots=1, noise_model=noise_model).result()
         self.assertSuccess(result)
@@ -164,3 +165,56 @@ class TestNoise(SimulatorTestCase):
         result = backend.run(circuit, shots=shots).result()
         self.assertSuccess(result)
         self.compare_counts(result, [circuit], [ref_target], delta=0.1 * shots)
+
+    @supported_methods(ALL_METHODS)
+    def test_clifford_circuit_noise(self, method, device):
+        """Test simulation with mixed Clifford quantum errors in circuit."""
+        backend = self.backend(method=method, device=device)
+        shots = 1000
+        error1 = noise.QuantumError([
+            ([{"name": "id", "qubits": [0]}], 0.8),
+            ([{"name": "reset", "qubits": [0]}], 0.1),
+            ([{"name": "h", "qubits": [0]}], 0.1)])
+
+        error2 = noise.QuantumError([
+            ([{"name": "id", "qubits": [0]}], 0.75),
+            ([{"name": "reset", "qubits": [0]}], 0.1),
+            ([{"name": "reset", "qubits": [1]}], 0.1),
+            ([{"name": "reset", "qubits": [0]},
+              {"name": "reset", "qubits": [1]}], 0.05)])
+
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.append(error1, [0])
+        qc.cx(0, 1)
+        qc.append(error2, [0, 1])
+        target_probs = qi.DensityMatrix(qc).probabilities_dict()
+
+        # Add measurement
+        qc.measure_all()
+        result = backend.run(qc, shots=shots).result()
+        self.assertSuccess(result)
+        probs = {key: val / shots for key, val in result.get_counts(0).items()}
+        self.assertDictAlmostEqual(target_probs, probs, delta=0.1)
+
+    @supported_methods(['automatic', 'statevector', 'density_matrix'])
+    def test_kraus_circuit_noise(self, method, device):
+        """Test simulation with Kraus quantum errors in circuit."""
+        backend = self.backend(method=method, device=device)
+        shots = 1000
+        error1 = noise.amplitude_damping_error(0.05)
+        error2 = error1.tensor(error1)
+
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.append(error1, [0])
+        qc.cx(0, 1)
+        qc.append(error2, [0, 1])
+        target_probs = qi.DensityMatrix(qc).probabilities_dict()
+
+        # Add measurement
+        qc.measure_all()
+        result = backend.run(qc, shots=shots).result()
+        self.assertSuccess(result)
+        probs = {key: val / shots for key, val in result.get_counts(0).items()}
+        self.assertDictAlmostEqual(target_probs, probs, delta=0.1)
