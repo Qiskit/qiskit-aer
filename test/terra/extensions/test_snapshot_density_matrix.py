38da2dccf56ab6cf2bd85254b1ff5b90142b4032
Jake Lishman <jake.lishman@ibm.com>
2021-09-28 20:25:57 +0100

    Add missing classmethod decorators from setUpClass (#1361)

--- test/terra/extensions/test_snapshot_density_matrix.py
+++ test/terra/extensions/test_snapshot_density_matrix.py
@@ -23,6 +23,7 @@ from ..common import QiskitAerTestCase
 class TestSnapshotDensityMatrixExtension(QiskitAerTestCase):
     """SnapshotDensityMatrix extension tests"""
 
+    @classmethod
     def setUpClass(cls):
         super().setUpClass()
         filterwarnings(
