0d95c93a969e45154383a1523842328de2cfbc1b
Christopher J. Wood <cjwood@us.ibm.com>
2021-11-08 13:54:05 -0500

    Add support for adding QuantumError to circuits (#1350)

--- qiskit/providers/aer/backends/qasm_simulator.py
+++ qiskit/providers/aer/backends/qasm_simulator.py
@@ -303,12 +303,13 @@ class QasmSimulator(AerBackend):
     ])
 
     _DEFAULT_CUSTOM_INSTR = sorted([
-        'roerror', 'kraus', 'snapshot', 'save_expval', 'save_expval_var',
+        'quantum_channel', 'qerror_loc', 'roerror', 'kraus', 'snapshot',
+        'save_expval', 'save_expval_var',
         'save_probabilities', 'save_probabilities_dict',
         'save_amplitudes', 'save_amplitudes_sq', 'save_state',
         'save_density_matrix', 'save_statevector', 'save_statevector_dict',
         'save_stabilizer', 'set_statevector', 'set_density_matrix',
-        'set_stabilizer'
+        'set_stabilizer',
     ])
 
     _DEFAULT_CONFIGURATION = {
@@ -616,35 +617,37 @@ class QasmSimulator(AerBackend):
         method = self._options.get('method', None)
         if method in ['statevector', 'statevector_gpu', 'statevector_thrust']:
             return sorted([
-                'roerror', 'kraus', 'snapshot', 'save_expval', 'save_expval_var',
-                'save_probabilities', 'save_probabilities_dict',
+                'quantum_channel', 'qerror_loc', 'roerror', 'kraus', 'snapshot', 'save_expval',
+                'save_expval_var', 'save_probabilities', 'save_probabilities_dict',
                 'save_amplitudes', 'save_amplitudes_sq', 'save_state',
                 'save_density_matrix', 'save_statevector', 'save_statevector_dict',
                 'set_statevector'
             ])
         if method in ['density_matrix', 'density_matrix_gpu', 'density_matrix_thrust']:
             return sorted([
-                'roerror', 'kraus', 'superop', 'snapshot', 'save_expval', 'save_expval_var',
-                'save_probabilities', 'save_probabilities_dict',
+                'quantum_channel', 'qerror_loc', 'roerror', 'kraus', 'superop', 'snapshot',
+                'save_expval', 'save_expval_var', 'save_probabilities', 'save_probabilities_dict',
                 'save_state', 'save_density_matrix', 'save_amplitudes_sq',
                 'set_statevector', 'set_density_matrix'
             ])
         if method == 'matrix_product_state':
             return sorted([
-                'roerror', 'snapshot', 'kraus', 'save_expval', 'save_expval_var',
-                'save_probabilities', 'save_probabilities_dict',
+                'quantum_channel', 'qerror_loc', 'roerror', 'snapshot', 'kraus', 'save_expval',
+                'save_expval_var', 'save_probabilities', 'save_probabilities_dict',
                 'save_density_matrix', 'save_state', 'save_statevector',
                 'save_amplitudes', 'save_amplitudes_sq', 'save_matrix_product_state',
                 'set_matrix_product_state'])
         if method == 'stabilizer':
             return sorted([
-                'roerror', 'snapshot', 'save_expval', 'save_expval_var',
-                'save_probabilities', 'save_probabilities_dict',
+                'quantum_channel', 'qerror_loc', 'roerror', 'snapshot', 'save_expval',
+                'save_expval_var', 'save_probabilities', 'save_probabilities_dict',
                 'save_amplitudes_sq', 'save_state', 'save_stabilizer',
                 'set_stabilizer'
             ])
         if method == 'extended_stabilizer':
-            return sorted(['roerror', 'snapshot', 'save_statevector'])
+            return sorted([
+                'quantum_channel', 'qerror_loc', 'roerror',
+                'snapshot', 'save_statevector'])
         return QasmSimulator._DEFAULT_CUSTOM_INSTR
 
     def _set_method_config(self, method=None):
