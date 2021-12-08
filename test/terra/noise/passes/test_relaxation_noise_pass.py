cf1e2ede269fb833642b6a38b87d9c529222a5be
Toshinari Itoko <itoko@jp.ibm.com>
2021-12-08 17:21:28 +0900

    Improve test

--- test/terra/noise/passes/test_relaxation_noise_pass.py
+++ test/terra/noise/passes/test_relaxation_noise_pass.py
@@ -13,17 +13,50 @@
 RelaxationNoisePass class tests
 """
 
+from qiskit.providers.aer.noise.errors import thermal_relaxation_error
 from qiskit.providers.aer.noise.passes import RelaxationNoisePass
 
+import qiskit.quantum_info as qi
 from qiskit.circuit import QuantumCircuit, Delay
 from qiskit.compiler import transpile
-from qiskit.test.mock import FakeLagos
 from qiskit.transpiler import TranspilerError
 from test.terra.common import QiskitAerTestCase
 
 
 class TestRelaxationNoisePass(QiskitAerTestCase):
     """Testing RelaxationNoisePass class"""
+
+    def test_delay_circuit_on_single_qubit(self):
+        t1s = [0.10, 0.11]
+        t2s = [0.20, 0.21]
+        dt = 0.01
+        duration = 100
+
+        qc = QuantumCircuit(1)
+        qc.delay(duration, 0)
+
+        relax_pass = RelaxationNoisePass(t1s=t1s, t2s=t2s, dt=dt)
+        actual = qi.SuperOp(relax_pass(qc))
+        expected = qi.SuperOp(thermal_relaxation_error(t1s[0], t2s[0], duration * dt))
+        self.assertEqual(expected, actual)
+
+    def test_delay_circuit_on_multi_qubits(self):
+        t1s = [0.10, 0.11]
+        t2s = [0.20, 0.21]
+        dt = 0.01
+        duration = 100
+
+        qc = QuantumCircuit(2)
+        qc.delay(duration, 0)
+        qc.delay(duration, 1)
+
+        relax_pass = RelaxationNoisePass(t1s=t1s, t2s=t2s, dt=dt)
+        actual = qi.SuperOp(relax_pass(qc))
+
+        noise0 = thermal_relaxation_error(t1s[0], t2s[0], duration * dt)
+        noise1 = thermal_relaxation_error(t1s[1], t2s[1], duration * dt)
+        expected = qi.SuperOp(noise0.expand(noise1))
+        self.assertEqual(expected, actual)
 
     def test_default_with_scheduled_circuit(self):
         """Test adding noises to all ops in a scheduled circuit."""
@@ -32,17 +65,12 @@ class TestRelaxationNoisePass(QiskitAerTestCase):
         qc.cx(0, 1)
         qc.measure([0, 1], [0, 1])
 
-        backend = FakeLagos()
+        durations = [("h", None, 10), ("cx", None, 50), ("measure", None, 200)]
+        sched_circ = transpile(qc, scheduling_method='alap', instruction_durations=durations)
 
-        sched_circ = transpile(qc, backend, scheduling_method='alap')
-
-        noise_pass = RelaxationNoisePass(
-            t1s=[backend.properties().t1(q) for q in range(backend.configuration().num_qubits)],
-            t2s=[backend.properties().t2(q) for q in range(backend.configuration().num_qubits)],
-            dt=backend.configuration().dt
-        )
+        noise_pass = RelaxationNoisePass(t1s=[0.10, 0.11], t2s=[0.20, 0.21], dt=0.01)
         noisy_circ = noise_pass(sched_circ)
-        self.assertEqual(9, noisy_circ.count_ops()["quantum_channel"])
+        self.assertEqual(6, noisy_circ.decompose().decompose().count_ops()["kraus"])
 
     def test_default_with_non_scheduled_circuit(self):
         """Test never adding noises to non-scheduled circuit."""
@@ -51,11 +79,7 @@ class TestRelaxationNoisePass(QiskitAerTestCase):
         qc.cx(0, 1)
         qc.measure([0, 1], [0, 1])
 
-        noise_pass = RelaxationNoisePass(
-            t1s=[0.10, 0.11],
-            t2s=[0.20, 0.21],
-            dt=0.01,
-        )
+        noise_pass = RelaxationNoisePass(t1s=[0.10, 0.11], t2s=[0.20, 0.21], dt=0.01)
         with self.assertWarns(UserWarning):
             noisy_circ = noise_pass(qc)
 
@@ -63,29 +87,30 @@ class TestRelaxationNoisePass(QiskitAerTestCase):
 
     def test_raise_if_supplied_invalid_ops(self):
         with self.assertRaises(TranspilerError):
-            RelaxationNoisePass(
-                t1s=[1],
-                t2s=[1],
-                dt=1,
-                op_types="delay",  # str is invalid
-            )
+            RelaxationNoisePass(t1s=[1], t2s=[1], dt=1, op_types="delay")  # str is invalid
 
-    def test_ops_option_with_scheduled_circuit(self):
+    def test_ops_types(self):
         """Test adding noises only to delays in a scheduled circuit."""
+        t1s = [0.10, 0.11]
+        t2s = [0.20, 0.21]
+        dt = 0.01
+
         qc = QuantumCircuit(2, 2)
         qc.h(0)
         qc.cx(0, 1)
         qc.measure([0, 1], [0, 1])
 
-        backend = FakeLagos()
+        durations = [("h", None, 10), ("cx", None, 50), ("measure", None, 200)]
+        sched_circ = transpile(qc, scheduling_method='alap', instruction_durations=durations)
 
-        sched_circ = transpile(qc, backend, scheduling_method='alap')
+        noise_pass = RelaxationNoisePass(t1s=t1s, t2s=t2s, dt=dt, op_types=Delay)
+        noisy_circ = noise_pass(sched_circ)
 
-        delay_pass = RelaxationNoisePass(
-            t1s=[backend.properties().t1(q) for q in range(backend.configuration().num_qubits)],
-            t2s=[backend.properties().t2(q) for q in range(backend.configuration().num_qubits)],
-            dt=backend.configuration().dt,
-            op_types=Delay,
-        )
-        noisy_circ = delay_pass(sched_circ)
-        self.assertEqual(6, noisy_circ.count_ops()["quantum_channel"])
+        expected = QuantumCircuit(2, 2)
+        expected.h(0)
+        expected.delay(10, 1)
+        expected.append(thermal_relaxation_error(t1s[1], t2s[1], 10 * dt).to_instruction(), [1])
+        expected.cx(0, 1)
+        expected.measure([0, 1], [0, 1])
+
+        self.assertEqual(expected, noisy_circ)
