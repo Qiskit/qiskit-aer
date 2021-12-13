f887cdd4c7e20f6aa93edc8312cc43477a4b6128
Jake Lishman <jake.lishman@ibm.com>
2021-12-13 15:55:15 +0000

    Handle ScheduleBlock in noise model assembly

--- qiskit/providers/aer/backends/aerbackend.py
+++ qiskit/providers/aer/backends/aerbackend.py
@@ -26,7 +26,7 @@ from qiskit.circuit import QuantumCircuit
 from qiskit.compiler import assemble
 from qiskit.providers import BackendV1 as Backend
 from qiskit.providers.models import BackendStatus
-from qiskit.pulse import Schedule
+from qiskit.pulse import Schedule, ScheduleBlock
 from qiskit.qobj import QasmQobj, PulseQobj
 from qiskit.result import Result
 from qiskit.utils import deprecate_arguments
@@ -367,7 +367,7 @@ class AerBackend(Backend, ABC):
         if isinstance(circuits, (QasmQobj, PulseQobj)):
             return circuits, run_options
 
-        if isinstance(circuits, (QuantumCircuit, Schedule)):
+        if isinstance(circuits, (QuantumCircuit, Schedule, ScheduleBlock)):
             circuits = [circuits]
 
         # Flag for if we need to make a deep copy of the noise model
@@ -388,7 +388,7 @@ class AerBackend(Backend, ABC):
         # Check if circuits contain quantum error instructions
         run_circuits = []
         for circ in circuits:
-            if isinstance(circ, Schedule):
+            if isinstance(circ, (Schedule, ScheduleBlock)):
                 run_circuits.append(circ)
             else:
                 updated_circ = False
