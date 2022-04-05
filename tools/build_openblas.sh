12eea452c1ec6d2b671193ac422eeea0cdf74242
Matthew Treinish <mtreinish@kortar.org>
2022-04-05 07:18:52 -0400

    Try building openblas from source on i686

---
+++ tools/build_openblas.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+SOURCE_URL="https://github.com/xianyi/OpenBLAS/releases/download/v0.3.20/OpenBLAS-0.3.20.tar.gz"
+
+pushd /tmp
+curl -L $SOURCE_URL -o openblas.tar.gz
+tar xzvf openblas.tar.gz
+pushd OpenBLAS-0.3.20
+DYNAMIC_ARCH=1 make
+sudo make install
+popd
+popd
