c7bef3361c860083bd5982a168f0cee8194080da
Matthew Treinish <mtreinish@kortar.org>
2022-04-05 21:44:33 -0400

    Install Rust compiler and build terra from source in CI (#1498)
    Starting with the recently released qiskit-terra 0.20.0 qiskit only
    builds packages on manylinux2014 now. This commit update the wheel jobs
    for Aer to install the rust compiler and build terra from source when CI uses 
    manylinux2010 for i686 builds.
    In manylinux2014 for i686, binary of OpenBLAS is not available and its installation
    from source codes is necessary. Installation of rust compiler and terra is a
    lot less effort than compiling OpenBLAS.
    See the details in https://github.com/Qiskit/qiskit-aer/pull/1498.
    
    * Thread control for Thrust CPU is now same as device=CPU
    
    This commit also includes #1497, which fix performance issue in device=Thrust.
    A cause of this issue is that Thrust CPU always applied OpenMP for threading
    kernel programs without referring to the number of qubits.
    This fix applies OpenMP referring to statevector_parallel_threshold parameter
    as same as device=CPU.
    See the details in https://github.com/Qiskit/qiskit-aer/pull/1497.
    
    Co-authored-by: Jun Doi <doichan@jp.ibm.com>

--- src/simulators/statevector/chunk/chunk_container.hpp
+++ src/simulators/statevector/chunk/chunk_container.hpp
@@ -108,6 +108,8 @@ protected:
   bool keep_conditional_bit_;         //keep conditional bit alive
   int_t num_pow2_qubits_;             //largest number of qubits that meets num_chunks_ = m*(2^num_pow2_qubits_)
   bool density_matrix_;
+
+  int_t omp_threads_;                 //number of threads can be used for parallelization on CPU
 public:
   ChunkContainer()
   {
@@ -121,6 +123,7 @@ public:
     keep_conditional_bit_ = false;
     matrix_bits_ = AER_DEFAULT_MATRIX_BITS;
     density_matrix_ = false;
+    omp_threads_ = 1;
   }
   virtual ~ChunkContainer(){}
 
@@ -195,6 +198,11 @@ public:
   void set_chunk_index(uint_t chunk_index)
   {
     chunk_index_ = chunk_index;
+  }
+
+  void set_omp_threads(int_t nthreads)
+  {
+    omp_threads_ = nthreads;
   }
 
   virtual thrust::complex<data_t>& operator[](uint_t i) = 0;
@@ -485,7 +493,10 @@ void ChunkContainer<data_t>::Execute(Function func,uint_t iChunk,uint_t count)
   else
     size = count * func.size(chunk_bits_);
   auto ci = thrust::counting_iterator<uint_t>(0);
-  thrust::for_each_n(thrust::device, ci , size, func);
+  if(omp_threads_ > 1)
+    thrust::for_each_n(thrust::device, ci , size, func);
+  else
+    thrust::for_each_n(thrust::seq, ci , size, func);
 #endif
 
 }
@@ -642,7 +653,10 @@ void ChunkContainer<data_t>::ExecuteSum(double* pSum,Function func,uint_t iChunk
     auto ci = thrust::counting_iterator<uint_t>(0);
 
     double sum;
-    sum = thrust::transform_reduce(thrust::device, ci, ci + size, func,0.0,thrust::plus<double>());
+    if(omp_threads_ > 1)
+      sum = thrust::transform_reduce(thrust::device, ci, ci + size, func,0.0,thrust::plus<double>());
+    else
+      sum = thrust::transform_reduce(thrust::seq, ci, ci + size, func,0.0,thrust::plus<double>());
     if(count == 1 && pSum){
       *pSum = sum;
     }
@@ -783,7 +797,10 @@ void ChunkContainer<data_t>::ExecuteSum2(double* pSum,Function func,uint_t iChun
 
     auto ci = thrust::counting_iterator<uint_t>(0);
 
-    ret = thrust::transform_reduce(thrust::device, ci, ci + size, func,zero,complex_sum());
+    if(omp_threads_ > 1)
+      ret = thrust::transform_reduce(thrust::device, ci, ci + size, func,zero,complex_sum());
+    else
+      ret = thrust::transform_reduce(thrust::seq, ci, ci + size, func,zero,complex_sum());
 
     if(count == 1 && pSum){
       *((thrust::complex<double>*)pSum) = ret;
