10054dde00954461242de64eb2561676c605d677
Jake Lishman <jake.lishman@ibm.com>
2022-06-27 15:26:18 +0100

    Remove deprecated use of qiskit.test.mock
    Terra 0.21 triggers deprecation warnings for `qiskit.test.mock`, in
    favour of `qiskit.providers.fake_provider`, which has been available
    since Terra 0.20.  The only uses are in the tests, but it still
    technically requires a version bump in the Terra requirement.  It's
    fairly likely something else will bump the Terra version further before
    release of Aer 0.11.

--- test/terra/backends/test_config_pulse_simulator.py
+++ test/terra/backends/test_config_pulse_simulator.py
@@ -20,8 +20,8 @@ import numpy as np
 from test.terra import common
 
 from qiskit import QuantumCircuit, transpile, schedule
-from qiskit.test.mock.backends.armonk.fake_armonk import FakeArmonk
-from qiskit.test.mock.backends.athens.fake_athens import FakeAthens
+from qiskit.providers.fake_provider.backends.armonk.fake_armonk import FakeArmonk
+from qiskit.providers.fake_provider.backends.athens.fake_athens import FakeAthens
 
 from qiskit.providers.aer.backends import PulseSimulator
 from qiskit.pulse import (Schedule, Play, ShiftPhase, SetPhase, Delay, Acquire,
