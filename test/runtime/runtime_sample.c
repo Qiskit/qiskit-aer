4acf422544f2951ab44543bed8c0689129104e1d
Hiroshi Horii <horii@jp.ibm.com>
2022-08-27 11:15:30 +0900

    update sample of aer_runtime

--- test/runtime/runtime_sample.c
+++ test/runtime/runtime_sample.c
@@ -13,7 +13,7 @@ int main() {
 
   printf("%d-qubits GHZ\n", num_of_qubits);
 
-  void* state = aer_state_initialize();
+  void* state = aer_state();
 
   aer_state_configure(state, "method", "statevector");
   aer_state_configure(state, "device", "CPU");
@@ -21,6 +21,8 @@ int main() {
 
   int start_qubit = aer_allocate_qubits(state, num_of_qubits);
   
+  aer_state_initialize(state);
+
   int* qubits = (int*) malloc(sizeof(int) * num_of_qubits);
   for (int i = 0; i < num_of_qubits; ++i)
     qubits[i] = start_qubit + i;
@@ -29,19 +31,19 @@ int main() {
   for (int i = 0; i < num_of_qubits - 1; ++i)
       aer_apply_cx(state, qubits[i], qubits[i + 1]);
 
-  printf("non-zero probabilities and amplitudes:\n", num_of_qubits);
+  printf("non-zero probabilities and amplitudes:\n");
   for (uint_t i = 0; i < (1UL << num_of_qubits); ++i) {
     double prob = aer_probability(state, i);
     if (prob > 0.0) {
       double complex amp = aer_amplitude(state, i);
-      printf("  %08x %lf [%lf, %lf]\n", i, prob, creal(amp), cimag(amp));
+      printf("  %08llx %lf [%lf, %lf]\n", i, prob, creal(amp), cimag(amp));
     }
   }
   
-  printf("all the amplitudes:\n", num_of_qubits);
+  printf("all the amplitudes:\n");
   double complex* sv = aer_release_statevector(state);
   for (uint_t i = 0; i < (1UL << num_of_qubits); ++i) {
-      printf("  %08x [%lf, %lf]\n", i, creal(sv[i]), cimag(sv[i]));
+      printf("  %08llx [%lf, %lf]\n", i, creal(sv[i]), cimag(sv[i]));
   }
 
   aer_state_finalize(state);
