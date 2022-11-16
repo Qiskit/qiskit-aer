f410260547086d7c1eea5fb1ad11c75c9d8518d8
Hiroshi Horii <hhorii@users.noreply.github.com>
2022-11-16 14:21:44 +0900

    change TARGET from P2 to KATMAI in OpenBLAS build

--- tools/install_openblas_i686.sh
+++ tools/install_openblas_i686.sh
@@ -3,5 +3,5 @@
 wget https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21.zip
 unzip OpenBLAS-0.3.21.zip
 cd OpenBLAS-0.3.21
-make TARGET=P2
-make TARGET=P2 PREFIX=/usr/ install
+make TARGET=KATMAI
+make TARGET=KATMAI PREFIX=/usr/ install
