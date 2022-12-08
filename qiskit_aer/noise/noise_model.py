10d013f4f7a6292464299b12ae6041aad584cdcd
Ryuhei Yoshida <yoshida0112@chem.s.u-tokyo.ac.jp>
2022-12-08 12:01:17 +0900

    Shorten the document for :func:`from_backend_properties`

--- qiskit_aer/noise/noise_model.py
+++ qiskit_aer/noise/noise_model.py
@@ -454,67 +454,14 @@ class NoiseModel:
     ):
         """Return a noise model derived from a devices backend properties.
 
-        This function generates a noise model based on:
+        This function generates a noise model in similar way as the :func:`from_backend`.
+        The difference is this function construct the noise model using the
+        :class:`BackendProperties` not :class:`Backend` like :func:`from_backend`.
 
-        * 1 and 2 qubit gate errors consisting of a
-          :func:`depolarizing_error` followed
-          by a :func:`thermal_relaxation_error`.
-
-        * Single qubit :class:`ReadoutError` on all measurements.
-
-        The Error error parameters are tuned for each individual qubit based on
-        the :math:`T_1`, :math:`T_2`, frequency and readout error parameters for
-        each qubit, and the gate error and gate time parameters for each gate
-        obtained from the device backend properties.
-
-        **Additional Information**
-
-        The noise model includes the following errors:
-
-        * If ``readout_error=True`` include single qubit readout
-          errors on measurements.
-
-        * If ``gate_error=True`` and ``thermal_relaxation=True`` include:
-
-            * Single-qubit gate errors consisting of a :func:`depolarizing_error`
-              followed by a :func:`thermal_relaxation_error` for the qubit the
-              gate acts on.
-
-            * Two-qubit gate errors consisting of a 2-qubit
-              :func:`depolarizing_error` followed by single qubit
-              :func:`thermal_relaxation_error` on each qubit participating in
-              the gate.
-
-        * If ``gate_error=True`` is ``True`` and ``thermal_relaxation=False``:
-
-            * An N-qubit :func:`depolarizing_error` on each N-qubit gate.
-
-        * If ``gate_error=False`` and ``thermal_relaxation=True`` include
-          single-qubit :func:`thermal_relaxation_errors` on each qubits
-          participating in a multi-qubit gate.
-
-        For best practice in simulating a backend make sure that the
-        circuit is compiled using the set of basis gates in the noise
-        module by setting ``basis_gates=noise_model.basis_gates``
-        and using the device coupling map with
-        ``coupling_map=backend.configuration().coupling_map``
-
-        **Specifying custom gate times**
-
-        The ``gate_lengths`` kwarg can be used to specify custom gate times
-        to add gate errors using the :math:`T_1` and :math:`T_2` values from
-        the backend properties. This should be passed as a list of tuples
-        ``gate_lengths=[(name, value), ...]``
-        where ``name`` is the gate name string, and ``value`` is the gate time
-        in nanoseconds.
-
-        If a custom gate is specified that already exists in
-        the backend properties, the ``gate_lengths`` value will override the
-        gate time value from the backend properties.
-        If non-default values are used gate_lengths should be a list
+        For the detailed infromation about this function, please refer to :func:`from_backend`.
 
         Args:
-            backend_properties (BackendProperties): backend.
+            backend_properties (BackendProperties): The property of backend.
             gate_error (Bool): Include depolarizing gate errors (Default: True).
             readout_error (Bool): Include readout errors in model
                                   (Default: True).
