aa89f61c6a4631e6b03aadbef18f1005e3572c2d
merav-aharoni <merav@il.ibm.com>
2023-01-31 12:17:49 +0000

    Changed the apply_measure algorithm so that it does not move the qubits to the sorted order before measurement. Instead it sorts the measurement outcomes as a post-processing step.

--- src/simulators/matrix_product_state/matrix_product_state_internal.hpp
+++ src/simulators/matrix_product_state/matrix_product_state_internal.hpp
@@ -453,6 +453,13 @@ private:
 
   uint_t sample_measure_single_qubit(uint_t qubit, double &prob, 
 				     double rnd, cmatrix_t &mat) const;
+
+  // The return result contains the elements of input_qubits sorted according to
+  // their order in ordering_.order_.
+  // The vector returned in sub_ordering contains the indices of the qubits to be
+  // measured in ordering_.order_.
+  reg_t sort_qubits_by_ordering(reg_t input_qubits, reg_t& sub_ordering);
+  reg_t sort_measured_values(reg_t input_outcome, reg_t& sub_ordering);
   //----------------------------------------------------------------
   // Function name: get_single_probability0
   // Description: Returns the probability that `qubit` will measure 0, given all the measurements
