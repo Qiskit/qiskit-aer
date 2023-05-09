34365cfa680834a8821c8883418050f344a65d8f
Hiroshi Horii <hhorii@users.noreply.github.com>
2023-05-09 23:28:54 +0900

    Update conf.py to pass lint

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ extensions = [
     'matplotlib.sphinxext.plot_directive',
     'sphinx.ext.intersphinx',
     'nbsphinx',
-    "qiskit_sphinx_theme",
+    'qiskit_sphinx_theme'
 ]
 
 # -----------------------------------------------------------------------------
