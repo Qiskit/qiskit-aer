059b19a194db14f57ac404e4b229c419b834ea83
Toshinari Itoko <itoko@jp.ibm.com>
2023-05-19 14:00:25 +0900

    simplify a bit

--- qiskit_aer/noise/device/models.py
+++ qiskit_aer/noise/device/models.py
@@ -359,10 +359,8 @@ def _device_thermal_relaxation_error(
 
 def _truncate_t2_value(t1, t2):
     """Return t2 value truncated to 2 * t1 (for t2 > 2 * t1)"""
-    if t1 is None:
+    if t1 is None or t2 is None:
         return t2
-    if t2 is None:
-        return None
     return min(t2, 2 * t1)
 
 
