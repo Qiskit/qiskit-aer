b2fc156e1e3dcd7ad281498592f0b8c7b7668f06
Hiroshi Horii <horii@jp.ibm.com>
2023-06-07 17:19:40 +0900

    update version in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ author = 'Qiskit Development Team'
 # The short X.Y version
 version = '0.12.1'
 # The full version, including alpha/beta/rc tags
-release = '0.12.0'
+release = '0.12.1'
 
 templates_path = ['_templates']
 
