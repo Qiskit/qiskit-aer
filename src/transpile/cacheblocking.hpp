5db037f624f2b0e1228d0dad98f1f16c42b73826
Adrian Roman <aroman_ro@yahoo.com>
2023-08-23 09:04:36 +0300

    For https://github.com/Qiskit/qiskit-aer/issues/1905 and https://github.com/Qiskit/qiskit-aer/issues/1906

--- src/transpile/cacheblocking.hpp
+++ src/transpile/cacheblocking.hpp
@@ -98,7 +98,7 @@ protected:
 
   void insert_swap(std::vector<Operations::Op> &ops, uint_t bit0, uint_t bit1,
                    bool chunk) const;
-  void insert_sim_op(std::vector<Operations::Op> &ops, char *name,
+  void insert_sim_op(std::vector<Operations::Op> &ops, const char *name,
                      const reg_t &qubits) const;
   void insert_pauli(std::vector<Operations::Op> &ops, reg_t &qubits,
                     std::string &pauli) const;
@@ -192,7 +192,7 @@ void CacheBlocking::insert_swap(std::vector<Operations::Op> &ops, uint_t bit0,
   ops.push_back(sgate);
 }
 
-void CacheBlocking::insert_sim_op(std::vector<Operations::Op> &ops, char *name,
+void CacheBlocking::insert_sim_op(std::vector<Operations::Op> &ops, const char *name,
                                   const reg_t &qubits) const {
   Operations::Op op;
   op.type = Operations::OpType::sim_op;
