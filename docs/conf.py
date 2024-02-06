5baadeedaff8402b1d5efc2fb2aa7c2f5d9d744c
Jun Doi <doichan@jp.ibm.com>
2024-02-06 15:53:29 +0900

    fix version numbers

--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ copyright = f"2017-{datetime.date.today().year}, Qiskit Development Team"  # pyl
 author = 'Qiskit Development Team'
 
 # The short X.Y version
-version = '0.13.1'
+version = '0.13.3'
 # The full version, including alpha/beta/rc tags
-release = '0.13.1'
+release = '0.13.3'
 
 templates_path = ['_templates']
 
