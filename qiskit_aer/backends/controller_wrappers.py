286599032143f3822919f171810981fa7915039a
Jun Doi <doichan@jp.ibm.com>
2024-03-15 16:04:50 +0900

    Update controller_wrappers.py

--- qiskit_aer/backends/controller_wrappers.py
+++ qiskit_aer/backends/controller_wrappers.py
@@ -1,6 +1,20 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Aer controll wrappers
+"""
 import importlib
 
-
+""" get wrapper with suffix """
 def try_import_backend(backend_module_suffix):
     module_name = f".controller_wrappers_{backend_module_suffix}"
     try:
