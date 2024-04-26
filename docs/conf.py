0b4f2c9670643dbe6aeda607ef42dfb37cb1dae8
Jun Doi <doichan@jp.ibm.com>
2024-04-26 13:12:25 +0900

    add release note and set versions

--- docs/conf.py
+++ docs/conf.py
@@ -47,9 +47,9 @@ copyright = f"2017-{datetime.date.today().year}, Qiskit Development Team"  # pyl
 author = 'Qiskit Development Team'
 
 # The short X.Y version
-version = '0.14.0'
+version = '0.14.1'
 # The full version, including alpha/beta/rc tags
-release = '0.14.0.1'
+release = '0.14.1'
 
 templates_path = ['_templates']
 
