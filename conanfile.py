7a0050310c5e983949da25d82b7f574ba5ef529f
Gadi Aleksandrowicz <gadial@gmail.com>
2025-09-21 12:10:30 +0300

    Typo fix + attempt to fix the windows error directly in the conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
 
 class AerConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -12,3 +13,11 @@ class AerConan(ConanFile):
 
         if self.settings.os in ["Linux", "Macos"]:
             self.requires("llvm-openmp/20.1.6")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_POLICY_DEFAULT_CMP0091"] = "NEW"
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
